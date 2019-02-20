3ad3f49142db67eb76e80919baad032adcafdbca
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2019-02-20 16:03:30 +0000

    VS_Solution to Create New Database

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Dim_Feedback.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Dim_Feedback
-(Feedback_SK int identity(9999,1) PRIMARY KEY,
- FeedbackType varchar(255),
- CategoryType varchar(max),
- CategorySubType varchar(255),
- MeasureType varchar(255)
- )
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FIC_Feedback.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Stg_FIC_Feedback]
+(
+	CategoryType_1 varchar(max) null,
+	CategoryType_2 varchar(max) null,
+	CampaignStartDate varchar(255) null,
+	CampaignEndDate varchar(255) null,
+	Count Varchar(255) null
+)

3b8ffb305941711622f507620a1f0d70b6c9d343
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-04 12:18:13 +0000

    Change Masking Function on Employer Account Id

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_Load_Commitments.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_Load_Commitments.sql
@@ -4,7 +4,7 @@ CREATE TABLE [Comt].[Stg_Load_Commitment]
 (
 	[Id] BIGINT NOT NULL PRIMARY KEY , 
     [Reference] NVARCHAR(100) MASKED WITH (FUNCTION='PARTIAL(1,"######",0)') NOT NULL, 
-    [EmployerAccountId] BIGINT MASKED WITH (FUNCTION='PARTIAL(2,"######",1)') NOT NULL, 
+    [EmployerAccountId] BIGINT MASKED WITH (FUNCTION='RANDOM(1,5)') NOT NULL, 
     [LegalEntityId] NVARCHAR(50) NOT NULL, 
     [LegalEntityName] NVARCHAR(100) NOT NULL, 
 	[LegalEntityAddress] NVARCHAR(256) NOT NULL,

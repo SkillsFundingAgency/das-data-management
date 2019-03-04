fff151594f20b811e6ef7bc273d33173e3235f73
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2019-03-04 11:01:37 +0000

    AddViews

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/vw_Apprenticeship_Commitments.sql
@@ -0,0 +1,33 @@
+﻿CREATE VIEW vw_Apprenticeship_Commitments
+AS
+SELECT Comm.Id as CommitmentId
+      ,Comm.ProviderId as ProviderId
+	  ,Comm.ProviderName as ProviderName
+	  ,Comm.EmployerAccountId
+	  ,Comm.Reference
+	  ,Comm.LegalEntityName
+	  ,Comm.LegalEntityAddress
+	  ,Comm.CommitmentStatus
+	  ,App.TrainingName
+	  ,App.FirstName ApprenticeFirstName
+	  ,App.LastName ApprenticeLastName
+	  ,App.ULN
+	  ,App.TrainingCode
+	  ,App.TrainingName
+	  ,App.StartDate TrainingStartDate
+	  ,App.EndDate  TrainingEndDate
+	  ,CASE WHEN PaymentStatus=1 then 'PendingApproval'
+	        WHEN PaymentStatus=2 then 'Active'
+			WHEN PaymentStatus=3 then 'Paused'
+			WHEN PaymentStatus=4 then 'Withdrawn'
+			WHEN PaymentStatus=5 then 'Completed'
+			WHEN PaymentStatus=6 then 'Deleted'
+			Else 'Unknown'
+		END AS PaymentStatus
+	  ,AO.Name AS EPAOName
+  FROM dbo.Apprenticeship App
+  JOIN dbo.Commitment Comm
+    ON App.CommitmentId=comm.Id
+  JOIN dbo.AssessmentOrganisation AO
+    ON AO.EPAOrgId=APP.EPAOrgId
+Go
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/vw_CommitmentSummary.sql
@@ -0,0 +1,59 @@
+﻿CREATE VIEW [dbo].[vw_CommitmentSummary]
+AS 
+
+WITH ApprenticeshipApprovalStatus_CTE (Id, CommitmentId, AgreementStatus, EmployerCanApproveApprenticeship, ProviderCanApproveApprenticeship)
+AS (SELECT Id, CommitmentId, AgreementStatus,
+	CASE 
+		WHEN
+			a.FirstName IS NOT NULL AND 
+			a.LastName IS NOT NULL AND 
+			a.Cost IS NOT NULL AND 
+			a.StartDate IS NOT NULL AND 
+			a.EndDate IS NOT NULL AND 
+			a.TrainingCode IS NOT NULL AND 
+			a.DateOfBirth IS NOT NULL
+		THEN 1 ELSE 0
+	END AS 'EmployerCanApproveApprenticeship',
+	CASE 
+		WHEN
+			a.FirstName IS NOT NULL AND 
+			a.LastName IS NOT NULL AND 
+			a.ULN IS NOT NULL AND -- ULN is required for provider approval
+			a.Cost IS NOT NULL AND 
+			a.StartDate IS NOT NULL AND 
+			a.EndDate IS NOT NULL AND 
+			a.TrainingCode IS NOT NULL AND 
+			a.DateOfBirth IS NOT NULL
+		THEN 1 ELSE 0
+	END AS 'ProviderCanApproveApprenticeship'
+
+	FROM 
+		Apprenticeship a
+)
+SELECT c.*, 
+	COUNT(a.Id) AS ApprenticeshipCount,
+
+	COALESCE((SELECT TOP 1 AgreementStatus FROM ApprenticeshipApprovalStatus_CTE WHERE CommitmentId = c.Id), 0) AS AgreementStatus, -- because should all be same value
+
+	CASE
+		WHEN COUNT(a.Id) > 0 AND 1 = ALL (SELECT EmployerCanApproveApprenticeship FROM ApprenticeshipApprovalStatus_CTE WHERE CommitmentId = c.Id) 
+		THEN 1
+		ELSE 0 
+	END AS EmployerCanApproveCommitment,
+
+	CASE
+		WHEN COUNT(a.Id) > 0 AND 1 = ALL (SELECT ProviderCanApproveApprenticeship FROM ApprenticeshipApprovalStatus_CTE WHERE CommitmentId = c.Id) 
+		THEN 1 
+		ELSE 0 
+	END AS ProviderCanApproveCommitment
+
+FROM 
+	Commitment c
+LEFT JOIN 
+	Apprenticeship a ON a.CommitmentId = c.Id
+
+GROUP BY 
+	c.Id, c.Reference, c.EmployerAccountId, c.LegalEntityId, c.LegalEntityName, c.ProviderId, c.ProviderName, c.CommitmentStatus, c.EditStatus, c.CreatedOn, c.LastAction, 
+	c.LastUpdatedByEmployerEmail, c.LastUpdatedByProviderEmail, c.LastUpdatedByEmployerName, c.LastUpdatedByProviderName, c.LegalEntityAddress, 
+	c.LegalEntityOrganisationType, c.TransferSenderId, c.TransferSenderName, c.TransferApprovalStatus, c.TransferApprovalActionedByEmployerEmail,
+	c.TransferApprovalActionedByEmployerName, c.TransferApprovalActionedOn, c.AccountLegalEntityPublicHashedId, c.Originator

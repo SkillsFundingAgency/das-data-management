505e018c8014a5a2e049826d8de5860c04fe3aa6
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-12 11:59:24 +0000

    Change_FIU_Feedback_Table_Definition

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FIC_Feedback.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Stg_FIC_Feedback]
-(
-	CategoryType_1 varchar(max) null,
-	CategoryType_2 varchar(max) null,
-	CampaignStartDate varchar(255) null,
-	CampaignEndDate varchar(255) null,
-	Count Varchar(255) null
-)
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FIU_Feedback.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Stg_FIU_Feedback]
+(
+	Campaign_Category varchar(255) null,
+	Campaign_SubCategory varchar(255) null,
+	Campaign_Metrics varchar(255) null,
+	CampaignStartDate varchar(255) null,
+	CampaignEndDate varchar(255) null,
+	Count Varchar(255) null
+)

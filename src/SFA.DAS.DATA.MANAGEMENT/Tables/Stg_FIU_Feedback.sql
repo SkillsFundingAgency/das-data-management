8bc04ced15fc892f850f89501905df3da7e83a5e
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-13 13:07:02 +0000

    Alter Feedback Table

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FIU_Feedback.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FIU_Feedback.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE [dbo].[Stg_FIU_Feedback]
 (
+	Feedback_Id int identity(1,1) ,
 	Campaign_Category varchar(255) null,
 	Campaign_SubCategory varchar(255) null,
 	Campaign_Metrics varchar(255) null,

bccbcdc5aabd3d663e8d0949ef06216e31ef738b
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-14 14:15:56 +0000

    Drop Stg_FIU_Feedback Table

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -9,6 +9,8 @@ EXEC Mgmt.USP_UnitTest1_CheckCounts
 EXEC mgmt.usp_Manage_GA_Hierarchy_Metadata
 
 
-DROP PROCEDURE [dbo].[USP_UnitTest1_CheckCounts]
+--DROP PROCEDURE [dbo].[USP_UnitTest1_CheckCounts]
 
-DROP TABLE [dbo].[Deployment_Audit]
+--DROP TABLE [dbo].[Deployment_Audit]
+
+DROP TABLE [dbo].Stg_FIU_Feedback

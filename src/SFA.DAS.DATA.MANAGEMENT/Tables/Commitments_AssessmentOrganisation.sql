f2ec299ab1d1e3530e1073a5e46c41a766272806
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-15 15:19:13 +0000

    Change Constraint Name

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Commitments_AssessmentOrganisation.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Commitments_AssessmentOrganisation.sql
@@ -6,6 +6,6 @@
 	[AsDm_Created_Date] datetime default(getdate()),
 	[AsDm_Updated_Date] datetime default(getdate()),
 	Load_Id int,
-	CONSTRAINT AK_EPAOrgId UNIQUE([EPAOrgId])
+	CONSTRAINT AO_EPAOrgId UNIQUE([EPAOrgId])
 )
 Go

106ed576da3f8647f3193564f4636afb7174f1cb
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2019-03-18 10:17:58 +0000

    Drop Unwanted Tables

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -3,6 +3,14 @@
 --if exists (select * from sys.objects where name = 'Stg_FIC_Feedback' and type = 'u')
 --DROP TABLE dbo.Stg_FIC_Feedback
 
+
+DROP TABLE mgmt.Metadata_GA_Hierarchy
+
+DROP PROCEDURE Mgmt.usp_Manage_Commitments_Lookup
+
+DROP PROCEDURE Mgmt.usp_Manage_GA_Hierarchy_Metadata
+
+
 EXEC Mgmt.USP_UnitTest1_CheckCounts
 
 
@@ -10,5 +18,7 @@ EXEC Mtd.usp_Manage_GA_Hierarchy_Metadata
 
 
 EXEC mtd.usp_Manage_Commitments_Lookup
+
+
 
 

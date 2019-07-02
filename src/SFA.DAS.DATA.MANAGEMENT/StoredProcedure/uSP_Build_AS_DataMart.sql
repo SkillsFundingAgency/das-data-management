536024337e3b506510f511aec8eec89f88b2bf2b
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-07-02 16:44:55 +0100

    no message

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/uSP_Build_AS_DataMart.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/uSP_Build_AS_DataMart.sql
@@ -28,8 +28,8 @@ EXEC dbo.uSP_Import_Provider @RunId
 
 EXEC dbo.uSP_Import_Employer @RunId
 
-EXEC uSP_Import_Commitments @RunId
+--EXEC uSP_Import_Commitments @RunId
 
-EXEC uSP_Import_Transfers @RunId
+--EXEC uSP_Import_Transfers @RunId
 
 

a3876dd5024b772f1fa2ffa2c7ee14d9fce98d22
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-08-12 17:08:30 +0100

    Add Schema for Reservations, Finance , Users and Account

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Schemas/Acct.sql
@@ -0,0 +1,2 @@
+﻿CREATE SCHEMA [Acct]
+Go
--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/uSP_Build_AS_DataMart.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/uSP_Build_AS_DataMart.sql
@@ -22,15 +22,15 @@ EXEC dbo.uSP_Create_External_Tables 'rsrvDBConnection','Ext_Tbl_InfSch_Reservati
 
 EXEC uSP_Create_System_External_Tables 'easfinDBConnection','Finance','Fin',@RunId
 
-EXEC dbo.uSP_Create_External_Tables 'easfinDBConnection','Finance','Fin',@RunId
+EXEC dbo.uSP_Create_External_Tables 'easfinDBConnection','Ext_Tbl_InfSch_Finance','Fin',@RunId
 
 EXEC uSP_Create_System_External_Tables 'usersDBConnection','Users','User',@RunId
 
-EXEC dbo.uSP_Create_External_Tables 'usersDBConnection','Users','User',@RunId
+EXEC dbo.uSP_Create_External_Tables 'usersDBConnection','Ext_Tbl_InfSch_Users','User',@RunId
 
 EXEC uSP_Create_System_External_Tables 'easAccDBConnection','Account','Acct',@RunId
 
-EXEC dbo.uSP_Create_External_Tables 'easAccDBConnection','Account','Acct',@RunId
+EXEC dbo.uSP_Create_External_Tables 'easAccDBConnection','Ext_Tbl_InfSch_Account','Acct',@RunId
 
 
 

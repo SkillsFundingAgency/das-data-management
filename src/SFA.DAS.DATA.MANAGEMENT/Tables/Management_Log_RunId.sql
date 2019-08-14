c09d869ff668b1167d229f3870e91cf19235b1e7
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-08-14 20:03:03 +0100

    Remove Default from Log_RunId Table

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_RunId.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_RunId.sql
@@ -1,6 +1,6 @@
 ﻿
 CREATE TABLE [Mgmt].[Log_RunId](
-	[RunId] [bigint] IDENTITY(1,1) NOT NULL DEFAULT(-1),
+	[RunId] [bigint] IDENTITY(1,1) NOT NULL,
 	[StartDateTime] [datetime2](7) NOT NULL,
 	[EndDateTime] [datetime2](7) NULL,
 	CONSTRAINT [PK_LogRunId_RunId] PRIMARY KEY CLUSTERED ([RunId] ASC)

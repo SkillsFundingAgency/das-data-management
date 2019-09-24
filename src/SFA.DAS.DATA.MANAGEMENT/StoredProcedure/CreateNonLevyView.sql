22e87f109bb19d4890eb39794de7686fadbcb11a
rgt7419 <rob.tanner@outlook.com>
2019-09-24 13:37:07 +0100

    Updated view to include payments and agreement data, and added optimisation

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/CreateNonLevyView.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/CreateNonLevyView.sql
@@ -5,7 +5,7 @@
 AS
 -- =========================================================================
 -- Author:      Rob Tanner
--- Create Date: 23/09/2019
+-- Create Date: 24/09/2019
 -- Description: Create View for NonLevy
 -- =========================================================================
 
@@ -47,54 +47,78 @@ Drop View Data_Pub.Das_NonLevy
 '
 SET @VSQL2='
 CREATE VIEW [Data_Pub].[Das_NonLevy]
-	AS 
+	AS
+
+WITH NonLevyPayments
+AS
+(SELECT *
+   FROM Fin.Ext_Tbl_Payment P
+  WHERE ApprenticeshipId in 
+             (SELECT a.Id as ApprenticeshipId
+                FROM Comt.Ext_Tbl_Apprenticeship a
+                join comt.Ext_Tbl_Commitment c
+                  on a.CommitmentId=c.Id
+               INNER
+                join Resv.Ext_Tbl_AccountLegalEntity AEL
+                  ON AEL.AccountId=c.EmployerAccountId
+                 AND AEL.AgreementType=1
+                 AND AEL.IsLevy=0) 
+)
+
 SELECT 
-       A.accountlegalentityid                      AS AccountLegalEntityId 
-      ,L.NAME                                      AS AccountLegalEntityName
-      ,L.created                                   AS AccountLegalEntityCreatedDate 
-      ,COALESCE(R.modifieddate, 0)                 AS AccountModifiedDate 
+       A.accountlegalentityid										AS AccountLegalEntityId 
+      ,L.NAME														AS AccountLegalEntityName
+      ,L.created													AS AccountLegalEntityCreatedDate 
+      ,COALESCE(R.modifieddate, ''9999-12-31'')                     AS AccountModifiedDate 
       ,CASE 
          WHEN A.templateid = ''1'' THEN ''LevyAgreementV1'' 
-         WHEN A.templateid = ''2'' THEN ''LevyAgreementV2''
+         WHEN A.templateid = ''2'' THEN ''LevyAgreementV2'' 
          WHEN A.templateid = ''3'' THEN ''NonLevyAgreementV1'' 
          ELSE ''Other'' 
-       END                                         AS AgreementTemplateId 
+       END															AS AgreementTemplateId 
       ,CASE 
          WHEN A.statusid = ''1'' THEN ''Pending'' 
          WHEN A.statusid = ''2'' THEN ''Signed'' 
          WHEN A.statusid = ''3'' THEN ''Expired'' 
          WHEN A.statusid = ''4'' THEN ''Superceded'' 
          ELSE ''InvalidEvent'' 
-       END                                         AS AgreementStatusId 
-      ,COALESCE(A.signeddate, 0)                   AS AgreementSignedDate 
-      ,COALESCE(F.accountid, -99999)               AS UserAccountId 
+       END															AS AgreementStatusId 
+      ,COALESCE(A.signeddate, ''9999-12-31'')                       AS AgreementSignedDate 
+      ,COALESCE(F.accountid, -99999)								AS UserAccountId 
       ,CASE 
          WHEN F.islevyaccount = ''1'' THEN ''Yes'' 
          WHEN F.islevyaccount = ''0'' THEN ''No'' 
          ELSE ''NA'' 
-       END                                         AS LevyAccount 
-      ,COALESCE(F.createddate, ''9999-12-31'')       AS CreatedDate 
-      ,COALESCE(F.startdate, ''9999-12-31'')         AS StartDate 
-      ,COALESCE(F.expirydate, ''9999-12-31'')        AS ReservationExpiryDate 
+       END															AS LevyAccount 
+      ,COALESCE(F.createddate, ''9999-12-31'')						AS CreatedDate 
+      ,COALESCE(F.startdate, ''9999-12-31'')						AS StartDate 
+      ,COALESCE(F.expirydate, ''9999-12-31'')						AS ReservationExpiryDate 
       ,CASE 
          WHEN F.status = ''1'' THEN ''Commitment'' 
          WHEN F.status = ''0'' THEN ''Reservation'' 
          ELSE ''NA'' 
-       END                                         AS ReservationStatus 
-      ,COALESCE(F.courseid, ''-99999'')              AS ReservationCourseId 
-      ,COALESCE(T.title, ''NA'')                     AS ReservationCourseTitle 
-      ,COALESCE(T.level, -99999)                   AS ReservationLevel 
-      ,COALESCE(CONVERT(VARCHAR(255), F.id), ''NA'') AS ReservationId 
-      ,COALESCE(P.commitmentid, -1)                AS CommitmentId 
-      ,COALESCE(P.createdon, ''9999-12-31'')         AS CommitmentCreatedOn 
-      ,COALESCE(P.startdate, ''9999-12-31'')         AS CommitmentStartDate 
-      ,COALESCE(P.trainingtype, -99999)            AS CommitmentTrainingType 
-      ,COALESCE(P.trainingname, ''NA'')              AS CommitmentTrainingName 
-      ,COALESCE(P.trainingcode, -99999)            AS CommitmentTrainingCode 
-      ,COALESCE(P.isapproved, -99999)              AS CommitmentIsApproved 
-      ,COALESCE(P.agreedon, ''9999-12-31'')          AS ApprovalAgreedOn 
-      ,COALESCE(C.providerid, -1)                  AS ProviderId
-      ,COALESCE(C.providername, ''NA'')              AS ApprovalProviderName 
+       END															AS ReservationStatus 
+      ,COALESCE(F.courseid, ''-99999'')								AS ReservationCourseId 
+      ,COALESCE(T.title, ''NA'')									AS ReservationCourseTitle 
+      ,COALESCE(T.level, -99999)									AS ReservationLevel 
+      ,COALESCE(CONVERT(VARCHAR(255), F.id), ''NA'')				AS ReservationId 
+      ,COALESCE(P.commitmentid, -1)									AS CommitmentId 
+      ,COALESCE(P.createdon, ''9999-12-31'')						AS CommitmentCreatedOn 
+      ,COALESCE(P.startdate, ''9999-12-31'')						AS CommitmentStartDate 
+      ,COALESCE(P.trainingtype, -99999)								AS CommitmentTrainingType 
+      ,COALESCE(P.trainingname, ''NA'')								AS CommitmentTrainingName 
+      ,COALESCE(P.trainingcode, -99999)								AS CommitmentTrainingCode 
+      ,COALESCE(P.isapproved, -99999)								AS CommitmentIsApproved 
+      ,COALESCE(P.agreedon, ''9999-12-31'')							AS ApprovalAgreedOn 
+      ,COALESCE(C.providerid, -1)									AS ProviderId
+      ,COALESCE(C.providername, ''NA'')								AS ApprovalProviderName 
+      ,COALESCE(CONVERT(VARCHAR(255),NLP.PaymentId),''NA'')			AS PaymentId
+      ,COALESCE(NLP.EvidenceSubmittedOn,''9999-12-31'')				AS EvidenceSubmittedOn
+      ,COALESCE(NLP.CollectionPeriodYear,''0000'')					AS CollectionPeriodYear
+      ,COALESCE(NLP.CollectionPeriodMonth,''-1'')					AS CollectionPeriodMonth
+      ,COALESCE(NLP.DeliveryPeriodYear,''0000'')					AS DeliveryPeriodYear
+      ,COALESCE(NLP.DeliveryPeriodMonth,''-1'')						AS DeliveryPeriodMonth
+      ,COALESCE(NLP.Amount,0)										AS PaidAmount
 FROM   acct.ext_tbl_account R 
        LEFT JOIN acct.ext_tbl_accountlegalentity L 
               ON L.accountid = R.id 
@@ -108,7 +132,12 @@ FROM   acct.ext_tbl_account R
               ON P.reservationid = F.id 
        LEFT JOIN comt.ext_tbl_commitment C 
               ON C.id = P.commitmentid 
-WHERE  A.templateid = ''3'' 
+       LEFT JOIN Resv.Ext_Tbl_AccountLegalEntity ALE
+              ON ALE.AccountId=R.Id
+       LEFT JOIN NonLevyPayments NLP
+              ON NLP.ApprenticeshipId=P.Id
+WHERE  ALE.AgreementType=1
+       AND ALE.IsLevy=0
        AND L.id <> ''33275'' 
        AND L.id <> ''33278'' 
        AND L.id <> ''33301''

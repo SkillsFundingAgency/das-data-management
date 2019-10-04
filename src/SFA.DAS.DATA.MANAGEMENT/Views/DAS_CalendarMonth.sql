fc1aaeaeb8e8064032ce964faa4b237b9dd5ae39
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-10-04 13:42:17 +0100

    CreateCalendarMonthView

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/DAS_CalendarMonth.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [Data_Pub].[DAS_CalendarMonth]
+	AS 
+SELECT CalendarMonthID
+    , CalendarDate
+    , CalendarMonthNumber
+    , CalendarMonthName
+    , CalendarMonthShortName
+    , CalendarYear 
+    , CalendarMonthNameYear 
+    , CalendarMonthShortNameYear 
+    , TaxMonthNumber 
+    , TaxYear
+    , AcademicMonthNumber 
+    , AcademicYear
+    , CalendarYearMonth_SortOrder
+  FROM dbo.DASCalendarMonth

52cbdc34e95c30c0d1aaa2df7e2a6fca1611d24e
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-11-08 16:39:33 +0000

    GrantAccessOnReferenceTableToDeveloper

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -23,7 +23,30 @@ GRANT SELECT ON SCHEMA :: Mgmt To Developer
 
 GRANT SELECT ON SCHEMA :: Resv To Developer
 
-GRANT SELECT ON SCHEMA :: dbo To Developer
+GRANT SELECT ON dbo.Apprentice To Developer
+
+GRANT SELECT ON dbo.Apprenticeship To Developer
+
+GRANT SELECT ON dbo.AssessmentOrganisation To Developer
+
+GRANT SELECT ON dbo.Commitment To Developer
+
+GRANT SELECT ON dbo.DataLockStatus To Developer
+
+GRANT SELECT ON dbo.EmployerAccount To Developer
+
+GRANT SELECT ON dbo.EmployerAccountLegalEntity To Developer
+
+GRANT SELECT ON dbo.Provider To Developer
+
+GRANT SELECT ON dbo.TrainingCourse To Developer
+
+GRANT SELECT ON dbo.Transfers To Developer
+
+GRANT SELECT ON dbo.[ReferenceData] To Developer
+
+GRANT SELECT ON [dbo].[DASCalendarMonth] To Developer
+
 
 IF EXISTS(select 1 from sys.views where name='Das_Commitments' and type='v')
 GRANT SELECT ON Data_Pub.Das_Commitments TO Developer

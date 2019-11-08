b9d69016316fbbdb925cef3e782c7b9828c3faef
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2019-11-08 13:52:08 +0000

    AddSelectPermissionOnAllTablesForDevRole, RemoveAccessToPaymentsOnAnalystRole

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -23,25 +23,7 @@ GRANT SELECT ON SCHEMA :: Mgmt To Developer
 
 GRANT SELECT ON SCHEMA :: Resv To Developer
 
-GRANT SELECT ON dbo.Apprentice To Developer
-
-GRANT SELECT ON dbo.Apprenticeship To Developer
-
-GRANT SELECT ON dbo.AssessmentOrganisation To Developer
-
-GRANT SELECT ON dbo.Commitment To Developer
-
-GRANT SELECT ON dbo.DataLockStatus To Developer
-
-GRANT SELECT ON dbo.EmployerAccount To Developer
-
-GRANT SELECT ON dbo.EmployerAccountLegalEntity To Developer
-
-GRANT SELECT ON dbo.Provider To Developer
-
-GRANT SELECT ON dbo.TrainingCourse To Developer
-
-GRANT SELECT ON dbo.Transfers To Developer
+GRANT SELECT ON SCHEMA :: dbo To Developer
 
 IF EXISTS(select 1 from sys.views where name='Das_Commitments' and type='v')
 GRANT SELECT ON Data_Pub.Das_Commitments TO Developer
@@ -79,6 +61,7 @@ GRANT SELECT ON Data_Pub.DAS_Employer_Transfer_Relationship TO Developer
 IF EXISTS(select 1 from sys.views where name='Das_TransactionLine' and type='v')
 GRANT SELECT ON Data_Pub.Das_TransactionLine TO Developer
 
+
 IF DATABASE_PRINCIPAL_ID('DataAnalyst') IS NULL
 BEGIN
 	CREATE ROLE [DataAnalyst]
@@ -90,8 +73,8 @@ GRANT SELECT ON Data_Pub.Das_Commitments TO DataAnalyst
 if exists(select 1 from sys.views where name='Das_NonLevy' and type='v')
 GRANT SELECT ON Data_Pub.Das_NonLevy TO DataAnalyst
 
-IF EXISTS(select 1 from sys.views where name='Das_Payments' and type='v')
-GRANT SELECT ON Data_Pub.Das_Payments TO DataAnalyst
+--IF EXISTS(select 1 from sys.views where name='Das_Payments' and type='v')
+--GRANT SELECT ON Data_Pub.Das_Payments TO DataAnalyst
 
 IF EXISTS(select 1 from sys.views where name='Das_LevyDeclarations' and type='v')
 GRANT SELECT ON Data_Pub.Das_LevyDeclarations TO DataAnalyst

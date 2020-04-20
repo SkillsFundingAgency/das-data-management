bd45887fb13768b557e8654f4cfd7c8186384799
Simon Heath <simon.heath@digital.education.gov.uk>
2020-04-20 14:27:20 +0100

    ADM-1345 Added grants and masking.

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -232,7 +232,8 @@ END
 GRANT SELECT ON dbo.Payments_SS TO DataAnalyst
 
 
-
+IF EXISTS(select 1 from sys.views where name='DAS_TransactionLine' and type='v')
+GRANT SELECT ON ASData_PL.DAS_SpendControl TO DataAnalyst
 
 /* Finance Role Access */
 
@@ -245,3 +246,7 @@ END
 
 IF EXISTS(select 1 from sys.views where name='DAS_SpendControl' and type='v')
 GRANT SELECT ON ASData_PL.DAS_SpendControl TO Finance
+
+IF EXISTS(select 1 from sys.views where name='DAS_SpendControlNonLevy' and type='v')
+GRANT SELECT ON ASData_PL.DAS_SpendControl TO Finance
+

e0cc40143e5b586a2334407655e014c2ee49c480
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-04-27 17:12:08 +0100

    AddNewFieldsToBankHolidaysTable

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_ReferenceDataBankHolidays.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_ReferenceDataBankHolidays.sql
@@ -3,8 +3,9 @@
  SourseSK BIGINT IDENTITY(1,1) NOT NULL
 ,BinaryId VARCHAR(256)
 ,LastUpdatedDateTimeStamp Varchar(256)
-,Division varchar(256)
-,DivisionEvents Varchar(max)
+,NortherIrelandEvents Varchar(max)
+,ScotlandEvents varchar(max)
+,EnglandAndWalesEvents varchar(max)
 ,RunId bigint  default(-1)
 ,AsDm_CreatedDate datetime2 default(getdate()) 
 ,AsDm_UpdatedDate datetime2 default(getdatE())

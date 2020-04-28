05fac8610b14172902e88ebe33ea54e1a99f6a37
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-04-28 11:42:52 +0100

    BannedPhrases

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_ReferenceDataBannedPhrases.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_ReferenceDataBannedPhrases.sql
@@ -3,7 +3,7 @@
  SourseSK BIGINT IDENTITY(1,1) NOT NULL
 ,BinaryId VARCHAR(256)
 ,LastUpdatedDateTimeStamp Varchar(256)
-,BannedPhrases Varchar(256)
+,BannedPhrases Varchar(max)
 ,RunId bigint  default(-1)
 ,AsDm_CreatedDate datetime2 default(getdate()) 
 ,AsDm_UpdatedDate datetime2 default(getdatE())

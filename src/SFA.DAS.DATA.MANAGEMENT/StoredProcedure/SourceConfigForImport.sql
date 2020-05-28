37c10179a45e083504e5042f19c7f595adce04dc
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-05-28 17:31:56 +0100

    SourceConfigForImport

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/SourceConfigForImport.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/SourceConfigForImport.sql
@@ -3,8 +3,8 @@
 	[SCFI_Id] INT NOT NULL 
    ,SourceDatabaseName Varchar(256)
    ,SourceTableName Varchar(256)
-   ,ColumnNamesToInclude Varchar(512)
-   ,ColumnNamesToExclude Varchar(512)
+   ,ColumnNamesToInclude Varchar(MAX)
+   ,ColumnNamesToExclude Varchar(MAX)
    ,IsEnabled bit default(1)
    ,AddedDate DateTime2 default(getdate())
    ,UpdatedDate DateTime2

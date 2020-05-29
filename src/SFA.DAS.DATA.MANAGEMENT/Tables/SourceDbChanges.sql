6a5bf1c41764fea4d7f1bd84a551aaa7f741fabb
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-05-29 15:36:01 +0100

    SourceDbChangesAddIdentityColumn

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/SourceDbChanges.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/SourceDbChanges.sql
@@ -1,8 +1,9 @@
 ﻿CREATE TABLE [dbo].[SourceDbChanges]
 (
-	[SDC_Id] INT NOT NULL 
+	[SDC_Id] INT IDENTITY(1,1) NOT NULL 
    ,[SourceDatabaseName] Varchar(256)
    ,[SouceTableName] Varchar(256)
    ,[ChangesDetected] Varchar(256)
    ,DetectedDate DateTime2 default(getdate())
+   ,CONSTRAINT [PK_SourceDbChanges_SDCId] PRIMARY KEY CLUSTERED (SDC_ID ASC)
 )

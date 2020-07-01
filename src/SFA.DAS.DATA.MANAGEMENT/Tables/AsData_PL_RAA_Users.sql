25b2824bd9f7dccaa080819e2c3df1dfd54df729
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-07-01 15:54:35 +0100

    RAA_Users

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsData_PL_RAA_Users.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE AsData_PL.RAA_Users
+(
+  SourseSK INT IDENTITY(1,1) NOT NULL
+ ,BinaryId varchar(256)
+ ,TypeCode varchar(10)
+ ,IdamUserId varchar(256)
+ ,UserType varchar(256)
+ ,UserName varchar(256) MASKED WITH (FUNCTION = 'default()')
+ ,UserEmail varchar(256) MASKED WITH (FUNCTION = 'Email()') 
+ ,UserCreatedTimeStamp bigint
+ ,UserCreatedDateTime datetime2
+ ,LastSignedInTimeStamp bigint
+ ,LastSignedDateTime datetime2
+ ,EmployerAccountId varchar(100)
+ ,Ukprn int
+ ,RunId bigint  default(-1)
+ ,AsDm_CreatedDate datetime2 default(getdate()) 
+ ,AsDm_UpdatedDate datetime2 default(getdatE())
+ )
+

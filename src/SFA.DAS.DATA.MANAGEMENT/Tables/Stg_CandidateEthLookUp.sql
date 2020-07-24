0e8bb1c9fb31f2655bbf65fd3418f2154c124700
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-07-24 19:03:38 +0100

    CandidateEthLookUp

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_CandidateEthLookUp.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [Stg].[CandidateEthLookUp]
+(
+   SourceSK BIGINT IDENTITY(1,1) PRIMARY KEY
+  ,EID BIGINT
+  ,ShortName nvarchar(255)
+  ,FullName nvarchar(255)
+  ,SourceDb nvarchar(100)
+)

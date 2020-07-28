2623a5da9a100e79b1647daf9df01fbd85f6c93b
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-07-28 16:51:37 +0100

    GenerateConfig

---
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/GenerateConfig.sql
@@ -0,0 +1,121 @@
+﻿CREATE PROCEDURE [dbo].[GenerateConfig]
+(
+   @RunId int,@SourceDb Varchar(255),@Category Varchar(255)
+)
+AS
+
+-- =======================================================
+-- Author:      Himabindu Uddaraju
+-- Create Date: 27/07/2020
+-- Description: Generate Candidate Config for Import
+-- =======================================================
+
+BEGIN TRY
+
+    SET NOCOUNT ON
+
+ DECLARE @LogID int
+
+/* Start Logging Execution */
+
+  INSERT INTO Mgmt.Log_Execution_Results
+	  (
+	    RunId
+	   ,StepNo
+	   ,StoredProcedureName
+	   ,StartDateTime
+	   ,Execution_Status
+	  )
+  SELECT 
+        @RunId
+	   ,'Step-3'
+	   ,'GenerateCandidateConfig'
+	   ,getdate()
+	   ,0
+
+   SELECT @LogID=MAX(LogId) FROM Mgmt.Log_Execution_Results
+   WHERE StoredProcedureName='GenerateConfig'
+     AND RunId=@RunID
+
+
+IF @@TRANCOUNT=0
+BEGIN
+BEGIN TRANSACTION
+
+SELECT *
+  INTO #tConfig
+  FROM Stg.CandidateConfig 
+ WHERE SourceDb=@SourceDb
+   AND Category=@Category
+
+DELETE FROM Stg.CandidateConfig
+ WHERE SourceDb=@SourceDb
+   AND Category=@Category
+
+INSERT INTO Stg.CandidateConfig
+(SourceDb,Category,ShortCode,RunId)
+select SourceDb,Category,ShortCode,@RunId
+from #tConfig
+ORDER BY NEWID()
+
+
+
+
+
+COMMIT TRANSACTION
+END
+
+/* Update Log Execution Results as Success if the query ran succesfully*/
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=1
+      ,EndDateTime=getdate()
+	  ,FullJobStatus='Pending'
+ WHERE LogId=@LogID
+   AND RunID=@RunId
+
+ 
+END TRY
+
+BEGIN CATCH
+    IF @@TRANCOUNT>0
+	ROLLBACK TRANSACTION
+
+    DECLARE @ErrorId int
+
+  INSERT INTO Mgmt.Log_Error_Details
+	  (UserName
+	  ,ErrorNumber
+	  ,ErrorState
+	  ,ErrorSeverity
+	  ,ErrorLine
+	  ,ErrorProcedure
+	  ,ErrorMessage
+	  ,ErrorDateTime
+	  ,RunId
+	  )
+  SELECT 
+        SUSER_SNAME(),
+	    ERROR_NUMBER(),
+	    ERROR_STATE(),
+	    ERROR_SEVERITY(),
+	    ERROR_LINE(),
+	    'GenerateConfig',
+	    ERROR_MESSAGE(),
+	    GETDATE(),
+		@RunId as RunId; 
+
+  SELECT @ErrorId=MAX(ErrorId) FROM Mgmt.Log_Error_Details
+
+/* Update Log Execution Results as Fail if there is an Error*/
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=0
+      ,EndDateTime=getdate()
+	  ,ErrorId=@ErrorId
+ WHERE LogId=@LogID
+   AND RunID=@RunId
+
+  END CATCH
+
+GO

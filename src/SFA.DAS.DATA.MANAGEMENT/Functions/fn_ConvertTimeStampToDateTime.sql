243f378d68fb34014a217148f8674f89403774e8
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-07-30 17:55:59 +0100

    FunctionHandleNullValues

--- src/SFA.DAS.DATA.MANAGEMENT/Functions/fn_ConvertTimeStampToDateTime.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Functions/fn_ConvertTimeStampToDateTime.sql
@@ -2,5 +2,10 @@
 RETURNS DATETIME2
 AS
 BEGIN
-     RETURN CAST(DATEADD(ms, CAST(RIGHT(@UnixTimeStamp,3) AS smallint), DATEADD(s, @UnixTimeStamp / 1000, '1970-01-01')) AS datetime2(3))
+DECLARE @DateTime DateTime2
+     IF (@UnixTimeStamp IS NULL)
+	 SET @DateTime= NULL
+	 ELSE 
+	 SET @DateTime= CAST(DATEADD(ms, CAST(RIGHT(@UnixTimeStamp,3) AS smallint), DATEADD(s, @UnixTimeStamp / 1000, '1970-01-01')) AS datetime2(3))
+	 RETURN @DateTime
 END;

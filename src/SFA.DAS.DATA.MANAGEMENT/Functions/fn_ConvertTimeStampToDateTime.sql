c70fa9778406c6226a65163ce3735bba44447fe6
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-07-30 19:19:09 +0100

    FunctionChangeToCopeWithDodgyData

--- src/SFA.DAS.DATA.MANAGEMENT/Functions/fn_ConvertTimeStampToDateTime.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Functions/fn_ConvertTimeStampToDateTime.sql
@@ -3,9 +3,12 @@ RETURNS DATETIME2
 AS
 BEGIN
 DECLARE @DateTime DateTime2
-     IF (@UnixTimeStamp IS NULL)
+     IF (TRY_CONVERT(BIGINT,@UnixTimeStamp) IS NULL)
 	 SET @DateTime= NULL
 	 ELSE 
+	 IF (LEN(@UnixTimeStamp)>13)
+	 SET @DateTime=NULL
+	 ELSE
 	 SET @DateTime= CAST(DATEADD(ms, CAST(RIGHT(@UnixTimeStamp,3) AS smallint), DATEADD(s, @UnixTimeStamp / 1000, '1970-01-01')) AS datetime2(3))
 	 RETURN @DateTime
 END;

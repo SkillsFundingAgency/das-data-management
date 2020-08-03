6c36d7e6c86bf0b3f674a5c6bf663b5d55f598fe
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-08-03 11:28:22 +0100

    ConvertTimeStampToDateTime-FunctionChanges

--- src/SFA.DAS.DATA.MANAGEMENT/Functions/fn_ConvertTimeStampToDateTime.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Functions/fn_ConvertTimeStampToDateTime.sql
@@ -9,6 +9,6 @@ DECLARE @DateTime DateTime2
 	 IF (LEN(@UnixTimeStamp)>13)
 	 SET @DateTime=NULL
 	 ELSE
-	 SET @DateTime= CAST(DATEADD(ms, CAST(RIGHT(@UnixTimeStamp,3) AS smallint), DATEADD(s, @UnixTimeStamp / 1000, '1970-01-01')) AS datetime2(3))
+	 SET @DateTime= CAST(DATEADD(ms, CAST(TRY_CONVERT(smallint,RIGHT(@UnixTimeStamp,3)) AS smallint), DATEADD(s, TRY_CONVERT(INT,(CAST(@UnixTimeStamp as bigint) / 1000)), '1970-01-01')) AS datetime2(3))
 	 RETURN @DateTime
 END;

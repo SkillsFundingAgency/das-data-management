1ae035a62b0908a67442460160f93663f091d9a6
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-08-06 11:51:47 +0100

    VacancyCandidateDetailsToPL

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_CandidateDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [AsData_PL].[Va_CandidateDetails]
+(
+	[CandidateDetailsId] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY
+   ,[CandidateId] BIGINT
+   ,[CandidateEthnicCode] Varchar(25)
+   ,[CandidateEthnicDesc] Varchar(256)
+   ,[SourceDb] Varchar(256)
+   ,Foreign Key (CandidateId) References [AsData_PL].[Va_Candidate](CandidateId)
+)

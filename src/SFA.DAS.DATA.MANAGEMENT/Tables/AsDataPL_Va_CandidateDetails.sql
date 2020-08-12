7cb26a24808ad35d4bb89b5782c360492821c6b0
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-08-12 13:28:31 +0100

    Remove Foreign Key Constraint

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_CandidateDetails.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_CandidateDetails.sql
@@ -5,5 +5,4 @@
    ,[CandidateEthnicCode] Varchar(25)
    ,[CandidateEthnicDesc] Varchar(256)
    ,[SourceDb] Varchar(256)
-   ,Foreign Key (CandidateId) References [AsData_PL].[Va_Candidate](CandidateId)
 )

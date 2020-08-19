c62273e4dac422f9414f55a42e0169e02f06c336
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-08-19 12:32:59 +0100

    Revert "SaltKey logging,dashboard view stored procedures"
    This reverts commit 573b022c1a7b0a19ad623e80f599dee397c3d207.

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/SaltKeyLog.sql
+++
@@ -1,8 +0,0 @@
--- SaltKeyLog 
-Go
-Create Table [AsData_PL].[SaltKeyLog](
-	SaltKeyID BigInt PRIMARY KEY,
-	SaltKey  Varbinary(max) DEFAULT CRYPT_GEN_RANDOM(4096),
-	SourceType Varchar(50)
-) ON [PRIMARY]
-Go

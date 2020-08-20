f31a55a447327afeca9855fb67eaf503381aae81
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-08-20 15:00:13 +0100

    AccountsTableForIncentives

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_EL_Accounts.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE AsData_Pl.EL_Accounts
+(
+    [Id] BIGINT NOT NULL,
+	[AccountLegalEntityId] BIGINT  NOT NULL,
+	[LegalEntityId] BIGINT  NOT NULL,
+	[HasSignedIncentivesTerms] BIT NOT NULL 
+)
+
+
+
+

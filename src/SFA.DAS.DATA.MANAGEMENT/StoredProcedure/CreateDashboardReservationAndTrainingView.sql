1cdbc330c8253acd374a2dd3cfd5917a7352da70
AD\SEVANI <sarma.evani@education.gov.uk>
2020-09-02 08:33:53 +0100

    Fixing performance issues with commitment and training views

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/CreateDashboardReservationAndTrainingView.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/CreateDashboardReservationAndTrainingView.sql
@@ -52,20 +52,15 @@ BEGIN TRY
 		Select TOP 1 SaltKeyID,SaltKey From AsData_PL.SaltKeyLog Where SourceType =''CampaignDashboard''  Order by SaltKeyID DESC 
 		), reservationbase as 
 		(
-			SELECT	B1,B2,B4,B5,B11,B13,B16,B17,B26,B32,Approved,TransferApproved,CommDateTransfer,CommDateBase,
+			SELECT	B1,B2,B4,B5,B11,B13,B16,B17,B26,CommDateTransfer,CommDateBase,
 				case when CommDateTransfer IS NULL Then 1 Else 0 End as NoTransfer,
-				case when CommDateTransfer IS NULL Then CommDateBase Else CommDateTransfer End as ApprovedDate,
-				SaltKeyID
+				case when CommDateTransfer IS NULL Then CommDateBase Else CommDateTransfer End as ApprovedDate
 					FROM 
 					(	
 						SELECT 
-						CASE 
-						WHEN Apprenticeship.Id IS NOT NULL THEN CONVERT(NVARCHAR(500),HASHBYTES(''SHA2_512'',LTRIM(RTRIM(CONCAT(Apprenticeship.Id, SaltKeyData.SaltKey)))),2) 
-						END AS B1,												
+						Apprenticeship.Id As B1,											
 						Case when Cast(Apprenticeship.CreatedOn as Date) != cast(''9999-12-31'' as Date) then Apprenticeship.CreatedOn Else NULl End AS B2,							
-						CASE 
-						WHEN Reservation.Id IS NOT NULL THEN CONVERT(NVARCHAR(500),HASHBYTES(''SHA2_512'',LTRIM(RTRIM(CONCAT(Reservation.Id, SaltKeyData.SaltKey)))),2) 
-						END AS B4,
+						Reservation.Id As B4,
 						Case when Cast(Reservation.StartDate as Date) != cast(''9999-12-31'' as Date) then Reservation.StartDate Else NULl End AS B5, 										
 						Case when Cast(Apprenticeship.StartDate as Date) != cast(''9999-12-31'' as Date) then Apprenticeship.StartDate Else NULl End AS B11, 								
 						case when upper(trim(Apprenticeship.TrainingCode)) =''NULL'' or  upper(trim(Apprenticeship.TrainingCode)) = '''' or trim(Apprenticeship.TrainingCode) = ''-99999'' Then NULL 
@@ -73,22 +68,17 @@ BEGIN TRY
 						AS B13,												
 						COALESCE(Apprenticeship.IsApproved, -1)	AS B16,				
 						Apprenticeship.Cost AS B17,												
-						Case when Cast(Commitment.ApprenticeshipEmployerTypeOnApproval As Int) != -1 Then Cast(Commitment.ApprenticeshipEmployerTypeOnApproval As Int) Else NULL End AS B26,									
-						Case when Cast(Apprenticeship.CompletionDate as Date) != cast(''9999-12-31'' as Date) then Apprenticeship.CompletionDate Else NULl End AS B32,						
-						Case when COALESCE(Commitment.Approvals, 1) = 3 AND COALESCE(Commitment.TransferApprovalStatus, 1) = 1 Then 1 Else 0 End as Approved,						
-						Case when COALESCE(Commitment.TransferApprovalStatus, 1) = 7 Then 1 Else 0 End as TransferApproved,						
+						Case when Cast(Commitment.ApprenticeshipEmployerTypeOnApproval As Int) != -1 Then Cast(Commitment.ApprenticeshipEmployerTypeOnApproval As Int) Else NULL End AS B26,																					
 						Case when COALESCE(Commitment.TransferApprovalStatus, 1) = 7 Then 
 								case when cast(Commitment.TransferApprovalActionedOn as date) != cast(''9999-12-31'' as Date) then cast(Commitment.TransferApprovalActionedOn as date) Else NULL End 
 							else NULL 		
 						End		as CommDateTransfer,						
-						Case when COALESCE(Commitment.Approvals, 1) = 3 AND COALESCE(Commitment.TransferApprovalStatus, 1) = 1 Then cast(Commitment.EmployerAndProviderApprovedOn as datetime) Else NULL End as CommDateBase,
-						SaltKeyData.SaltKeyID
+						Case when COALESCE(Commitment.Approvals, 1) = 3 AND COALESCE(Commitment.TransferApprovalStatus, 1) = 1 Then cast(Commitment.EmployerAndProviderApprovedOn as datetime) Else NULL End as CommDateBase						
 			FROM		Comt.Ext_Tbl_Apprenticeship Apprenticeship
 						FULL OUTER JOIN Resv.Ext_Tbl_Reservation Reservation
 						ON Reservation.Id = Apprenticeship.ReservationId
 						FULL OUTER JOIN Comt.Ext_Tbl_Commitment Commitment
-						ON Commitment.Id = Apprenticeship.CommitmentId
-						CROSS JOIN SaltKeyData 
+						ON Commitment.Id = Apprenticeship.CommitmentId						
 			WHERE		Reservation.CreatedDate >= ''01-Jan-2020''
 		) As reservations
 		),   
@@ -109,24 +99,20 @@ BEGIN TRY
 					End As Min_C2
 			from 
 			(
-						SELECT	DISTINCT CASE 									
-								WHEN App.Id IS NOT NULL THEN  CONVERT(NVARCHAR(500),HASHBYTES(''SHA2_512'',LTRIM(RTRIM(CONCAT(App.Id, SaltKeyData.SaltKey)))),2)
-								END AS C1,
-								Pay.CollectionPeriod AS C2
-					FROM		Comt.Ext_Tbl_Apprenticeship App
-								JOIN StgPmts.Payment Pay
-								ON Pay.LearnerUln = App.Uln
-								CROSS JOIN SaltKeyData 
-					WHERE		Pay.AcademicYear = ''1920''
+					SELECT	DISTINCT App.Id As C1,Pay.CollectionPeriod AS C2 FROM Comt.Ext_Tbl_Apprenticeship App JOIN StgPmts.Payment Pay ON Pay.LearnerUln = App.Uln	WHERE		Pay.AcademicYear = ''1920''
 					AND		Pay.CollectionPeriod IN (6, 7, 8, 9, 10, 11, 12, 13, 14)
 			)  as outerquery
 			group by C1
 		)
 		Select  
-				r.B1,
+				CASE 
+				WHEN r.B1 IS NOT NULL THEN CONVERT(NVARCHAR(500),HASHBYTES(''SHA2_512'',LTRIM(RTRIM(CONCAT(r.B1, SaltKeyData.SaltKey)))),2) 
+				END AS B1,				
 				r.B13,
-				r.B2,
-				r.B4,
+				r.B2,				
+				CASE 
+				WHEN r.B4 IS NOT NULL THEN CONVERT(NVARCHAR(500),HASHBYTES(''SHA2_512'',LTRIM(RTRIM(CONCAT(r.B4, SaltKeyData.SaltKey)))),2) 
+				END AS B4,
 				r.B5,
 				case when r.B5 IS NULL Then 0 Else 1 End as ReservedFlag,
 				r.B11,
@@ -142,11 +128,11 @@ BEGIN TRY
 				p.Min_C2,
 				case when p.Min_C2 IS NULL then 0 Else 1 End as StartedFlag,
 				datediff(day,r.ApprovedDate,p.min_c2) as ApprovedtoStartedLag,
-				r.SaltKeyID									
+				SaltKeyData.SaltKeyID									
 		from	reservationbase r 			
 		left join stdFrameworks s on r.B13 = s.Code 
 		left join payments p on r.B1 = p.C1
-		Where r.B1 IS NOT NULL  '		   
+		CROSS JOIN SaltKeyData   '		   
 
 	EXEC SP_EXECUTESQL @VSQL1 -- run check to drop view if it exists. 
 	EXEC (@VSQL2 + @VSQL3) -- run sql to create view. 

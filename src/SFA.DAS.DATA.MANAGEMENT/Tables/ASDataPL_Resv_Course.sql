15682c102b7c91b20d2cf501f216d1103def3b38
sarmaevanidfe <68238278+sarmaevanidfe@users.noreply.github.com>
2020-10-01 20:18:53 +0100

    Delete ASDataPL_Resv_Course.sql

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Resv_Course.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [ASData_PL].[Resv_Course](
-	[CourseId] [varchar](20) NOT NULL,
-	[Title] [varchar](500) NOT NULL,
-	[Level] [tinyint] NOT NULL,
-	[EffectiveTo] [datetime] NULL,
-	[AsDm_UpdatedDateTime] datetime2 default getdate(),
-PRIMARY KEY CLUSTERED  ([CourseId] ASC )) ON [PRIMARY]
-GO

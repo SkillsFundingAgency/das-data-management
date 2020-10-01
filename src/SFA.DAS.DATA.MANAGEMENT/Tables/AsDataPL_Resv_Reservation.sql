a3feea7c96dafba95985ad9069105396431636cd
AD\SEVANI <sarma.evani@education.gov.uk>
2020-10-01 20:45:50 +0100

    Rename the table schema script files

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Resv_Reservation.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [ASData_PL].[Resv_Reservation]
+(
+	[Id] [uniqueidentifier] NOT NULL,
+	[AccountId] [bigint] NOT NULL,
+	[IsLevyAccount] [tinyint] NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[StartDate] [datetime] NULL,
+	[ExpiryDate] [datetime] NULL,
+	[Status] [tinyint] NOT NULL,
+	[CourseId] [varchar](20) NULL,
+	[AccountLegalEntityId] [bigint] NULL,
+	[ProviderId] [int] NULL,	
+	[TransferSenderAccountId] [bigint] NULL,
+	[UserId] [uniqueidentifier] NULL,
+	[ClonedReservationId] [uniqueidentifier] NULL,
+	[ConfirmedDate] [datetime] NULL,
+	[CohortId] [bigint] NULL,
+	[DraftApprenticeshipId] [bigint] NULL,
+	[AsDm_UpdatedDateTime] datetime2 default getdate(),
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+) ON [PRIMARY]
+GO

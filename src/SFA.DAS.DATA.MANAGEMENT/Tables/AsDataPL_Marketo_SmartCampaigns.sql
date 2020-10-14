b0f3e506eb9f65600d42cd675468bb6bee0d8267
uhimabindu <Himabindu.uddaraju@digital.education.gov.uk>
2020-10-14 13:14:15 +0100

    SmartCampaingsTable

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Marketo_SmartCampaigns.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [AsData_PL].[Marketo_SmartCampaigns]
+(
+	[SmartCampaignId] [bigint] NULL,
+	[SmartCampaignName] [nvarchar](526) NULL,
+	[SmartCampaignDesc] nvarchar(max),
+	[SmartCampaignType] [nvarchar](255) NULL,
+	[Status] nvarchar(100),
+	[IsActive] nvarchar(100),
+	[SmartListId] bigint,
+	[ParentProgramId] [bigint] NULL,
+	[WorkspaceName] [nvarchar](max) NULL,
+	[createdAt] DATETIME2 NULL,
+	[updatedAt] DATETIME2 NULL,
+	[active] [bit] NULL,
+    AsDm_CreatedDate Datetime2,
+    AsDm_UpdatedDate datetime2
+)

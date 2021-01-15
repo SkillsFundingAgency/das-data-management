222d84d5585d6604d28a20b1673f8fa97d1717ce
AD\SEVANI <sarma.evani@education.gov.uk>
2021-01-15 11:24:54 +0000

    Errors correction for GA PL table deployment

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -554,28 +554,3 @@ DECLARE @RunId int
 EXEC @RunId= dbo.GenerateRunId
 
 EXEC ImportPaymentsSnapshot @RunId
-
-
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_IncentiveApplicationApprenticeship') AND c.[name] = 'UKPRN')
-ALTER TABLE [ASData_PL].[EI_IncentiveApplicationApprenticeship] DROP COLUMN [UKPRN];
-
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_ApprenticeshipIncentive') AND c.[name] = 'UKPRN')
-ALTER TABLE [ASData_PL].[EI_ApprenticeshipIncentive] DROP COLUMN [UKPRN]
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_ApprenticeshipIncentive') AND c.[name] = 'RefreshedLearnerForEarnings')
-ALTER TABLE [ASData_PL].[EI_ApprenticeshipIncentive] DROP COLUMN [RefreshedLearnerForEarnings]
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_ApprenticeshipIncentive') AND c.[name] = 'HasPossibleChangeOfCircumstances')
-ALTER TABLE [ASData_PL].[EI_ApprenticeshipIncentive] DROP COLUMN [HasPossibleChangeOfCircumstances]
-
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_CollectionCalendar') AND c.[name] = 'CensusDate')
-ALTER TABLE [ASData_PL].[EI_CollectionCalendar] DROP COLUMN [CensusDate]
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_CollectionCalendar') AND c.[name] = 'AcademicYear')
-ALTER TABLE [ASData_PL].[EI_CollectionCalendar] DROP COLUMN [AcademicYear]
-
-IF exists (SELECT * FROM  sys.columns c INNER JOIN  sys.objects t ON (c.[object_id] = t.[object_id]) WHERE t.[object_id] = OBJECT_ID(N'ASData_PL.EI_CollectionCalendar') AND c.[name] = 'Active')
-ALTER TABLE [ASData_PL].[EI_CollectionCalendar] DROP COLUMN [Active]

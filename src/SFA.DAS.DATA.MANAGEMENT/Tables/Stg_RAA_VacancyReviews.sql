6d1c16e640b65c9d2a484beb1440e5366358beb0
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-01-18 19:05:12 +0000

    Vacancy Review Additional Columns

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReviews.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReviews.sql
@@ -3,7 +3,12 @@
   SourseSK BIGINT IDENTITY(1,1) NOT NULL
  ,BinaryId VARCHAR(256) 
  ,TypeCode VARCHAR(256)
+ ,EmployerAccountId varchar(256)
+ ,UserId varchar(256)
+ ,CreatedTimeStamp varchar(256)
+ ,SubmittedTimeStamp varchar(256)
  ,VacancyReference VARCHAR(256)
+ ,SourceVacancyReference varchar(256)
  ,ManualOutcome varchar(256)
  ,ManualQaFieldIndicator varchar(256)
  ,ManualQaFieldChangeRequested varchar(256)

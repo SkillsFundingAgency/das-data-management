2ef7a5eb172c0ae5d650e024850b2fb262111293
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-01-18 18:52:39 +0000

    RAA Vacancy Reviews

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReviews.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE Stg.RAA_VacancyReviews
+(
+  SourseSK BIGINT IDENTITY(1,1) NOT NULL
+ ,BinaryId VARCHAR(256) 
+ ,TypeCode VARCHAR(256)
+ ,VacancyReference VARCHAR(256)
+ ,ManualOutcome varchar(256)
+ ,ManualQaFieldIndicator varchar(256)
+ ,ManualQaFieldChangeRequested varchar(256)
+ ,ManualQaComment varchar(256)
+ ,RunId bigint  default(-1)
+ ,AsDm_CreatedDate datetime2 default(getdate()) 
+ ,AsDm_UpdatedDate datetime2 default(getdatE())
+ )

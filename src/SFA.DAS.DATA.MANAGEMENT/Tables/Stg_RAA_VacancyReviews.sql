9024e8d369fd899e0a584b138d171dea91deeeba
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-01-18 19:41:03 +0000

    Change Size

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReviews.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReviews.sql
@@ -12,7 +12,7 @@
  ,ManualOutcome varchar(256)
  ,ManualQaFieldIndicator varchar(256)
  ,ManualQaFieldChangeRequested varchar(256)
- ,ManualQaComment varchar(256)
+ ,ManualQaComment varchar(max)
  ,RunId bigint  default(-1)
  ,AsDm_CreatedDate datetime2 default(getdate()) 
  ,AsDm_UpdatedDate datetime2 default(getdatE())

aaa2b93cfec34b75df58b6f642236cc815f7945a
AD\SEVANI <sarma.evani@education.gov.uk>
2021-02-01 16:38:46 +0000

    Schema changes for [Stg].[GA_SessionDataDetailTest]

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -546,3 +546,6 @@ BEGIN
      GRANT SELECT ON ASData_PL.MarketoPrograms TO DataGov
 END
 
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[Stg].[GA_SessionDataDetailTest]') AND type in (N'U'))
+DROP TABLE [Stg].[GA_SessionDataDetailTest]
+GO

a9766474349981959518f572ecccfcd1373b8975
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2021-02-03 15:27:17 +0000

    Asintel 1277 import faa to data mart (#131)
    * Payments Snapshot Refresh
    
    * FAA Staging Tables
    
    * Additional Columns
    
    * Change Column Data Type
    
    * New Columns
    
    * RAA Vacancy Reviews
    
    * Vacancy Review Additional Columns
    
    * Change Size
    
    * FAA & RAA Build
    
    * Missing Columns

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FAA_Apprenticeships.sql
@@ -0,0 +1,25 @@
+﻿CREATE TABLE Stg.[FAA_Apprenticeships]
+(
+  SourseSK BIGINT IDENTITY(1,1) NOT NULL
+ ,BinaryId varchar(256)
+ ,TypeCode varchar(256)
+ ,EntityId varchar(256)
+ ,EntityTypeCode varchar(256)
+ ,DateCreatedTimeStamp varchar(256)
+ ,DateUpdatedTimeStamp varchar(256)
+ ,DateAppliedTimeStamp varchar(256)
+ ,CandidateId varchar(256)
+ ,LegacyApplicationId varchar(256)
+ ,VacancyId varchar(256)
+ ,VacancyReference varchar(256) 
+ ,IsRecruitVacancy varchar(256) 
+ ,ApplyViaEmployerWebsite varchar(256) 
+ ,SuccessfulTimeStamp varchar(256)
+ ,UnsuccessfulTimeStamp varchar(256)
+ ,WithdrawnOrDeclinedReason varchar(512)
+ ,UnsuccessfulReason varchar(512)
+ ,RunId bigint  default(-1)
+ ,AsDm_CreatedDate datetime2 default(getdate()) 
+ ,AsDm_UpdatedDate datetime2 default(getdatE())
+)
+

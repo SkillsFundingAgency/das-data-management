fc00d9ad7056f3331a04bc5023733d80ebf4c6f7
AD\SEVANI <sarma.evani@education.gov.uk>
2021-02-09 10:25:13 +0000

    Change the schema as per new source db changes

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_EI_ApprenticeshipDaysInLearning.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_EI_ApprenticeshipDaysInLearning.sql
@@ -1,9 +1,9 @@
 ﻿CREATE TABLE [ASData_PL].[EI_ApprenticeshipDaysInLearning]
 (
-	[LearnerId]							[uniqueidentifier]	NOT NULL PRIMARY KEY,
+	[LearnerId]							[uniqueidentifier]	NOT NULL,
 	[NumberOfDaysInLearning]			[int]				NULL,
-	[CollectionPeriodNumber]			[tinyint]			NULL,
-	[CollectionPeriodYear]				[smallint]			NULL,
+	[CollectionPeriodNumber]			[tinyint]			NOT NULL,
+	[CollectionPeriodYear]				[smallint]			NOT NULL,
 	[CreatedDate]						[datetime2](7)		NULL,
 	[UpdatedDate]						[datetime2](7)		NULL,
 	[AsDm_UpdatedDateTime]				[datetime2](7)		DEFAULT (getdate())

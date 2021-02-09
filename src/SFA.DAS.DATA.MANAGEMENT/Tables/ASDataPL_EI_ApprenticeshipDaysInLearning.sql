39756aed21b8548e6d25ef262f77df8f42c7ab67
AD\SEVANI <sarma.evani@education.gov.uk>
2021-02-09 12:29:30 +0000

    Changes in schema

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_EI_ApprenticeshipDaysInLearning.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_EI_ApprenticeshipDaysInLearning.sql
@@ -2,8 +2,8 @@
 (
 	[LearnerId]							[uniqueidentifier]	NOT NULL,
 	[NumberOfDaysInLearning]			[int]				NULL,
-	[CollectionPeriodNumber]			[tinyint]			NOT NULL,
-	[CollectionPeriodYear]				[smallint]			NOT NULL,
+	[CollectionPeriodNumber]			[tinyint]			NULL,
+	[CollectionPeriodYear]				[smallint]			NULL,
 	[CreatedDate]						[datetime2](7)		NULL,
 	[UpdatedDate]						[datetime2](7)		NULL,
 	[AsDm_UpdatedDateTime]				[datetime2](7)		DEFAULT (getdate())	

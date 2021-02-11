59f7ee0c35fa47a56804001d73748e61b2f5fd14
AD\SEVANI <sarma.evani@education.gov.uk>
2021-02-11 14:56:17 +0000

    Changes in modelled PL procedure

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/Build_Modelled_PL.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/Build_Modelled_PL.sql
@@ -7,18 +7,18 @@ AS
 -- Description: Master Stored Proc that builds Modelled Presentation Layer
 -- =========================================================================
 
-EXEC dbo.ImportAppRedundancyToPL @RunId
+--EXEC dbo.ImportAppRedundancyToPL @RunId
 
-EXEC dbo.ImportAccountsToPL @RunId
+--EXEC dbo.ImportAccountsToPL @RunId
 
 
 /* Import CRS and CRS Delivery Data to Presentation Layer */
 
-EXEC dbo.ImportProviderToPL @RunId
+--EXEC dbo.ImportProviderToPL @RunId
 
-EXEC dbo.ImportFAT2FrameworkToPL @RunId
+--EXEC dbo.ImportFAT2FrameworkToPL @RunId
 
-EXEC dbo.ImportFAT2SectorStandardToPL @RunId
+--EXEC dbo.ImportFAT2SectorStandardToPL @RunId
 
 
 /* Run Payments Snaptshot */

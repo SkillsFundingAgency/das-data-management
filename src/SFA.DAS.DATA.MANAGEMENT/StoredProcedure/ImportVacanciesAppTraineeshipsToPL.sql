6ce80f2e5c01c8a58420d0ccc3d321857ad06916
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-03-01 17:42:27 +0000

    Apprenticeships From Vacancies

---
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacanciesAppTraineeshipsToPL.sql
@@ -0,0 +1,155 @@
+﻿CREATE PROCEDURE [dbo].[ImportVacanciesAdditionalInfoToPL]
+(
+   @RunId int
+)
+AS
+-- ==============================================================================================
+-- Author:      Himabindu Uddaraju
+-- Create Date: 01/03/2021
+-- Description: Import Vacancies Apprenticeships, Traineeships, Saved Searches, ContactMessages
+--              Vacancy Reviews etc to PL
+-- ==============================================================================================
+
+BEGIN TRY
+
+DECLARE @LogID int
+DEClARE @quote varchar(5) = ''''
+
+/* Start Logging Execution */
+
+  INSERT INTO Mgmt.Log_Execution_Results
+	  (
+	    RunId
+	   ,StepNo
+	   ,StoredProcedureName
+	   ,StartDateTime
+	   ,Execution_Status
+	  )
+  SELECT 
+        @RunId
+	   ,'Step-6'
+	   ,'ImportVacanciesAdditionalInfoToPL'
+	   ,getdate()
+	   ,0
+
+  SELECT @LogID=MAX(LogId) FROM Mgmt.Log_Execution_Results
+   WHERE StoredProcedureName='ImportVacanciesAdditionalInfoToPL'
+     AND RunId=@RunID
+
+BEGIN TRANSACTION
+
+DELETE FROM ASData_PL.Va_Apprenticeships
+
+INSERT INTO [ASData_PL].[Va_Candidate]
+           (CandidateId 
+           ,ApplicationId 
+           ,VacancyId 
+           ,VacancyReference 
+           ,CreatedDateTime 
+           ,UpdatedDateTime 
+           ,AppliedDateTime 
+           ,IsRecruitVacancy
+           ,ApplyViaEmployerWebsite 
+           ,SuccessfulDateTime 
+           ,UnsuccessfulDateTime 
+           ,WithDrawnOrDeclinedReason 
+           ,UnsuccessfulReason 
+           ,SourceApprenticeshipId
+           ,SourceDb 
+		   )
+SELECT vc.CandidateId                                                  as CandidateId
+      ,vv.VacancyId                                                    as VacancyId
+	  ,coalesce(va.ApplicationId,vav2.ApplicationId,FA.LEGACYAPPLICATIONID) as ApplicationID
+	  ,vv.VacancyId                                                    as VacancyId
+	  ,vv.VacancyReferenceNumber                                       as VacancyReferenceNumber
+	  ,dbo.Fn_ConvertTimeStampToDateTime(fa.DateCreatedTimeStamp)      as DateCreatedTimeStamp
+	  ,dbo.Fn_ConvertTimeStampToDateTime(fa.DateUpdatedTimeStamp)      as DateUpdatedTimeStamp
+	  ,dbo.Fn_ConvertTimeStampToDateTime(fa.DateAppliedTimeStamp)      as DateAppliedTimeStamp
+	  ,IsRecruitVacancy                                                as IsRecruitVacancy
+	  ,ApplyViaEmployerWebsite                                         as ApplyViaEmployerWebsite
+	  ,dbo.Fn_ConvertTimeStampToDateTime(fa.SuccessfulTimeStamp)       as SuccessfulTimeStamp
+	  ,dbo.Fn_ConvertTimeStampToDateTime(fa.UnsuccessfulTimeStamp)     as UnsuccessfulTimeStamp
+	  ,WithdrawnOrDeclinedReason                                       as WithdrawnOrDeclinedReason
+	  ,UnsuccessfulReason                                              as UnsuccessfulReason
+	  ,FA.BinaryId                                                     as SourceApprenticeshipId
+	  ,'RAAv2'                                                         as SourceDb
+  FROM Stg.FAA_Apprenticeships FA
+  LEFT
+  JOIN ASData_PL.Va_Candidate VC
+    ON FA.CandidateId=vc.SourceCandidateId_v2 
+  left
+  join ASData_PL.Va_Application VA
+    ON VA.SourceApplicationId=FA.LegacyApplicationId
+   AND VA.SourceDb='RAAv1'
+   AND FA.LegacyApplicationId<>0
+  left
+  Join ASData_PL.Va_Vacancy vv
+    on vv.VacancyReferenceNumber= cast(replace(fa.vacancyreference,'vac','') AS INT)
+  left
+  join STG.RAA_ApplicationReviews RAR
+    ON RAR.CandidateId=FA.CandidateId
+   AND RAR.VacancyReference=cast(replace(fa.vacancyreference,'vac','') AS INT)
+  LEFT
+  JOIN ASData_PL.Va_Application vav2
+    on vav2.SourceApplicationId=rar.SourseSK
+   and vav2.SourceDb='RAAv2'
+
+
+ 
+
+     
+COMMIT TRANSACTION
+
+
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=1
+      ,EndDateTime=getdate()
+	  ,FullJobStatus='Pending'
+ WHERE LogId=@LogID
+   AND RunId=@RunId
+
+ 
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT>0
+	ROLLBACK TRANSACTION;
+
+    DECLARE @ErrorId int
+
+  INSERT INTO Mgmt.Log_Error_Details
+	  (UserName
+	  ,ErrorNumber
+	  ,ErrorState
+	  ,ErrorSeverity
+	  ,ErrorLine
+	  ,ErrorProcedure
+	  ,ErrorMessage
+	  ,ErrorDateTime
+	  ,RunId
+	  )
+  SELECT 
+        SUSER_SNAME(),
+	    ERROR_NUMBER(),
+	    ERROR_STATE(),
+	    ERROR_SEVERITY(),
+	    ERROR_LINE(),
+	    'ImportVacanciesAdditionalInfoToPL',
+	    ERROR_MESSAGE(),
+	    GETDATE(),
+		@RunId as RunId; 
+
+  SELECT @ErrorId=MAX(ErrorId) FROM Mgmt.Log_Error_Details
+
+/* Update Log Execution Results as Fail if there is an Error*/
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=0
+      ,EndDateTime=getdate()
+	  ,ErrorId=@ErrorId
+ WHERE LogId=@LogID
+   AND RunID=@RunId
+
+  END CATCH
+
+GO
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_Apprenticeships.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE AsData_PL.[Va_Apprenticeships]
+(     
+       Faa_ApprenticeshipID BIGINT IDENTITY(1,1) PRIMARY KEY
+      ,CandidateId bigint
+      ,ApplicationId bigint
+      ,VacancyId bigint
+      ,VacancyReference varchar(256)
+      ,CreatedDateTime datetime2
+      ,UpdatedDateTime datetime2
+      ,AppliedDateTime datetime2
+      ,IsRecruitVacancy varchar(256)
+      ,ApplyViaEmployerWebsite varchar(256)
+      ,SuccessfulDateTime datetime2
+      ,UnsuccessfulDateTime datetime2
+      ,WithDrawnOrDeclinedReason nvarchar(max)
+      ,UnsuccessfulReason nvarchar(max)
+      ,SourceApprenticeshipId varchar(256)
+      ,SourceDb varchar(100)
+      ,Foreign Key (CandidateId)  References [AsData_PL].[Va_Candidate](CandidateId)
+      ,Foreign Key (VacancyId) References [AsData_PL].[Va_Vacancy](VacancyId) 
+ )

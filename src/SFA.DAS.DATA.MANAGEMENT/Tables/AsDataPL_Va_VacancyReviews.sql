866c851b1c3d795975e67085fd2edcfd4ffbbdfb
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-03-03 17:04:21 +0000

    VacancyReviews

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_VacancyReviews.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[AsDataPL_Va_VacancyReviews]
+(
+   VacancyReviewId BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY
+  ,EmployerAccountId VARCHAR(256)
+  ,CandidateId BIGINT
+  ,CreatedDateTime DATETIME2
+  ,SubmittedDateTime DateTime2
+  ,VacancyReference Varchar(256)
+  ,VacancyId bigint
+  ,ManualOutcome varchar(256)
+  ,ManualQaFieldIndicator varchar(256)
+  ,ManualQaFieldChangeRequested varchar(256)
+  ,ManualQaComment varchar(max)
+  ,SourceVacancyReviewId varchar(256)
+  ,SourceDb varchar(256)
+)

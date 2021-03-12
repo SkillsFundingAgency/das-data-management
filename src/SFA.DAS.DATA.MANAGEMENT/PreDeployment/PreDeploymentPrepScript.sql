cf76e4ea10c3fe646ab10896b1aae587f44382ce
AD\SEVANI <sarma.evani@education.gov.uk>
2021-03-12 15:02:53 +0000

    GA metrics data amendments

--- src/SFA.DAS.DATA.MANAGEMENT/PreDeployment/PreDeploymentPrepScript.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PreDeployment/PreDeploymentPrepScript.sql
@@ -479,9 +479,3 @@ IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES
 		      AND TABLE_SCHEMA=N'Stg'
 	      )
 DROP TABLE [Stg].[GA_SessionData]
-
-
-
-TRUNCATE TABLE [Stg].[GA_SessionDataDetail]
-
-TRUNCATE TABLE [ASData_PL].[GA_SessionData]

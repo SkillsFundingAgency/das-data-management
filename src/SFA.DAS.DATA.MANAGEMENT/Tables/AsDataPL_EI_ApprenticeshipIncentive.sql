be17a0b6ed958178d247ebca6855540b29f692a7
AD\SEVANI <sarma.evani@education.gov.uk>
2021-03-26 15:06:55 +0000

    EI new table and amendments

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_EI_ApprenticeshipIncentive.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_EI_ApprenticeshipIncentive.sql
@@ -1,15 +1,18 @@
 ﻿CREATE TABLE [AsData_PL].[EI_ApprenticeshipIncentive]
 (
-   ID UniqueIdentifier Primary Key Not Null
-  ,AccountId bigint not null
-  ,ApprenticeshipId bigint not null
-  ,EmployerType int not null
-  ,IncentiveApplicationApprenticeshipId uniqueidentifier not null
-  ,AccountLegalEntityId bigint null
-  ,UKPRN bigint NULL
-  ,RefreshedLearnerForEarnings bit NULL
-  ,HasPossibleChangeOfCircumstances bit NULL
-  ,PausePayments bit NULL
-  ,StartDate datetime2(7) NULL
-  ,AsDm_UpdatedDateTime datetime2 default getdate()
+   ID													UniqueIdentifier Primary Key	Not Null
+  ,AccountId											bigint							not null
+  ,ApprenticeshipId										bigint							not null
+  ,EmployerType											int								not null
+  ,IncentiveApplicationApprenticeshipId					uniqueidentifier				not null 
+  ,AccountLegalEntityId									bigint							null
+  ,UKPRN												bigint							NULL
+  ,RefreshedLearnerForEarnings							bit								NULL
+  ,HasPossibleChangeOfCircumstances						bit								NULL
+  ,PausePayments										bit								NULL
+  ,StartDate											datetime2(7)					NULL
+  ,SubmittedDate										datetime2(7)					NULL
+  ,CourseName											nvarchar(200)					NULL
+  ,[Status]												nvarchar(50)					NULL
+  ,AsDm_UpdatedDateTime									datetime2						default getdate()
 )
--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_EI_IncentiveApplicationApprenticeship.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_EI_IncentiveApplicationApprenticeship.sql
@@ -1,12 +1,15 @@
 ﻿CREATE TABLE [AsData_PL].[EI_IncentiveApplicationApprenticeship]
 (
-	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY,
-	[IncentiveApplicationId] UNIQUEIDENTIFIER NOT NULL,
-	[ApprenticeshipId] BIGINT NOT NULL,
-	[PlannedStartDate] DATETIME2 NOT NULL,
-	[ApprenticeshipEmployerTypeOnApproval] INT NOT NULL,
-	[TotalIncentiveAmount] MONEY NOT NULL,
-	[EarningsCalculated] bit null,
-	[UKPRN]  BIGINT	NULL,
-	[AsDm_UpdatedDateTime] DateTime2 default(getdate())
+	[Id]										UNIQUEIDENTIFIER		NOT NULL PRIMARY KEY,
+	[IncentiveApplicationId]					UNIQUEIDENTIFIER		NOT NULL,
+	[ApprenticeshipId]							BIGINT					NOT NULL,
+	[PlannedStartDate]							DATETIME2				NOT NULL,
+	[ApprenticeshipEmployerTypeOnApproval]		INT						NOT NULL,
+	[TotalIncentiveAmount]						MONEY					NOT NULL,
+	[EarningsCalculated]						bit						null,
+	[UKPRN]										BIGINT					NULL,
+	[WithdrawnByEmployer]						bit						NULL,
+	[WithdrawnByCompliance]						bit						NULL,
+	[CourseName]								nvarchar(126)			NULL,
+	[AsDm_UpdatedDateTime]						DateTime2 default(getdate())
 )

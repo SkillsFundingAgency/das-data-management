944af39ff5e08cb020de1382f21606e5629b84c9
AD\SEVANI <sarma.evani@education.gov.uk>
2021-04-14 16:59:42 +0100

    Assessor PL tables

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Assessor_OrganisationStandard.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [AsData_PL].[Assessor_OrganisationStandard]
+(
+	[Id]								[int]				Primary Key NOT NULL,
+	[EndPointAssessorOrganisationId]	[nvarchar](12)					NULL,
+	[StandardCode]						[int]							NULL,
+	[EffectiveFrom]						[datetime]						NULL,
+	[EffectiveTo]						[datetime]						NULL,
+	[DateStandardApprovedOnRegister]	[datetime]						NULL,
+	[Comments]							[nvarchar](500)					NULL,
+	[Status]							[nvarchar](10)					NULL,
+	[ContactId]							[uniqueidentifier]				NULL,
+	[DeliveryAreaComments]				[NVarchar](500)					NULL,
+	[StandardReference]					[nvarchar](10)					NULL,
+	[AsDm_UpdatedDateTime]				[Datetime2](7)					default getdate(),
+)

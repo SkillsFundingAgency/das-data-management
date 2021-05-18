d022b1b17c4ab29e7e33a38b4ff8a0756e26f399
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-05-18 19:41:47 +0100

    Remove Masking

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/SaltKeyLog.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/SaltKeyLog.sql
@@ -2,7 +2,7 @@
 Go
 Create Table [Mgmt].[SaltKeyLog](
 	SaltKeyID BigInt PRIMARY KEY,
-	SaltKey  Varbinary(max) MASKED WITH (FUNCTION = 'default()') DEFAULT CRYPT_GEN_RANDOM(4096) ,
+	SaltKey  Varbinary(max) DEFAULT CRYPT_GEN_RANDOM(4096) ,
 	SourceType Varchar(50),
 	CreatedDateTime DateTime2 default (getdate())
 ) ON [PRIMARY]

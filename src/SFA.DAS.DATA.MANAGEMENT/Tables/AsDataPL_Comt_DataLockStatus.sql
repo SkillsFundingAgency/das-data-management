f1406a977aef80fe95816b4daf94595d9514bf48
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2021-06-03 15:57:41 +0100

    Asintel 1639 decommission phase2 import tables (#185)
    * Decommission Phase 2 - Import Commitments Tables
    
    * Correct Errors
    
    * Add AsDm_UpdatedDateTime

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_DataLockStatus.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [AsData_PL].[Comt_DataLockStatus]
+(
+	   [Id] bigint Primary Key not null
+      ,[DataLockEventId] bigint not null
+      ,[DataLockEventDatetime] datetime not null
+      ,[PriceEpisodeIdentifier] nvarchar(25) not null
+      ,[ApprenticeshipId] Bigint Not null
+      ,[IlrTrainingCourseCode] nvarchar(20)
+      ,[IlrTrainingType] tinyint
+      ,[IlrActualStartDate] datetime
+      ,[IlrEffectiveFromDate] datetime
+      ,[IlrPriceEffectiveToDate] datetime
+      ,[IlrTotalCost] decimal(18,0)
+      ,[ErrorCode] int not null
+      ,[Status] tinyint not null
+      ,[TriageStatus] tinyint not null
+      ,[ApprenticeshipUpdateId] bigint 
+      ,[IsResolved] bit not null
+      ,[EventStatus] tinyint not null
+      ,[IsExpired] bit not null
+      ,[Expired] datetime null
+	  ,[AsDm_UpdatedDateTime] datetime2 default getdate()
+)

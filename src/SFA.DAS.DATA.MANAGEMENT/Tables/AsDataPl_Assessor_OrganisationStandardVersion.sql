5ea256b8691f7d932580fbe3bbb4617081b4f4f9
AD\SEVANI <sarma.evani@education.gov.uk>
2021-06-10 15:33:22 +0100

    Assessor schema amendments

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPl_Assessor_OrganisationStandardVersion.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPl_Assessor_OrganisationStandardVersion.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [AsData_PL].[Assessor_OrganisationStandardVersion]
 (
-	[StandardUId]				[varchar](20)		PRIMARY KEY NOT NULL,
-	[Version]					[decimal](18, 1)	NULL,
-	[OrganisationStandardId]	[int]				NOT NULL,
-	[EffectiveFrom]				[datetime]			NULL,
-	[EffectiveTo]				[datetime]			NULL,
-	[DateVersionApproved]		[datetime]			NULL,
-	[Comments]					[nvarchar](500)		NULL,
-	[Status]					[nvarchar](10)		NULL,
-	[AsDm_UpdatedDateTime]		[Datetime2](7)		default getdate()
+	[OrganisationStandardVersionID]  [BigInt]			PRIMARY KEY	NOT NULL,
+	[StandardUId]					 [varchar](20)		NOT NULL,
+	[Version]						 [decimal](18, 1)	NULL,
+	[OrganisationStandardId]		 [int]				NOT NULL,
+	[EffectiveFrom]					 [datetime]			NULL,
+	[EffectiveTo]					 [datetime]			NULL,
+	[DateVersionApproved]			 [datetime]			NULL,
+	[Comments]					     [nvarchar](500)	NULL,
+	[Status]					     [nvarchar](10)		NULL,
+	[AsDm_UpdatedDateTime]		     [Datetime2](7)		default getdate()
 )

d9a364cecb49a7822cc40ba0a3d9fff232c226b1
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-06-14 13:18:59 +0100

    Revert "GA Optimisation"
    This reverts commit 3505397300132584d8c074aceb7db5fb5b2aed55.

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportGASessionDataToPL.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportGASessionDataToPL.sql
@@ -37,9 +37,7 @@ BEGIN TRY
 
 		 CREATE TABLE #StgClientIDs(ClientId NVARCHAR(500),ClientIDSource  Varchar(50))
 
-		BEGIN TRANSACTION		
-		        
-				DELETE FROM [ASData_PL].[GA_DataForReporting]		
+		BEGIN TRANSACTION				
 
 				if (select count([GASD_Id]) from [ASData_PL].[GA_SessionData]  with (nolock))  > 0 
 					Select @importdatetime = ISNULL(max([GA_ImportDate]),cast('01-01-1900'  as datetime2(7))) from [ASData_PL].[GA_SessionData] with (nolock)
@@ -170,21 +168,7 @@ BEGIN TRY
 				Where GAData.[StgImportDate] > @importdatetime AND ClientIDSource ='STG'
 		
 				IF  EXISTS (select * from INFORMATION_SCHEMA.TABLES  where table_name ='GA_SessionDataDetail' AND TABLE_SCHEMA='Stg' AND TABLE_TYPE='BASE TABLE')
-				TRUNCATE TABLE [Stg].[GA_SessionDataDetail]			
-				
-				/* Insert Into GA Table with required data for Summarized Reporting */
-				INSERT INTO ASData_PL.GA_DataForReporting
-				(CD_EmployerId,ESFATOKEN,ClientId,VisitDate)
-				SELECT DISTINCT
-                      [CD_EmployerId]
-                     ,g.ESFATOKEN
-                     ,ga.ClientId
-                     ,ga.VisitDate
-                  FROM ASData_PL.GA_SessionData as ga with (nolock)
-            INNER JOIN (SELECT  TRY_CONVERT(BIGINT,coalesce(ESFAToken,[EventLabel_ESFAToken],[CD_ESFAToken])) as ESFAToken,clientid	
-                          FROM ASData_PL.GA_SessionData  with (nolock)		
-                         WHERE TRY_CONVERT(BIGINT,coalesce(ESFAToken,[EventLabel_ESFAToken],[CD_ESFAToken])) IS NOT NULL) as g
-                    ON ga.clientid=g.clientid
+				TRUNCATE TABLE [Stg].[GA_SessionDataDetail]				
 				
 		COMMIT TRANSACTION
 

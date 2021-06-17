b9ab84f6ed448619310bd53c9cffdf5dbb76e45e
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2021-06-17 14:27:18 +0100

    GA Optimisation (#199)
    * GA Optimisation
    
    * Change Transaction Block
    
    * Fix Payments Refresh Proc
    
    * Commitments Logic Change for Realised Commitment

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_GA_DataForReporting.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [ASData_PL].[GA_DataForReporting](
+	[GDFR_Id]						[bigint] IDENTITY(1,1) NOT NULL
+   ,[CD_EmployerId]                 [nvarchar](50)
+   ,[ESFATOKEN]                     [bigint]
+   ,[ClientId]                      [nvarchar](200)
+   ,[VisitDate]	                    [date]
+   ,[GA_ImportDate]					[datetime2](7) DEFAULT getdate()
+   ,CONSTRAINT PK_GA_DataForReporting PRIMARY KEY CLUSTERED (GDFR_Id)
+) 
+GO

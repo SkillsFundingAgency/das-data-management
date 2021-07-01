0710e445b4feb71ff2264d9eb1721db6e17bdf4a
AD\SEVANI <sarma.evani@education.gov.uk>
2021-07-01 18:48:51 +0100

    Employer demand amendments

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_AED_CourseDemand.sql
@@ -0,0 +1,22 @@
+﻿CREATE TABLE [AsData_PL].[AED_CourseDemand]
+(
+	Id						uniqueidentifier, 
+	ContactEmailAddress     varchar(255),
+	OrganisationName		varchar(1000),
+	NumberOfApprentices     int, 
+	CourseId				int, 
+	CourseTitle				varchar(1000),
+	CourseLevel				int, 
+	CourseRoute				varchar(500),
+	LocationName			varchar(1000),
+	Lat						float, 
+	Long					float, 
+	DateCreated				datetime, 
+	EmailVerified			bit, 
+	DateEmailVerified		datetime, 	
+	Stopped					bit, 
+	DateStopped				datetime, 	
+	ExpiredCourseDemandId   uniqueidentifier, 
+	EntryPoint				smallint,
+	Asdm_UpdatedDateTime datetime2 default getdate()
+)
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_AED_ProviderInterest.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [AsData_PL].[AED_ProviderInterest]
+(
+		Id						uniqueidentifier, 
+		EmployerDemandId		uniqueidentifier,
+		Ukprn					int, 
+		Email					varchar(256),
+		Phone					varchar(50),
+		Website					varchar(500),
+		DateCreated				datetime,
+		Asdm_UpdatedDateTime	datetime2 default getdate()
+)

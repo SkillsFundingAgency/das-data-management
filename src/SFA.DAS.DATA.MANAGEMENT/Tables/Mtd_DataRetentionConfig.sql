fea4354511dd31fdc683c7d33eecba11d69f38b2
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-07-07 09:18:36 +0100

    Metadata Table for Retention Config

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Mtd_DataRetentionConfig.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [Mtd].[DataRetentionConfig]
+(
+  DRC_Id BIGINT Identity(1,1) PRIMARY KEY
+ ,SFCI_Id BIGINT
+ ,DataSetName VARCHAR(25)
+ ,DataSetTable VARCHAR(25)
+ ,SensitiveColumns VARCHAR(255)
+ ,PreImportRetention BIT
+ ,PostImportRetention BIT
+ ,IsActive BIT
+ ,CreatedDate DATETIME2 default(getdate())
+ ,UpdatedDate DATETIME2
+ )
+

6be2432c6f0a4e80af793a9e78b5cd09fc2b4e47
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-07-27 12:19:03 +0100

    Remove additional Primary key

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_aComt_Apprentice.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_aComt_Apprentice.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [AsData_PL].[aComt_Apprentice]
 (
-  [Id] uniqueidentifier not null Primary Key
+  [Id] uniqueidentifier not null 
  ,[CreatedOn] datetime2 not null
  ,CONSTRAINT PK_aComt_Apprentice_Id PRIMARY KEY CLUSTERED (Id)
 )

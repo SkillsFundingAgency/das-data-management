1d3553020d83ee0d2215a09d3a3f643ef432efeb
AD\SEVANI <sarma.evani@education.gov.uk>
2021-08-13 12:54:28 +0100

    Amendments to ApprenticeshipConfirmationStatus

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_ApprenticeshipConfirmationStatus.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_ApprenticeshipConfirmationStatus.sql
@@ -1,6 +1,5 @@
 ﻿CREATE TABLE [ASData_PL].[Comt_ApprenticeshipConfirmationStatus]
-(
-	[ApprenticeshipId] [bigint] PRIMARY KEY NOT NULL,
+(	[ApprenticeshipId] [bigint] PRIMARY KEY NOT NULL,
 	[ApprenticeshipConfirmedOn] [datetime2](7) NULL,
 	[CommitmentsApprovedOn] [datetime2](7) NULL,
 	[ConfirmationOverdueOn] [datetime2](7) NULL,

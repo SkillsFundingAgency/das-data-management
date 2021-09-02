628e2f779076dd0af0a6751941440a431fd0c6d1
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-09-02 15:12:03 +0100

    Change Insert Query

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateMetadataDataRetention.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateMetadataDataRetention.sql
@@ -52,10 +52,20 @@ VALUES (-1,'Marketo','MarketoLeads','AsData_PL','LeadID',24,'FirstName,LastName,
 INSERT INTO Mtd.DataRetentionConfig
 ( SFCI_Id,DataSetName,DataSetTable,DataSetSchema,DataSetJOINColumn,RetentionPeriodInMonths,SensitiveColumns,RetentionColumn,RefColumn,RefDataSetTable,RefDataSetSchema,PreImportRetention,PostImportRetention,IsActive)
 SELECT SCFI_Id,'Accounts','Acc_Account','AsData_PL','Id',84,'','CreatedDate','','','',0,1,1 FROM Mtd.sourceconfigforimport WHERE SourceDatabaseName='Accounts' and SourceTableName='Account'
+INSERT INTO Mtd.DataRetentionConfig
+( SFCI_Id,DataSetName,DataSetTable,DataSetSchema,DataSetJOINColumn,RetentionPeriodInMonths,SensitiveColumns,RetentionColumn,RefColumn,RefDataSetTable,RefDataSetSchema,PreImportRetention,PostImportRetention,IsActive)
 SELECT SCFI_Id,'Accounts','Acc_AccountUserRole','AsData_PL','AccountId',84,'','','Id','acc_account','AsData_PL',0,1,1  FROM Mtd.sourceconfigforimport  WHERE SourceDatabaseName='Accounts' and SourceTableName='Membership'
+INSERT INTO Mtd.DataRetentionConfig
+( SFCI_Id,DataSetName,DataSetTable,DataSetSchema,DataSetJOINColumn,RetentionPeriodInMonths,SensitiveColumns,RetentionColumn,RefColumn,RefDataSetTable,RefDataSetSchema,PreImportRetention,PostImportRetention,IsActive)
 SELECT SCFI_Id,'Accounts','Acc_User','AsData_PL','Id',84,'FirstName,LastName,Email','','UserId','acc_accountuserrole','AsData_PL',0,1,1  FROM Mtd.sourceconfigforimport  WHERE SourceDatabaseName='Accounts' and SourceTableName='User'
+INSERT INTO Mtd.DataRetentionConfig
+( SFCI_Id,DataSetName,DataSetTable,DataSetSchema,DataSetJOINColumn,RetentionPeriodInMonths,SensitiveColumns,RetentionColumn,RefColumn,RefDataSetTable,RefDataSetSchema,PreImportRetention,PostImportRetention,IsActive)
 SELECT SCFI_Id,'Users','EAU_User','AsData_PL','Id',84,'FirstName,LastName,Email','','UserRef','acc_user','AsData_PL',0,1,1  FROM Mtd.sourceconfigforimport  WHERE SourceDatabaseName='Users' and SourceTableName='User'
+INSERT INTO Mtd.DataRetentionConfig
+( SFCI_Id,DataSetName,DataSetTable,DataSetSchema,DataSetJOINColumn,RetentionPeriodInMonths,SensitiveColumns,RetentionColumn,RefColumn,RefDataSetTable,RefDataSetSchema,PreImportRetention,PostImportRetention,IsActive)
 SELECT SCFI_Id,'Accounts','Acc_AccountHistory','AsData_PL','AccountId',84,'PayeRef','','ID','acc_account','AsData_PL',0,1,1  FROM Mtd.sourceconfigforimport  WHERE SourceDatabaseName='Accounts' and SourceTableName='AccountHistory'
+INSERT INTO Mtd.DataRetentionConfig
+( SFCI_Id,DataSetName,DataSetTable,DataSetSchema,DataSetJOINColumn,RetentionPeriodInMonths,SensitiveColumns,RetentionColumn,RefColumn,RefDataSetTable,RefDataSetSchema,PreImportRetention,PostImportRetention,IsActive)
 SELECT SCFI_Id,'Accounts','Acc_Paye','AsData_PL','Name',84,'Ref','','Name','acc_account','AsData_PL',0,1,1  FROM Mtd.sourceconfigforimport WHERE SourceDatabaseName='Accounts' and SourceTableName='Paye'
 
 

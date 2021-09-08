4cbe251ef225de9c10992570fc64ee9729bfd5b2
AD\SEVANI <sarma.evani@education.gov.uk>
2021-09-08 15:39:48 +0100

    Added commitment new amendment

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_StandardFunding.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [AsData_PL].[Comt_StandardFunding]
+(
+	[Id]					[int]		NOT NULL,
+	[EffectiveFrom]			[datetime]	NULL,
+	[EffectiveTo]			[datetime]	NULL,
+	[FundingCap]			[int] NOT	NULL,
+	[AsDm_UpdatedDateTime] datetime2 default getdate()
+)

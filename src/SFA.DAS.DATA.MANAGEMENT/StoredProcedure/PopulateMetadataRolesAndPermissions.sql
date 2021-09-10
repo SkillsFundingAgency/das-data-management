3ac1369a620de186fe1cca0af8a25e731d30b871
AD\SEVANI <sarma.evani@education.gov.uk>
2021-09-10 10:10:42 +0100

    Removing Standard Funding

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateMetadataRolesAndPermissions.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateMetadataRolesAndPermissions.sql
@@ -86,7 +86,6 @@ VALUES
 ('BetaUser','ASData_PL','Comt_Commitment','TABLE','SELECT',0,1),
 ('BetaUser','ASData_PL','Comt_ApprenticeshipConfirmationStatus','TABLE','SELECT',0,1),
 ('BetaUser','ASData_PL','Comt_ApprenticeshipUpdate','TABLE','SELECT',0,1),
-('BetaUser','ASData_PL','Comt_StandardFunding','TABLE','SELECT',0,1),
 ('BetaUser','ASData_PL','Comt_StandardOption','TABLE','SELECT',0,1),
 ('BetaUser','ASData_PL','EI_ApprenticeshipBreakInLearning','TABLE','SELECT',0,1),
 ('BetaUser','ASData_PL','EI_ApprenticeshipDaysInLearning','TABLE','SELECT',0,1),

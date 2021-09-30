949ebfc91b5440862a9f504a47060811156200ec
AD\SEVANI <sarma.evani@education.gov.uk>
2021-09-30 11:20:13 +0100

    LTM changes , cleared out post deployment script

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -1,12 +1,3 @@
 ﻿/* Execute Stored Procedure */
 
 EXEC [dbo].[Build_AS_DataMart]
-
-
-DROP TABLE If Exists Stg.Comt_PledgeSector
-DROP TABLE If Exists [dbo].[ASDataPL_LTM_PledgeLocation]
-
-
-DROP TABLE If Exists [ASData_PL].[LTM_PledgeSector]
-DROP TABLE If Exists [ASData_PL].[LTM_PledgeRole]
-DROP TABLE If Exists [ASData_PL].[LTM_PledgeLevel]

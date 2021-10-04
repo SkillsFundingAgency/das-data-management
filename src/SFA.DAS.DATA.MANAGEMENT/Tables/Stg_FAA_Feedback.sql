7b37bfe380048b2196e0d641910a47842262f229
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2021-10-04 16:20:04 +0100

    Asintel 2090 import faa feedback (#240)
    * Feedback
    
    * FAA Feeback
    
    * fix alias name
    
    * Seperate proc for Importing Faa Feedback to PL
    
    * Add sp
    
    * Rename
    
    * correct feedback spelling

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FAA_Feedback.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Stg.[FAA_Feedback]
+(
+  SourseSK BIGINT IDENTITY(1,1) NOT NULL
+ ,BinaryId varchar(256)
+ ,UserId varchar(256)
+ ,TypeCode varchar(10)
+ ,DateCreatedTimeStamp varchar(256)
+ ,Enquiry varchar(max)
+ ,Details nvarchar(max)
+ ,RunId bigint  default(-1)
+ ,AsDm_CreatedDate datetime2 default(getdate()) 
+ ,AsDm_UpdatedDate datetime2 default(getdatE())
+)

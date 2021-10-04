ef6745585dff754ef002afc32cf3ad78479d6c74
AD\SEVANI <sarma.evani@education.gov.uk>
2021-10-04 16:15:37 +0100

    Appeal table schema change

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_RP_Appeal.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_RP_Appeal.sql
@@ -1,10 +1,22 @@
 ﻿CREATE TABLE [AsData_PL].[RP_Appeal]
 (
-	[Id]							[uniqueidentifier]					NOT NULL,
-	[OversightReviewId]				[uniqueidentifier]					NULL,
-	[Message]						[nvarchar](max)						NULL,
-	[UserId]						[nvarchar](256)						NULL,	
-	[Username]						[nvarchar](256)						NULL,	
-	[CreatedOn]						[datetime2](7)						NULL,
-	[AsDm_UpdatedDateTime]			[datetime2](7) default getdate()	NULL
+	[Id]										[uniqueidentifier]			NOT NULL,
+	[ApplicationId]								[uniqueidentifier]			NOT NULL,
+	[Status]									[nvarchar](30)				NOT NULL,
+	[HowFailedOnPolicyOrProcesses]				[nvarchar](max)				NULL,
+	[HowFailedOnEvidenceSubmitted]				[nvarchar](max)				NULL,
+	[AppealSubmittedDate]						[datetime2](7)				NULL,
+	[AppealDeterminedDate]						[datetime2](7)				NULL,
+	[InternalComments]							[nvarchar](max)				NULL,
+	[ExternalComments]							[nvarchar](max)				NULL,
+	[UserId]									[nvarchar](256)				NULL,
+	[UserName]									[nvarchar](256)				NULL,
+	[InProgressDate]							[datetime2](7)				NULL,
+	[InProgressUserId]							[nvarchar](256)				NULL,
+	[InProgressUserName]						[nvarchar](256)				NULL,
+	[InProgressInternalComments]				[nvarchar](max)				NULL,
+	[InProgressExternalComments]				[nvarchar](max)				NULL,
+	[CreatedOn]									[datetime2](7)				NOT NULL,
+	[UpdatedOn]									[datetime2](7)				NULL,
+	[AsDm_UpdatedDateTime]						[datetime2](7) default getdate()	NULL
 )

ea5aa40ca7a58b4ea89435f7d21adc513c152cd8
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-10-28 17:26:34 +0100

    Views for AI

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataAI_Das_Accounts.sql
@@ -0,0 +1,28 @@
+﻿CREATE VIEW [AsData_AI].[Das_Accounts]
+	AS 
+SELECT	   DISTINCT
+        	COALESCE(Account.ApprenticeshipEmployerType, -1)   AS A1
+           ,COALESCE(Account.CreatedDate, '9999-12-31')	       AS A2
+		   ,Account.Id                                         AS A3
+		   ,ISNULL(Ale.Id ,-1)                                 AS A4
+		   ,ISNULL(AH.PayeRef,'-1')                            AS A5
+		   ,CASE WHEN AEA.AccountLegalEntityId IS NOT NULL THEN 1
+		         ELSE 0
+			 END                                               AS A6
+		   ,CASE WHEN FAT.SenderAccountId is not null then 1
+		         ELSE 0
+			 END                                               AS A7	
+		   ,Account.AsDm_UpdatedDateTime                       AS A8			
+FROM		ASData_PL.Acc_Account Account 
+LEFT
+JOIN        ASData_PL.Acc_AccountLegalEntity ALE
+  ON        Account.Id=ALE.AccountId
+LEFT
+JOIN        (SELECT DISTINCT EA.AccountLegalEntityId FROM ASData_PL.Acc_EmployerAgreement EA WHERE StatusId=2 and TemplateId=(SELECT MAX(TemplateId) from ASData_PL.Acc_EmployerAgreement))	AEA
+  ON        ale.Id=AEA.AccountLegalEntityId
+LEFT
+JOIN        ASDATA_PL.Fin_AccountTransfers FAT
+  ON        FAT.SenderAccountId=Account.Id
+LEFT
+JOIN        ASData_PL.Acc_AccountHistory AH
+  ON        AH.AccountId=Account.Id

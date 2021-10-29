7d8ecbd2a87ac1857b370226821a092ee13337f1
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-10-29 10:06:17 +0100

    Alias TPR Field Names

--- src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataAI_DAS_TprOrgDetails.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataAI_DAS_TprOrgDetails.sql
@@ -1,18 +1,18 @@
 ﻿CREATE VIEW [AsData_AI].[DAS_TPROrgDetails]
 As
-SELECT [TPRUniqueId] 
-	  ,[AORN] 
-      ,[CompaniesHouseNumber] 
-      ,[EmpRef] 
-      ,[PostCode]
-      ,[SchemeStartDate] 
-      ,[SchemeEndDate] 
-      ,[SchemeEndDateCodeDesc] 
-      ,[EmployeeCountDateTaken]       
-      ,[LiveEmployeeCount] 
-      ,[RestartDate] 
-      ,[TradeClass] 
-      ,[TradeClassDescription] 
-      ,[Asdm_UpdatedDateTime]
+SELECT [TPRUniqueId]             AS A1
+	  ,[AORN]                    AS A2
+      ,[CompaniesHouseNumber]    AS A3
+      ,[EmpRef]                  AS A4
+      ,[PostCode]                AS A5
+      ,[SchemeStartDate]         AS A6 
+      ,[SchemeEndDate]           AS A7
+      ,[SchemeEndDateCodeDesc]   AS A8
+      ,[EmployeeCountDateTaken]  AS A9      
+      ,[LiveEmployeeCount]       AS A10
+      ,[RestartDate]             AS A11
+      ,[TradeClass]              AS A12
+      ,[TradeClassDescription]   AS A13 
+      ,[Asdm_UpdatedDateTime]    AS A14
   FROM [ASData_PL].[Tpr_OrgDetails] TOD
 

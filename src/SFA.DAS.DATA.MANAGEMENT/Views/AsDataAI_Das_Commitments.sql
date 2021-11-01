7b8a0092317315aed486824e1c31dc64e37336ee
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-11-01 15:00:21 +0000

    Rename Columns, change code for schemeenddate

--- src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataAI_Das_Commitments.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataAI_Das_Commitments.sql
@@ -1,32 +1,32 @@
 ﻿CREATE VIEW [AsData_AI].[DAS_Commitments]
 	AS 
 SELECT DISTINCT 
-       Apprenticeship.Id                                  AS A1								
-      ,COALESCE(Apprenticeship.CreatedOn, '9999-12-31') AS A2
-      ,COALESCE(Course.Level, -1)                         AS A3 
-      ,COALESCE(Apprenticeship.StartDate, '9999-12-31') AS A4 
-      ,COALESCE(Apprenticeship.TrainingType, -99999)      AS A5
+       Apprenticeship.Id                                  AS B1								
+      ,COALESCE(Apprenticeship.CreatedOn, '9999-12-31') AS B2
+      ,COALESCE(Course.Level, -1)                         AS B3 
+      ,COALESCE(Apprenticeship.StartDate, '9999-12-31') AS B4 
+      ,COALESCE(Apprenticeship.TrainingType, -99999)      AS B5
       ,case when trainingtype=1 then ao2.[ApprenticeshipOccupationId]	 
             else ao.[ApprenticeshipOccupationId]	 
-        end                                               AS A6
+        end                                               AS B6
       ,case when pay.amount>0 then 1 
             else 0 
-        end                                               AS A7
-      ,COALESCE(Apprenticeship.isapproved, -1)	          AS A8
-      ,COALESCE(Apprenticeship.Cost, '-99999')	      AS A9
-      ,Commitment.EmployerAccountId                       AS A10
-      ,convert(NVarchar(500),HASHBYTES('SHA2_512',LTRIM(RTRIM(CONCAT(Commitment.ProviderId , skl.SaltKey)))),2) AS A11
+        end                                               AS B7
+      ,COALESCE(Apprenticeship.isapproved, -1)	          AS B8
+      ,COALESCE(Apprenticeship.Cost, '-99999')	      AS B9
+      ,Commitment.EmployerAccountId                       AS B10
+      ,convert(NVarchar(500),HASHBYTES('SHA2_512',LTRIM(RTRIM(CONCAT(Commitment.ProviderId , skl.SaltKey)))),2) AS B11
       ,CASE WHEN COMMITMENT.[TransferSenderId]IS NOT NULL THEN 1 
             ELSE 0 
-        END                                               AS A12 
+        END                                               AS B12 
       ,CASE WHEN Apprenticeship.[DateOfBirth] IS NULL	THEN - 1
 		    WHEN DATEPART([M], [Apprenticeship].[DateOfBirth]) > DATEPART([M], [Apprenticeship].[StartDate])
 	     	  OR DATEPART([M], [Apprenticeship].[DateOfBirth]) = DATEPART([M], [Apprenticeship].[StartDate])
 			 AND DATEPART([DD], [Apprenticeship].[DateOfBirth]) > DATEPART([DD], [Apprenticeship].[StartDate])
 			THEN DATEDIFF(YEAR, [Apprenticeship].[DateOfBirth], [Apprenticeship].[StartDate]) - 1
 			ELSE DATEDIFF(YEAR, [Apprenticeship].[DateOfBirth], [Apprenticeship].[StartDate]) 
-		END                                              AS A13
-       ,Apprenticeship.AsDm_UpdatedDateTime              AS A14 
+		END                                              AS B13
+       ,Apprenticeship.AsDm_UpdatedDateTime              AS B14 
 --,COALESCE(ac.OverallGrade,''N/A'') AS OverallGrade	Would be included later if needed
 FROM		ASData_PL.Comt_Apprenticeship Apprenticeship
 LEFT

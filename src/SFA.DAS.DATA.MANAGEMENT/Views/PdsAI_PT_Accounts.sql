504e71b4b80db51eb80ac4aa636643d86be0d1eb
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-11-03 16:36:45 +0000

    Remove PayeRef and ALE from Accounts View, Remove EmpRef and add AccountId to TPR View

--- src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_Accounts.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_Accounts.sql
@@ -4,9 +4,7 @@ SELECT	   DISTINCT
         	COALESCE(Account.ApprenticeshipEmployerType, -1)   AS A1
            ,COALESCE(Account.CreatedDate, '9999-12-31')	       AS A2
 		   ,Account.Id                                         AS A3
-		   ,ISNULL(Ale.Id ,-1)                                 AS A4
-		   ,ISNULL(AH.PayeRef,'-1')                            AS A5
-		   ,CASE WHEN AEA.AccountLegalEntityId IS NOT NULL THEN 1
+		   ,CASE WHEN AEA.AccountId IS NOT NULL THEN 1
 		         ELSE 0
 			 END                                               AS A6
 		   ,CASE WHEN FAT.SenderAccountId is not null then 1
@@ -15,14 +13,12 @@ SELECT	   DISTINCT
 		   ,Account.AsDm_UpdatedDateTime                       AS A8			
 FROM		ASData_PL.Acc_Account Account 
 LEFT
-JOIN        ASData_PL.Acc_AccountLegalEntity ALE
-  ON        Account.Id=ALE.AccountId
-LEFT
-JOIN        (SELECT DISTINCT EA.AccountLegalEntityId FROM ASData_PL.Acc_EmployerAgreement EA WHERE StatusId=2 and TemplateId=(SELECT MAX(TemplateId) from ASData_PL.Acc_EmployerAgreement))	AEA
-  ON        ale.Id=AEA.AccountLegalEntityId
+JOIN        (SELECT distinct ale.accountid
+               from ASData_PL.Acc_AccountLegalEntity ALE
+               JOIN (SELECT DISTINCT EA.AccountLegalEntityId FROM ASData_PL.Acc_EmployerAgreement EA WHERE StatusId=2 and TemplateId=(SELECT MAX(TemplateId) from ASData_PL.Acc_EmployerAgreement))	AEA
+                 ON        ale.Id=AEA.AccountLegalEntityId) AEA
+  ON        AEA.AccountId=Account.Id
 LEFT
 JOIN        ASDATA_PL.Fin_AccountTransfers FAT
   ON        FAT.SenderAccountId=Account.Id
-LEFT
-JOIN        ASData_PL.Acc_AccountHistory AH
-  ON        AH.AccountId=Account.Id
+
--- src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_DAS_TprOrgDetails.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_DAS_TprOrgDetails.sql
@@ -1,21 +1,25 @@
 ﻿CREATE VIEW [Pds_AI].[PT_D]
 As
-SELECT [TPRUniqueId]             AS D1
-	  ,[AORN]                    AS D2
-      ,[CompaniesHouseNumber]    AS D3
-      ,[EmpRef]                  AS D4
-      ,[PostCode]                AS D5
-      ,[SchemeStartDate]         AS D6 
-      ,[SchemeEndDate]           AS D7
-      ,COALESCE(sedc.rn,-1)      AS D8
-      ,[EmployeeCountDateTaken]  AS D9      
-      ,[LiveEmployeeCount]       AS D10
-      ,[RestartDate]             AS D11
-      ,TOD.TradeClass            AS D12
-      ,[Asdm_UpdatedDateTime]    AS D14
+SELECT [TPRUniqueId]                 AS D1
+	  ,[AORN]                        AS D2
+      ,[CompaniesHouseNumber]        AS D3
+  --    ,[EmpRef]                  AS D4
+      ,[PostCode]                    AS D5
+      ,[SchemeStartDate]             AS D6 
+      ,[SchemeEndDate]               AS D7
+      ,COALESCE(sedc.rn,-1)          AS D8
+      ,[EmployeeCountDateTaken]      AS D9      
+      ,[LiveEmployeeCount]           AS D10
+      ,[RestartDate]                 AS D11
+      ,TOD.TradeClass                AS D12
+      ,TOD.[Asdm_UpdatedDateTime]    AS D14
+      ,COALESCE(AH.AccountId,-1)     AS D15
   FROM [ASData_PL].[Tpr_OrgDetails] TOD
   left
   join (SELECT  SchemeEndDateCodeDesc,rank() over (order by SchemeEndDateCodeDesc desc) rn
           FROM (select distinct SchemeEndDateCodeDesc from AsData_PL.TPR_OrgDetails) a) sedc
     on sedc.SchemeEndDateCodeDesc=tod.SchemeEndDateCodeDesc
+  left
+  join [ASData_PL].[Acc_AccountHistory] AH
+    ON AH.PayeRef=TOD.EmpRef
 

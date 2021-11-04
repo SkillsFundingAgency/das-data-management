55243db6912efb29289b00e690600eec5080151e
AD\SEVANI <sarma.evani@education.gov.uk>
2021-11-04 19:14:46 +0000

    Forecast DB amendments

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_FCast_Balance.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [AsData_PL].[Fcast_Balance]
+(
+	[EmployerAccountId]				[bigint] NOT NULL,
+	[Amount]						[decimal](18, 5) NOT NULL,
+	[TransferAllowance]				[decimal](18, 5) NOT NULL,
+	[RemainingTransferBalance]		[decimal](18, 5) NOT NULL,
+	[BalancePeriod]					[datetime] NOT NULL,
+	[ReceivedDate]					[datetime] NOT NULL,
+	[UnallocatedCompletionPayments] [decimal](18, 5) NOT NULL,
+	[Asdm_UpdatedDateTime]			[datetime2] default getdate()
+)

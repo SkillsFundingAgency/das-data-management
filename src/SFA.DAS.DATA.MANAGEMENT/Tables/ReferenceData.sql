871570b5700d7e94c1e085a9b23a912172c0b856
AD\SEVANI <sarma.evani@education.gov.uk>
2021-11-11 16:16:04 +0000

    ReferenceData amendments

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ReferenceData.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ReferenceData.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[ReferenceData]
 (
 	Id INT IDENTITY(1,1) NOT NULL
-   ,Category varchar(100)
-   ,FieldName varchar(100)
-   ,FieldValue varchar(20)
-   ,FieldDesc Varchar(255)
-   ,FieldDetailDesc Varchar(4000) DEFAULT 'N/A'
+   ,Category			varchar(100)
+   ,FieldName			varchar(100)
+   ,FieldValue			varchar(20)
+   ,FieldDesc			Varchar(255)
+   ,FieldDetailDesc		Varchar(4000) DEFAULT 'N/A'
    ,CONSTRAINT PK_ReferenceData_ID PRIMARY KEY (ID)
 )

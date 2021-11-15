d4d905d71362dcc181aeaa3bb2f81c253aaf5e43
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2021-11-15 09:22:09 +0000

    Commitments and Levy Declarations View

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_DAS_LevyDeclarations.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW [Pds_AI].[PT_H]
+	AS 
+SELECT DISTINCT
+      [EmpRef]                                  AS H1
+      ,CASE WHEN sum([LevyDeclaredInMonth]) >= 5000 THEN 10000
+	        ELSE 0
+		END                                     AS H2
+  FROM [ASData_PL].[Fin_GetLevyDeclarationAndTopUp]
+ WHERE payrollyear in ('19-20','20-21','21-22')
+   AND lastsubmission=1
+ GROUP BY
+ empref

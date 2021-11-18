dd713e66b7539d8cc087c503e1348c799a7ce65f
AD\SEVANI <sarma.evani@education.gov.uk>
2021-11-18 16:12:19 +0000

    ROATP changes

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_RP_FinancialReview.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [AsData_PL].[RP_FinancialReview]
+(
+	[Id]									[uniqueidentifier]			NOT NULL,
+	[ApplicationId]							[uniqueidentifier]			NOT NULL,
+	[Status]								[nvarchar](20)				NOT NULL,
+	[SelectedGrade]							[nvarchar](20)				NULL,
+	[FinancialDueDate]						[datetime2](7)				NULL,
+	[GradedBy]								[nvarchar](256)				NULL,
+	[GradedOn]								[datetime2](7)				NULL,
+	[Comments]								[nvarchar](max)				NULL,
+	[ExternalComments]						[nvarchar](max)				NULL,	
+	[ClarificationRequestedOn]				[datetime2](7)				NULL,
+	[ClarificationRequestedBy]				[nvarchar](256)				NULL,
+	[ClarificationResponse]					[nvarchar](max)				NULL,
+	[AsDm_UpdatedDateTime]					[datetime2](7)		default getdate()	NULL
+)

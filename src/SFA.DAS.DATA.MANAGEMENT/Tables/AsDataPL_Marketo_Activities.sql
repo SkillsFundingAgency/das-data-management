6527699d7d7a1d79bd8f4fa3dd58c89f383fb325
AD\SEVANI <sarma.evani@education.gov.uk>
2021-11-23 16:29:14 +0000

    AI Performance

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Marketo_Activities.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Marketo_Activities.sql
@@ -18,5 +18,5 @@ GO
 CREATE NONCLUSTERED INDEX [NCI_MA_CampaignId] ON [AsData_PL].[MarketoLeadActivities](CampaignId ASC)
 GO
 CREATE NONCLUSTERED INDEX [NCI_MA_MarketoGUID] ON [AsData_PL].[MarketoLeadActivities](MarketoGUID ASC)
-GO
-CREATE NONCLUSTERED INDEX [NCI_MarketoLeadActivities_ActivityTypeId] ON [ASData_PL].[MarketoLeadActivities] ([ActivityTypeId]) INCLUDE ([MarketoGUID],[LeadId],[CampaignId])
+--GO
+--CREATE NONCLUSTERED INDEX [NCI_MarketoLeadActivities_ActivityTypeId] ON [ASData_PL].[MarketoLeadActivities] ([ActivityTypeId]) INCLUDE ([MarketoGUID],[LeadId],[CampaignId])

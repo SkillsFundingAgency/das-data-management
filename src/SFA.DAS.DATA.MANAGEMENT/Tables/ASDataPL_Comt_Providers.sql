3f617d2f033803a7a2aa03635f5b1f49a20b6830
AD\SEVANI <sarma.evani@education.gov.uk>
2021-11-29 14:03:33 +0000

    Make name as null for Comt_Providers

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_Providers.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_Providers.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [ASData_PL].[Comt_Providers]
 (	
 	UKPRN		[bigint] NOT NULL,
-	Name		[nvarchar](100) NOT NULL,
+	Name		[nvarchar](100) NULL,
 	Created		[datetime2](7) NOT NULL,
 	Updated		[datetime2](7) NULL,
 	AsDm_UpdatedDateTime datetime2 default getdate(),

a0ac461d82e491833da7e46e2ea8d5eae748ac56
AD\SEVANI <sarma.evani@education.gov.uk>
2021-12-02 17:42:54 +0000

    Added DWH lookup tables for SSA and Geography locations

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/StgPmts_PstLookupGOR.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [StgPmts].[Pst_LookupGOR]
+(
+	[Pst_GOR]					[varchar](9) NULL,
+	[Pst_GOR_Gov_Off_Desc]		[varchar](30) NULL,
+	[Pst_GOR_Gov_Off_Desc2]		[varchar](15) NULL
+) ON [Primary]
+GO

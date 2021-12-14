cb27bc98c63d5c477dbf3c8a9a597cbf272cac78
AD\SEVANI <sarma.evani@education.gov.uk>
2021-12-14 17:46:35 +0000

    Moving RP lookup tables to Lookup schema

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -3,3 +3,13 @@
 EXEC [dbo].[Build_AS_DataMart]
 DROP TABLE IF EXISTS [ASData_PL].[RP_AppealUpload]
 
+DROP TABLE IF EXISTS [Mtd].[RP_LookupOversightReviewStatus]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupPageTitles]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupQuestionTitles]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupSectionTitles]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupSequenceTitles]
+

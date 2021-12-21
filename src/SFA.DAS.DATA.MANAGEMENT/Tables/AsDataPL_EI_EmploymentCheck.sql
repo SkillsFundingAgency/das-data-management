251a557f76c59c860db36c3c42df6c5f55c7b9a7
AD\SEVANI <sarma.evani@education.gov.uk>
2021-12-21 15:44:42 +0000

    Adding EI_EmploymentCheck

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_EI_EmploymentCheck.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [AsData_PL].[EI_EmploymentCheck]
+(
+	[Id]							[uniqueidentifier]	NOT NULL,
+	[ApprenticeshipIncentiveId]		[uniqueidentifier]	NOT NULL,
+	[CheckType]						[nvarchar](50)		NOT NULL,
+	[MinimumDate]					[datetime2](7)		NOT NULL,
+	[MaximumDate]					[datetime2](7)		NOT NULL,
+	[CorrelationId]					[uniqueidentifier]	NOT NULL,
+	[Result]						[bit]				NULL,
+	[CreatedDateTime]				[datetime2](7)		NOT NULL,
+	[UpdatedDateTime]				[datetime2](7)		NULL,
+	[ResultDateTime]				[datetime2](7)		NULL,
+	[AsDm_UpdatedDateTime]			[datetime2]			default getdate()
+) ON [PRIMARY]
+GO

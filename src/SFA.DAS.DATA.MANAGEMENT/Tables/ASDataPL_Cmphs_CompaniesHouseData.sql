b4300679c845be162b80f31ccd49228b6908645d
AD\SEVANI <sarma.evani@education.gov.uk>
2022-01-19 14:20:37 +0000

    Adding Companies House view

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Cmphs_CompaniesHouseData.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Cmphs_CompaniesHouseData.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [ASData_PL].[Cmphs_CompaniesHouseData]
 (	
-	[CompanyNumber]						[varchar](50)		NULL,	
+	[CompanyNumber]						[varchar](255)		NULL,	
 	[CompanyCategory]					[varchar](100)		NULL,
 	[CompanyStatus]						[varchar](100)		NULL,	
 	[SICCodeSicText_1]					[varchar](250)		NULL,

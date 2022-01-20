a9b2be1544339964cda643e66029271bb32263ae
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-01-20 13:41:54 +0000

    change to 8000 instead

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Marketo_Leads.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Marketo_Leads.sql
@@ -6,8 +6,8 @@
 ,EmailAddress nvarchar(255)
 ,LeadCreatedAt datetime2
 ,LeadUpdatedAt datetime2
-,EmployerHashedId  nvarchar(max)
-,ProviderId  nvarchar(max) NULL
+,EmployerHashedId  nvarchar(8000)
+,ProviderId  nvarchar(8000) NULL
 ,AsDm_CreatedDate Datetime2
 ,AsDm_UpdatedDate datetime2
 ,IsRetentionApplied bit DEFAULT (0)

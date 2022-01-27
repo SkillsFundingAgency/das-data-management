d5e57edde3e97b305a4c24aa42c8ba8dab818e12
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-01-27 16:53:59 +0000

    Company House Staging & Rejected table

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_CmphsDataFromBlob.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Stg].[CmphsDataFromBlob]
+(
+	[Id] BIGINT IDENTITY(1,1) NOT NULL PRIMARY KEY
+   ,[Name] nvarchar(255)
+   ,[Value] nvarchar(max)
+   ,[Unit] nvarchar(255)
+   ,[CHN] nvarchar(100)
+
+)

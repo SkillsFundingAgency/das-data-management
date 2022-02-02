2be68ba29331be2cd452d5db005e77a5b2a0cddc
AD\SEVANI <sarma.evani@education.gov.uk>
2022-02-02 15:49:56 +0000

    Added MergeOrganisations

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Assessor_MergeOrganisationStandardVersion.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [AsData_PL].[Assessor_MergeOrganisationStandardVersion]
+(
+	[Id]							[int]			NOT NULL,
+	[StandardUId]					[varchar](20)	NOT NULL,
+	[Version]						[nvarchar](20)	NULL,
+	[OrganisationStandardId]		[int]			NOT NULL,
+	[EffectiveFrom]					[datetime]		NULL,
+	[EffectiveTo]					[datetime]		NULL,
+	[DateVersionApproved]			[datetime]		NULL,
+	[Comments]						[nvarchar](500) NULL,
+	[Status]						[nvarchar](10)	NOT NULL,
+	[MergeOrganisationId]			[int]			NOT NULL,
+	[Replicates]					[nvarchar](6)	NOT NULL,
+	[AsDm_UpdatedDateTime] datetime2 default(getdate())
+)

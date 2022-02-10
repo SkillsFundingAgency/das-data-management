ee681cbc88625113111dcda6bd1bc036e1a85b2c
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-02-10 15:58:47 +0000

    Truncate vacancy tables

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -1,4 +1,9 @@
 ﻿/* Execute Stored Procedure */
 EXEC [dbo].[Build_AS_DataMart]
 
+TRUNCATE TABLE asdata_pl.va_Application
+
+TRUNCATE TABLE asdata_pl.va_apprenticeships
+
 TRUNCATE TABLE asdata_pl.va_vacancy
+

6acd4bcd44f0d883cc800ad9e4c832ecbf0bb07a
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2022-02-15 14:20:42 +0000

    Personal Qualities surface to PL, Clean up HTML Tags in Vacancy Descr… (#277)
    * Personal Qualities surface to PL, Clean up HTML Tags in Vacancy Description, Skills Required and Qualifications Required
    
    * Name foreign key
    
    * Remove named constraint
    
    * Add new column to the end of table instead of first to avoid re-building table at deployment
    
    * Remove Constraint name on Applications
    
    * Remove spaces
    
    * Remove quotes for line break
    
    * Replace with empty space
    
    * Add more tags for clean up
    
    * Remove Bull html tag

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Functions/Fn_RemoveHTMLTags.sql
@@ -0,0 +1,213 @@
+﻿CREATE FUNCTION [dbo].[Fn_RemoveHtmlTags] (@HTMLText VARCHAR(MAX))
+RETURNS VARCHAR(MAX) AS
+BEGIN
+ 
+    DECLARE    @Start INT
+    DECLARE    @End INT
+    DECLARE    @Length INT
+ 
+    SET @Start = CHARINDEX('&amp;', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '&')
+        SET @Start = CHARINDEX('&amp;', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+    SET @Start = CHARINDEX('&lt;', @HTMLText)
+    SET @End = @Start + 3
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0) 
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '<')
+        SET @Start = CHARINDEX('&lt;', @HTMLText)
+        SET @End = @Start + 3
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+    SET @Start = CHARINDEX('&gt;', @HTMLText)
+    SET @End = @Start + 3
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '>')
+        SET @Start = CHARINDEX('&gt;', @HTMLText)
+        SET @End = @Start + 3
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+    SET @Start = CHARINDEX('&amp;amp;', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '&')
+        SET @Start = CHARINDEX('&amp;amp;', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+
+    SET @Start = CHARINDEX('&nbsp;', @HTMLText)
+    SET @End = @Start + 5
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('&nbsp;', @HTMLText)
+        SET @End = @Start + 5
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+    
+    SET @Start = CHARINDEX('<br>', @HTMLText)
+    SET @End = @Start + 3
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('<br>', @HTMLText)
+        SET @End = @Start + 3
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+ 
+    
+    SET @Start = CHARINDEX('<br/>', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('<br/>', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+    
+    SET @Start = CHARINDEX('<br />', @HTMLText)
+    SET @End = @Start + 5
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('<br />', @HTMLText)
+        SET @End = @Start + 5
+        SET @Length = (@End - @Start) + 1
+    END
+
+    
+    SET @Start = CHARINDEX('<ul>', @HTMLText)
+    SET @End = @Start + 3
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('<ul>', @HTMLText)
+        SET @End = @Start + 3
+        SET @Length = (@End - @Start) + 1
+    END
+
+    SET @Start = CHARINDEX('<li>', @HTMLText)
+    SET @End = @Start + 3
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('<li>', @HTMLText)
+        SET @End = @Start + 3
+        SET @Length = (@End - @Start) + 1
+    END
+
+    SET @Start = CHARINDEX('</ul>', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('</ul>', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+
+    SET @Start = CHARINDEX('</li>', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '.')
+        SET @Start = CHARINDEX('</li>', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+
+    SET @Start = CHARINDEX('<p>', @HTMLText)
+    SET @End = @Start + 2
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('<p>', @HTMLText)
+        SET @End = @Start + 2
+        SET @Length = (@End - @Start) + 1
+    END
+
+    
+    SET @Start = CHARINDEX('</p>', @HTMLText)
+    SET @End = @Start + 3
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('</p>', @HTMLText)
+        SET @End = @Start + 3
+        SET @Length = (@End - @Start) + 1
+    END
+
+    SET @Start = CHARINDEX('&bull', @HTMLText)
+    SET @End = @Start + 4
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, ' ')
+        SET @Start = CHARINDEX('&bull', @HTMLText)
+        SET @End = @Start + 4
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+ 
+    
+    SET @Start = CHARINDEX('<', @HTMLText)
+    SET @End = CHARINDEX('>', @HTMLText, CHARINDEX('<', @HTMLText))
+    SET @Length = (@End - @Start) + 1
+ 
+    WHILE (@Start > 0 AND @End > 0 AND @Length > 0)
+    BEGIN
+        SET @HTMLText = STUFF(@HTMLText, @Start, @Length, '')
+        SET @Start = CHARINDEX('<', @HTMLText)
+        SET @End = CHARINDEX('>', @HTMLText, CHARINDEX('<', @HTMLText))
+        SET @Length = (@End - @Start) + 1
+    END
+ 
+ 
+    RETURN LTRIM(RTRIM(@HTMLText))
+END
+GO

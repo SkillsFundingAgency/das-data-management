720b9137cf688ade05faccc6424f1930288e51a6
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-02-15 16:56:11 +0000

    re-order columns to avoid re-building table

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Cmphs_CompaniesHouseData.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Cmphs_CompaniesHouseData.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [ASData_PL].[Cmphs_CompaniesHouseData]
-(	[ID]                                bigint              NULL,
-	[CompanyNumber]						[varchar](255)		NULL,	
+(	[CompanyNumber]						[varchar](255)		NULL,	
 	[CompanyCategory]					[varchar](100)		NULL,
 	[CompanyStatus]						[varchar](100)		NULL,	
 	[SICCodeSicText_1]					[varchar](250)		NULL,
-	[ImportDateTime]					[datetime] DEFAULT (getdate())
+	[ImportDateTime]					[datetime] DEFAULT (getdate()),
+	[ID]                                bigint              NULL
 ) ON [PRIMARY]

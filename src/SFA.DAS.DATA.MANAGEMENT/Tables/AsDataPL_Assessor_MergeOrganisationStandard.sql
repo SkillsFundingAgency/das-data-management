6bde5fcfea2c0bf65e5f4dddb6c65256fd2ed263
sarmaevanidfe <68238278+sarmaevanidfe@users.noreply.github.com>
2022-02-18 15:41:16 +0000

    Added MergeOrganisations (#290)
    * Added MergeOrganisations
    
    * Add master build
    
    Co-authored-by: uhimabindu <himabindu.uddaraju@digital.education.gov.uk>

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Assessor_MergeOrganisationStandard.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [AsData_PL].[Assessor_MergeOrganisationStandard]
+(
+	[Id]											[int]			NOT NULL,
+	[EndPointAssessorOrganisationId]				[nvarchar](12)	NOT NULL,
+	[StandardCode]									[int]			NOT NULL,
+	[EffectiveFrom]									[datetime]		NULL,
+	[EffectiveTo]									[datetime]		NULL,
+	[DateStandardApprovedOnRegister]				[datetime]		NULL,
+	[Comments]										[nvarchar](500) NULL,
+	[Status]										[nvarchar](10) NOT NULL,
+	[ContactId]										[uniqueidentifier] NULL,
+	[OrganisationStandardData]						[nvarchar](max) NULL,
+	[StandardReference]								[nvarchar](10) NULL,
+	[MergeOrganisationId]							[int] NOT NULL,
+	[OrganisationStandardId]						[int] NOT NULL,
+	[Replicates]									[nvarchar](6) NOT NULL,
+	[AsDm_UpdatedDateTime] datetime2 default(getdate())
+)
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Assessor_MergeOrganisationStandardDeliveryArea.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [AsData_PL].[Assessor_MergeOrganisationStandardDeliveryArea]
+(
+	[Id]									[int] NOT NULL,
+	[OrganisationStandardId]				[int] NOT NULL,
+	[DeliveryAreaId]						[int] NOT NULL,
+	[Comments]								[nvarchar](500) NULL,
+	[Status]								[nvarchar](10) NOT NULL,
+	[MergeOrganisationId]					[int] NOT NULL,
+	[OrganisationStandardDeliveryAreaId]	[int] NOT NULL,
+	[Replicates]							[nvarchar](6) NOT NULL,
+	[AsDm_UpdatedDateTime] datetime2 default(getdate())
+)

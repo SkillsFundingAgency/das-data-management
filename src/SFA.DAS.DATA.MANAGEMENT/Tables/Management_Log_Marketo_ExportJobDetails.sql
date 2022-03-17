b8fd0ed08f29bb988a6c2b0211bf1e8680f8cf35
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-03-17 14:29:45 +0000

    Marketo Log Table

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_Marketo_ExportJobDetails.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Mgmt].[Log_Marketo_ExportJobDetails] (
+    [LogId]          BIGINT      NOT NULL,
+    [RunId]            BIGINT         NOT NULL DEFAULT(-1),
+    [UserName]         VARCHAR (100) NULL,
+	[ADFTaskType] VARCHAR(256) NULL,
+    [LogMessage]     VARCHAR (MAX) NULL,
+    [LogDateTime]    DATETIME2 (7) default(Getdate()) NULL,
+    CONSTRAINT [FK_MLD_RunId] FOREIGN KEY ([RunId]) REFERENCES [Mgmt].[Log_RunId] ([RunId])
+);

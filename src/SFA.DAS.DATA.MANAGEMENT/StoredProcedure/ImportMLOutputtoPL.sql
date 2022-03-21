4aae9bf8f913848818e1c595bfb679eb9e0fc136
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-03-21 17:08:12 +0000

    ML Output to PL

---
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportMLOutputtoPL.sql
@@ -0,0 +1,104 @@
+﻿CREATE PROCEDURE [dbo].[ImportMLOutputToPL]
+(
+   @RunId int
+)
+AS
+-- ==========================================================================================================
+-- Author:      Himabindu Uddaraju
+-- Create Date: 21/03/2022
+-- Description: Import Machine Learning Output from Staging to Presentation Layer
+-- ==========================================================================================================
+
+BEGIN TRY
+
+		DECLARE @LogID int
+
+
+		/* Start Logging Execution */
+
+		  INSERT INTO Mgmt.Log_Execution_Results
+			  (
+				RunId
+			   ,StepNo
+			   ,StoredProcedureName
+			   ,StartDateTime
+			   ,Execution_Status
+			  )
+		  SELECT 
+				@RunId
+			   ,'Step-6'
+			   ,'ImportMLOutputToPL'
+			   ,getdate()
+			   ,0
+
+		  SELECT @LogID=MAX(LogId) FROM Mgmt.Log_Execution_Results
+		   WHERE StoredProcedureName='ImportMLOutputToPL'
+			 AND RunId=@RunID
+
+				INSERT INTO ASData_PL.ML_LevyModelPredictions
+				(     
+				  [AccountId]
+                 ,[LevyModelPredictions]
+				)
+				SELECT [AccountId]
+                      ,[LevyModelPredictions]
+				 FROM Stg.ML_LevyPredictions MLP
+				
+
+
+				 
+
+			
+
+			--	 EXEC SP_EXECUTESQL @VSQL
+
+
+
+		COMMIT TRANSACTION
+
+				UPDATE Mgmt.Log_Execution_Results
+				   SET Execution_Status=1
+					  ,EndDateTime=getdate()
+					  ,FullJobStatus='Pending'
+				 WHERE LogId=@LogID
+				   AND RunId=@RunId
+END TRY
+BEGIN CATCH
+			IF @@TRANCOUNT>0
+			ROLLBACK TRANSACTION;
+
+			DECLARE @ErrorId int
+
+		  INSERT INTO Mgmt.Log_Error_Details
+			  (UserName
+			  ,ErrorNumber
+			  ,ErrorState
+			  ,ErrorSeverity
+			  ,ErrorLine
+			  ,ErrorProcedure
+			  ,ErrorMessage
+			  ,ErrorDateTime
+			  ,RunId
+			  )
+		  SELECT 
+				SUSER_SNAME(),
+				ERROR_NUMBER(),
+				ERROR_STATE(),
+				ERROR_SEVERITY(),
+				ERROR_LINE(),
+				'ImportMLOutputToPL',
+				ERROR_MESSAGE(),
+				GETDATE(),
+				@RunId as RunId; 
+
+		  SELECT @ErrorId=MAX(ErrorId) FROM Mgmt.Log_Error_Details
+
+		/* Update Log Execution Results as Fail if there is an Error*/
+
+		UPDATE Mgmt.Log_Execution_Results
+		   SET Execution_Status=0
+			  ,EndDateTime=getdate()
+			  ,ErrorId=@ErrorId
+		 WHERE LogId=@LogID
+		   AND RunID=@RunId
+  END CATCH
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Ml_LevyModelPredictions.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE AsData_Pl.ML_LevyModelPredictions
+(
+    [Id] BIGINT identity(1,1) NOT NULL,
+	[AccountId] BIGINT,
+	[Levy_Model_Prediction] nvarchar(max),
+	[AsDm_UpdatedDateTime] datetime2(7) default(getdate())
+	,CONSTRAINT PK_ML_LevyPredictions PRIMARY KEY CLUSTERED (Id)
+)
+GO
+
+CREATE NONCLUSTERED INDEX [NCI_ML_LevyPredictions_AccountId] ON AsData_Pl.ML_LevyModelPredictions(AccountId)
+GO

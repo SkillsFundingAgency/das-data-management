378d1fa704c5fbf37def2f982b486b6c714aff46
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2022-03-22 14:38:21 +0000

    Look up changes to match Data Science given structure (#300)

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/lkp_LARSSectorSubjectAreaTier1.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/lkp_LARSSectorSubjectAreaTier1.sql
@@ -1,7 +1,13 @@
 ﻿CREATE TABLE [lkp].[LARS_SectorSubjectAreaTier1](
-	[SectorSubjectAreaTier1]			[decimal](5, 2) NOT NULL,
-	[SectorSubjectAreaTier1_Integer]	[varchar](5) NOT NULL,
-	[SectorSubjectAreaTier1_Desc]		[varchar](50) NULL,
-	[SectorSubjectAreaTier1_Desc2]		[varchar](30) NULL
+    [SectorSubjectAreaTier1] [decimal](5, 2) NOT NULL,
+	[SectorSubjectAreaTier1_Integer] [varchar](5) NOT NULL,
+	[SectorSubjectAreaTier1_Desc] [varchar](50) NULL,
+	[SectorSubjectAreaTier1_Desc2] [varchar](30) NULL,
+	[File_Code] [varchar](50) NULL,
+	[File_Alias] [varchar](50) NULL,
+	[LoadDate] [int] NULL,
+	[LoadID] [int] NULL,
+	[Load_SourceFile_Row] [bigint] NULL,
+	[AsDm_UpdatedDateTime] datetime2(7) default(getdate()) null
 ) ON [Primary]
 GO

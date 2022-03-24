24a2b348f7348734175b32aec4a8f6448098f5d7
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-03-24 23:09:06 +0000

    ML output file log table

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_ML_OutputFileLog.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [Mgmt].[ML_OutputFileLog]
+(
+	[Id] [bigint] IDENTITY(1,1) NOT NULL,
+	[RunId] bigint,
+	[LogId] bigint,
+	[BlobOutputFolderName] varchar(256),
+	[FileName] [varchar](100) ,
+	[FileModifiedDateTime] [datetime] DEFAULT(GETDATE()) NOT NULL,
+	CONSTRAINT [PK_ML_ID] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_ML_RunID] FOREIGN KEY ([RunId]) REFERENCES [Mgmt].[Log_RunId] ([RunId]),
+	CONSTRAINT [FK_ML_LogID] FOREIGN KEY ([LogId]) REFERENCES [Mgmt].[Log_Execution_Results] ([LogId])
+ )

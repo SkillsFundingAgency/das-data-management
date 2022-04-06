75b84316f96a6b5ab7bbafe1cefe92a7171dde35
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-04-06 16:49:25 +0100

    remove runid

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_PFBE_EmployerFeedback.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_PFBE_EmployerFeedback.sql
@@ -9,7 +9,6 @@
 	FeedbackName							[NVarchar](500)							NULL,
 	FeedbackValue							[Smallint]								NULL,
 	ProviderRating							[NVarchar](10)							NULL,
-	RunID									[BigInt]								NULL,
 	AsDm_CreatedDate						[datetime2](7)							DEFAULT (getdate()),
 	AsDm_UpdatedDate					    [datetime2](7)							DEFAULT (getdate())
 )

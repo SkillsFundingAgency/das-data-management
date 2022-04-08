7a3fcda521d8754608718a3fd4c0c3bae4c2e70b
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2022-04-08 15:30:28 +0100

    Data Analyst Tier 2 permissions to LTM Tables (#307)
    * Data Analyst Tier 2 permissions to LTM Tables
    
    * LTM Lookup tables added

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateMetadataRolesAndPermissions.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateMetadataRolesAndPermissions.sql
@@ -255,6 +255,15 @@ VALUES
 ('DataAnalystTier2','ASData_PL','EI_ClawbackPayment','TABLE','SELECT',0,1),
 ('DataAnalystTier2','ASData_PL','Va_Vacancy','TABLE','SELECT',0,1),
 ('DataAnalystTier2','ASData_PL','Va_Employer','TABLE','SELECT',0,1),
+/* LTM Tables */
+('DataAnalystTier2','ASData_PL','LTM_ApplicationEmailAddress','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_Application','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_Pledge','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_PledgeLocation','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_ApplicationStatusHistory','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_PledgeLevel','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_PledgeJobRole','TABLE','SELECT',0,1),
+('DataAnalystTier2','ASData_PL','LTM_PledgeSector','TABLE','SELECT',0,1),
 /*DWH Lookup tables*/
 ('DWHLkpUser','lkp','LARS_Framework','TABLE','ALTER',0,1),
 ('DWHLkpUser','lkp','LARS_Framework','TABLE','SELECT',0,1),

cbc4bc642708ae5a2fa3c03643e0f8e98397e23a
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-05-04 12:29:32 +0100

    candidate reg details

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FAA_CandidateRegDetails.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Stg].[FAA_CandidateRegDetails]
+(
+	[SourceSK] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY
+   ,[CandidateId] Varchar(256)
+   ,[LegacyCandidateId] BIGINT
+   ,[FirstName] varchar(255)
+   ,[MiddleName] varchar(255)
+   ,[LastName] varchar(255)
+   ,[RunID] BIGINT
+)

cd5dab40b84be86a3a33c2c07bccc2ecaa67cb69
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-05-10 15:09:18 +0100

    Apprenticeship Reg Details

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_ApprenticeshipRegDetails.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [ASData_PL].[Comt_ApprenticeshipRegDetails]
+(
+	[Id] [bigint] NOT NULL,
+	[CommitmentId] [bigint] NOT NULL,	
+    [FirstName] nvarchar(500) null,
+	[LastName] nvarchar(500) null,
+	[FullName] nvarchar(500) null,
+	[EmailAddress] nvarchar(500) null,
+	[AsDm_UpdatedDateTime] datetime2 default getdate()
+	CONSTRAINT PK_Comt_ApprenticeshipRegDetails_Id PRIMARY KEY CLUSTERED (Id)
+) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [NCI_Comt_RegDetails_CommitmentId] ON [AsData_PL].[Comt_Apprenticeship]([CommitmentId] ASC)
+GO
+

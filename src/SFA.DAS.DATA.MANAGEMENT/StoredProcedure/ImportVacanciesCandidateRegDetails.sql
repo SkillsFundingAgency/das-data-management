37c03393030940a0adc4424e75c16af81181fe49
uhimabindu <himabindu.uddaraju@digital.education.gov.uk>
2022-05-12 10:44:05 +0100

    Vacancy candidate reg details to PL

---
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacanciesCandidateRegDetails.sql
@@ -0,0 +1,144 @@
+﻿CREATE PROCEDURE [dbo].[ImportVacanciesCandidateRegDetailsToPL]
+(
+   @RunId int
+)
+AS
+-- ================================================================================================
+-- Author:      Himabindu Uddaraju
+-- Create Date: 12/05/2022
+-- Description: Import and Integrate Vacancies Candidate Reg Details from FAA Cosmos and AVMS Dbs
+-- ================================================================================================
+
+BEGIN TRY
+
+DECLARE @LogID int
+
+/* Start Logging Execution */
+
+  INSERT INTO Mgmt.Log_Execution_Results
+	  (
+	    RunId
+	   ,StepNo
+	   ,StoredProcedureName
+	   ,StartDateTime
+	   ,Execution_Status
+	  )
+  SELECT 
+        @RunId
+	   ,'Step-6'
+	   ,'ImportVacanciesCandidateRegDetailsToPL'
+	   ,getdate()
+	   ,0
+
+  SELECT @LogID=MAX(LogId) FROM Mgmt.Log_Execution_Results
+   WHERE StoredProcedureName='ImportVacanciesCandidateRegDetailsToPL'
+     AND RunId=@RunID
+
+
+BEGIN TRANSACTION
+
+
+DELETE FROM ASData_PL.Va_CandidateRegDetails
+
+
+INSERT INTO [ASData_PL].[Va_CandidateRegDetails]
+           (
+		    [CandidateId] 
+           ,[CandidateFirstName]
+		   ,[CandidateLastName]
+		   ,[CandidateMiddleName]
+		   ,[CandidateFullName]
+		   ,[CandidateEmail]
+           ,[SourceDb] 
+		   )
+SELECT      VC.[CandidateId] 
+           ,FCD.[FirstName]
+		   ,FCD.[LastName]
+		   ,FCD.[MiddleName]
+		   ,FCD.[FullName]
+		   ,FCD.[EmailAddress]
+     	   ,'FAA-Cosmos'
+  FROM Stg.FAA_CandidateRegDetails FCD
+  JOIN ASData_PL.Va_Candidate VC
+    ON VC.SourceCandidateId_v2=FCD.CandidateId
+
+
+INSERT INTO [ASData_PL].[Va_CandidateRegDetails]
+           (
+		    [CandidateId] 
+           ,[CandidateFirstName]
+		   ,[CandidateLastName]
+		   ,[CandidateMiddleName]
+		   ,[CandidateFullName]
+		   ,[CandidateEmail]
+           ,[SourceDb] 
+		   )
+SELECT      VC.[CandidateId] 
+           ,ACD.[FirstName]
+		   ,ACD.[LastName]
+		   ,ACD.[MiddleName]
+		   ,ACD.[FullName]
+		   ,ACD.[EmailAddress]
+	       ,'FAA-Avms'
+  FROM Stg.Avms_CandidateRegDetails ACD
+  JOIN ASData_PL.Va_Candidate VC
+    ON VC.SourceCandidateId_v1=ACD.CandidateId
+ WHERE NOT EXISTS (SELECT 1 FROM stg.FAA_CandidateRegDetails where CandidateId=vc.SourceCandidateId_v2)
+
+   
+
+     
+COMMIT TRANSACTION
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=1
+      ,EndDateTime=getdate()
+	  ,FullJobStatus='Pending'
+ WHERE LogId=@LogID
+   AND RunId=@RunId
+
+
+ 
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT>0
+	ROLLBACK TRANSACTION;
+
+    DECLARE @ErrorId int
+
+  INSERT INTO Mgmt.Log_Error_Details
+	  (UserName
+	  ,ErrorNumber
+	  ,ErrorState
+	  ,ErrorSeverity
+	  ,ErrorLine
+	  ,ErrorProcedure
+	  ,ErrorMessage
+	  ,ErrorDateTime
+	  ,RunId
+	  )
+  SELECT 
+        SUSER_SNAME(),
+	    ERROR_NUMBER(),
+	    ERROR_STATE(),
+	    ERROR_SEVERITY(),
+	    ERROR_LINE(),
+	    'ImportVacanciesCandidateRegDetailsToPL',
+	    ERROR_MESSAGE(),
+	    GETDATE(),
+		@RunId as RunId; 
+
+  SELECT @ErrorId=MAX(ErrorId) FROM Mgmt.Log_Error_Details
+
+/* Update Log Execution Results as Fail if there is an Error*/
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=0
+      ,EndDateTime=getdate()
+	  ,ErrorId=@ErrorId
+ WHERE LogId=@LogID
+   AND RunID=@RunId
+
+  END CATCH
+
+GO
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_CandidateRegDetails.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [AsData_PL].[Va_CandidateRegDetails]
+(
+	[CandidateRegDetailsId] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY
+   ,[CandidateId] BIGINT NOT NULL
+   ,[CandidateFirstName] nvarchar(512) --MASKED WITH (FUNCTION = 'default()')
+   ,[CandidateLastName] nvarchar(512) --MASKED WITH (FUNCTION = 'default()')
+   ,[CandidateMiddleName] nvarchar(512)
+   ,[CandidateFullName] nvarchar(512)
+   ,[CandidateEmail] nvarchar(512)
+   ,[SourceDb] Varchar(256)
+   ,[AsDm_UpdatedDateTime]  [datetime2](7)						DEFAULT (getdate())
+)

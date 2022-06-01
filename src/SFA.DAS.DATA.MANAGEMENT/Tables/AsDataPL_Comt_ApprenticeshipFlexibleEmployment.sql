f62e6da6923af7ac591bf53a016e568b44ca5a56
RaviPolavarapu <ravi.polavarapu@education.gov.uk>
2022-06-01 11:59:54 +0100

    Add Adm- UpdatedDatetime to Comt_ApprenticeshipFlexibleEmployment

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_ApprenticeshipFlexibleEmployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_ApprenticeshipFlexibleEmployment.sql
@@ -1,9 +1,9 @@
 CREATE TABLE [ASData_PL].[Comt_ApprenticeshipFlexibleEmployment](
-	[ApprenticeshipId] [bigint] NOT NULL,
-	[EmploymentPrice] [int] NULL,
-	[EmploymentEndDate] [datetime2](7) NULL,
+    [ApprenticeshipId] [bigint] NOT NULL,
+    [EmploymentPrice] [int] NULL,
+    [EmploymentEndDate] [datetime2](7) NULL,
+    [AsDm_UpdatedDateTime]  [datetime2](7)						DEFAULT (getdate()),
     CONSTRAINT [PK_Comt_ApprenticeshipFlexibleEmployment_Id] PRIMARY KEY CLUSTERED (ApprenticeshipId)
 ) ON [PRIMARY]
-GO
 
 
--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Comt_ApprenticeshipFlexibleEmployment.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [ASData_PL].[Comt_ApprenticeshipFlexibleEmployment]
-(
-	[ApprenticeshipId] [bigint] NOT NULL,
-	[EmploymentPrice] [int] NULL,
-	[EmploymentEndDate] [datetime2](7) NULL,
-	CONSTRAINT [PK_Comt_ApprenticeshipFlexibleEmployment_Id]  PRIMARY KEY CLUSTERED (ApprenticeshipId)
-) ON [PRIMARY]
-GO

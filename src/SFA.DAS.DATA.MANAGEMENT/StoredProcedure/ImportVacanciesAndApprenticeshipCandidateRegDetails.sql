de0bf5d5afb63629e7246f208692dd789925fa81
uhimabindu <44999719+uhimabindu@users.noreply.github.com>
2022-06-15 15:11:26 +0100

    Asintel 2431 learner xref from avms (#309)
    * Candidate Personal Info
    
    * Correct Avms
    
    * correct avms config
    
    * correct query
    
    * Format the fields to get them consistent between avms, faa and commitments
    
    * candidate reg details
    
    * correct config
    
    * correct config
    
    * Candidate Reg Details
    
    * Full Name on FAA Staging table
    
    * Add Full Name in AVMS Import
    
    * Add MiddleName in columns to Include list
    
    * Add Email Address
    
    * change from date to varchar
    
    * Apprenticeship Reg Details
    
    * correct syntax error
    
    * correct apprenticeshipid
    
    * Vacancy candidate reg details to PL
    
    * Correct Lastname in Surname
    
    * Add Candidate Date of Birth to commitments, rename commitments table
    
    * correct syntax error
    
    * Clear staging tables after load to PL
    
    * change the staging table to truncate name
    
    * convert to date not varchar
    
    * Update PL for both Vacancies and Candidate in one proc so that they are consistent after hashing
    
    * Delete before insert

---
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacanciesAndApprenticeshipCandidateRegDetails.sql
@@ -0,0 +1,194 @@
+﻿CREATE PROCEDURE [dbo].[ImportVacanciesAndApprenticeshipCandidateRegDetailsToPL]
+(
+   @RunId int
+)
+AS
+-- ======================================================================================================================
+-- Author:      Himabindu Uddaraju
+-- Create Date: 12/05/2022
+-- Description: Import and Integrate Vacancies and Apprenticeship Candidate Reg Details from FAA Cosmos and AVMS Dbs
+-- ======================================================================================================================
+
+BEGIN TRY
+
+DECLARE @LogID int
+
+/* Start Logging Execution */
+
+  INSERT INTO Mgmt.Log_Execution_Results
+	  (
+	    RunId
+	   ,StepNo
+	   ,StoredProcedureName
+	   ,StartDateTime
+	   ,Execution_Status
+	  )
+  SELECT 
+        @RunId
+	   ,'Step-6'
+	   ,'ImportVacanciesCandidateRegDetailsToPL'
+	   ,getdate()
+	   ,0
+
+  SELECT @LogID=MAX(LogId) FROM Mgmt.Log_Execution_Results
+   WHERE StoredProcedureName='ImportVacanciesCandidateRegDetailsToPL'
+     AND RunId=@RunID
+
+
+BEGIN TRANSACTION
+
+/* Import Vacancies Candidate Reg Details */
+
+DELETE FROM ASData_PL.Va_CandidateRegDetails
+
+
+INSERT INTO [ASData_PL].[Va_CandidateRegDetails]
+           (
+		    [CandidateId] 
+           ,[CandidateFirstName]
+		   ,[CandidateLastName]
+		   ,[CandidateMiddleName]
+		   ,[CandidateFullName]
+		   ,[CandidateDateOfBirth]
+		   ,[CandidateEmail]
+           ,[SourceDb] 
+		   )
+SELECT      VC.[CandidateId] 
+           ,FCD.[FirstName]
+		   ,FCD.[LastName]
+		   ,FCD.[MiddleName]
+		   ,FCD.[FullName]
+		   ,FCD.[DateOfBirth]
+		   ,FCD.[EmailAddress]
+     	   ,'FAA-Cosmos'
+  FROM Stg.FAA_CandidateRegDetails FCD
+  JOIN ASData_PL.Va_Candidate VC
+    ON VC.SourceCandidateId_v2=FCD.CandidateId
+
+
+
+INSERT INTO [ASData_PL].[Va_CandidateRegDetails]
+           (
+		    [CandidateId] 
+           ,[CandidateFirstName]
+		   ,[CandidateLastName]
+		   ,[CandidateMiddleName]
+		   ,[CandidateFullName]
+		   ,[CandidateDateOfBirth]
+		   ,[CandidateEmail]
+           ,[SourceDb] 
+		   )
+SELECT      VC.[CandidateId] 
+           ,ACD.[FirstName]
+		   ,ACD.[SurName]
+		   ,ACD.[MiddleName]
+		   ,ACD.[FullName]
+		   ,ACD.[DateOfBirth]
+		   ,ACD.[EmailAddress]
+	       ,'FAA-Avms'
+  FROM Stg.Avms_CandidateRegDetails ACD
+  JOIN ASData_PL.Va_Candidate VC
+    ON VC.SourceCandidateId_v1=ACD.CandidateId
+ WHERE NOT EXISTS (SELECT 1 FROM stg.FAA_CandidateRegDetails where CandidateId=vc.SourceCandidateId_v2)
+
+ /* Import Commitments Candidate Reg Details */
+
+ DELETE FROM [ASData_PL].[Comt_ApprenticeshipCandidateRegDetails]
+
+ INSERT INTO [ASData_PL].[Comt_ApprenticeshipCandidateRegDetails]
+           ([ApprenticeshipId]
+           ,[CommitmentId]
+           ,[CandidateFirstName]
+           ,[CandidateLastName]
+           ,[CandidateFullName]
+           ,[CandidateDateOfBirth]
+           ,[CandidateEmail]
+		   )
+SELECT [ApprenticeshipId]
+           ,[CommitmentId]
+           ,[CandidateFirstName]
+           ,[CandidateLastName]
+           ,[CandidateFullName]
+           ,[CandidateDateOfBirth]
+           ,[CandidateEmail]
+  FROM Stg.Comt_ApprenticeshipCandidateRegDetails
+
+       
+COMMIT TRANSACTION
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=1
+      ,EndDateTime=getdate()
+	  ,FullJobStatus='Pending'
+ WHERE LogId=@LogID
+   AND RunId=@RunId
+
+/* Truncate staging tables after loading to PL */
+
+ IF  EXISTS (select * from INFORMATION_SCHEMA.TABLES  where table_name ='Avms_CandidateRegDetails' AND TABLE_SCHEMA='Stg' AND TABLE_TYPE='BASE TABLE')
+		       DROP TABLE [Stg].[Avms_CandidateRegDetails]
+
+
+ IF  EXISTS (select * from INFORMATION_SCHEMA.TABLES  where table_name ='Comt_ApprenticeshipCandidateRegDetails' AND TABLE_SCHEMA='Stg' AND TABLE_TYPE='BASE TABLE')
+		       DROP TABLE [Stg].[Comt_ApprenticeshipCandidateRegDetails]
+
+
+TRUNCATE TABLE [Stg].[Faa_CandidateRegDetails]
+
+
+ 
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT>0
+	ROLLBACK TRANSACTION;
+
+    DECLARE @ErrorId int
+
+  INSERT INTO Mgmt.Log_Error_Details
+	  (UserName
+	  ,ErrorNumber
+	  ,ErrorState
+	  ,ErrorSeverity
+	  ,ErrorLine
+	  ,ErrorProcedure
+	  ,ErrorMessage
+	  ,ErrorDateTime
+	  ,RunId
+	  )
+  SELECT 
+        SUSER_SNAME(),
+	    ERROR_NUMBER(),
+	    ERROR_STATE(),
+	    ERROR_SEVERITY(),
+	    ERROR_LINE(),
+	    'ImportVacanciesCandidateRegDetailsToPL',
+	    ERROR_MESSAGE(),
+	    GETDATE(),
+		@RunId as RunId; 
+
+  SELECT @ErrorId=MAX(ErrorId) FROM Mgmt.Log_Error_Details
+
+/* Update Log Execution Results as Fail if there is an Error*/
+
+UPDATE Mgmt.Log_Execution_Results
+   SET Execution_Status=0
+      ,EndDateTime=getdate()
+	  ,ErrorId=@ErrorId
+ WHERE LogId=@LogID
+   AND RunID=@RunId
+
+/* Truncate staging tables even if it fails */
+
+ IF  EXISTS (select * from INFORMATION_SCHEMA.TABLES  where table_name ='Avms_CandidateRegDetails' AND TABLE_SCHEMA='Stg' AND TABLE_TYPE='BASE TABLE')
+		       DROP TABLE [Stg].[Avms_CandidateRegDetails]
+
+
+ IF  EXISTS (select * from INFORMATION_SCHEMA.TABLES  where table_name ='Comt_ApprenticeshipCandidateRegDetails' AND TABLE_SCHEMA='Stg' AND TABLE_TYPE='BASE TABLE')
+		       DROP TABLE [Stg].[Comt_ApprenticeshipCandidateRegDetails]
+
+
+TRUNCATE TABLE [Stg].[Faa_CandidateRegDetails]
+
+  END CATCH
+
+GO
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_ApprenticeshipCandidateRegDetails.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [ASData_PL].[Comt_ApprenticeshipCandidateRegDetails]
+(
+	[ApprenticeshipId] [bigint] NOT NULL,
+	[CommitmentId] [bigint] NOT NULL,	
+    [CandidateFirstName] nvarchar(500) null,
+	[CandidateLastName] nvarchar(500) null,
+	[CandidateFullName] nvarchar(500) null,
+	[CandidateDateOfBirth] nvarchar(500) null,
+	[CandidateEmail] nvarchar(500) null,
+	[AsDm_UpdatedDateTime] datetime2 default getdate()
+	CONSTRAINT PK_Comt_ApprenticeshipCandidateRegDetails_Id PRIMARY KEY CLUSTERED (ApprenticeshipId)
+) ON [PRIMARY]
+GO
+CREATE NONCLUSTERED INDEX [NCI_Comt_RegDetails_CommitmentId] ON [AsData_PL].[Comt_ApprenticeshipCandidateRegDetails]([CommitmentId] ASC)
+GO
+

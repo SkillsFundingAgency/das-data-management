4b3f04a33c48bcb56d70b9dbb7d7d9cccc713f2f
RaviPolavarapu <ravi.polavarapu@education.gov.uk>
2022-07-28 10:41:18 +0100

    Change view to get data from payments

--- src/SFA.DAS.DATA.MANAGEMENT/Views/DataPub_DAS_Employer_Account_Transfers_V2.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/DataPub_DAS_Employer_Account_Transfers_V2.sql
@@ -1,31 +1,20 @@
 ﻿CREATE VIEW [Data_Pub].[DAS_Employer_Account_Transfers_V2]	
 AS 
-	SELECT 
-     ISNULL(CAST(AT.Id AS bigint),-1)									As TransferId
-	 ,ISNULL(CAST(p.TransferSenderAccountId AS bigint),-1)						As SenderAccountId
-	, ISNULL(CAST(p.AccountId AS bigint),-1)					As ReceiverAccountId
-	, ISNULL(p.EventId,'00000000-0000-0000-0000-000000000000')		As RequiredPaymentId
-	, ISNULL(CAST(A.ID AS bigint),-1)									As CommitmentId
-	, CAST(p.Amount as decimal(18,5))									As Amount
-	, ISNULL(CAST(case when p.ApprenticeshipEmployerType = 1 then 'Levy' else 'Non-Levy' end as nvarchar(50)),'NA')						As Type
-	, ISNULL(CAST (p.PeriodEnd AS NVARCHAR(10)),'XXXX')					As CollectionPeriodName
-	, ISNULL(CAST(p.CreationDate AS datetime) ,'9999-12-31')  							As UpdateDateTime
-	FROM
-	(
-		 Select
-		 TransferSenderAccountId,
-		 AccountId,
-		 EventId,
-		 Amount,
-		 ApprenticeshipEmployerType,
-		 Cast( sp.AcademicYear AS varchar) + '-R' + RIGHT( '0' + Cast (sp.CollectionPeriod AS varchar),2)  As PeriodEnd,
-		 ApprenticeshipId, 
-		 CreationDate		 
-		 from [StgPmts].[Payment] As sp  
-		 WHERE sp.Fundingsource = 5 
-	) AS p
-	INNER JOIN [ASData_PL].[Comt_Apprenticeship] A 
-	ON p.ApprenticeshipId = A.ID
-	Left join [ASData_PL].[Fin_AccountTransfers] AT
-	ON AT.RequiredPaymentId = p.EventId
+	SELECT
+      MAX( ISNULL(CAST(AT.Id AS bigint),-1) )                                                As TransferId
+    , ISNULL(CAST(AT.TransferSenderAccountId AS bigint),-1)                                  As SenderAccountId
+    , ISNULL(CAST(AT.AccountId AS bigint),-1)                                                As ReceiverAccountId
+    , MAX( ISNULL(at.eventid,'00000000-0000-0000-0000-000000000000') )                       As RequiredPaymentId
+    , ISNULL(CAST(ApprenticeshipId AS bigint),-1)                                            As CommitmentId
+    , SUM(CAST(at.Amount as decimal(18,5)))                                                  As Amount
+    , ISNULL(CAST(at.ApprenticeshipEmployerType as nvarchar(50)),'NA')                       As Type
+    , ISNULL(CAST (at.PeriodEnd AS NVARCHAR(10)),'XXXX')                                     As CollectionPeriodName
+    , MAX(ISNULL(at.eventtime,'9999-12-31'))                                                 As UpdateDateTime
+    FROM (SELECT *,  Cast( sp.AcademicYear AS varchar) + '-R' + RIGHT( '0' + Cast (sp.CollectionPeriod AS varchar),2)  As PeriodEnd
+                FROM stgpmts.Payment sp
+                WHERE FundingSource=5) AT
+	GROUP BY AT.TransferSenderAccountId
+            ,AT.AccountId
+            ,AT.ApprenticeshipId
+            ,AT.PeriodEnd,AT.ApprenticeshipEmployerType
 GO

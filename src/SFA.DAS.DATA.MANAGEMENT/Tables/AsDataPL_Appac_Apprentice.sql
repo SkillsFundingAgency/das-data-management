dbe2a9d2623d8a7d04c08a7b6fcc8e15481a332d
Karthik <karthikeyan.kannan@education.gov.uk>
2022-08-22 13:18:16 +0100

    Removed unused table

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Appac_Apprentice.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [AsData_PL].[Appacc_Apprentice]
-(
-  [Id] uniqueidentifier not null 
- ,[CreatedOn] datetime2 not null
- ,[Asdm_UpdatedDateTime] datetime2 default getdate()
- ,CONSTRAINT PK_Appac_Apprentice_Id PRIMARY KEY CLUSTERED (Id)
-)
-

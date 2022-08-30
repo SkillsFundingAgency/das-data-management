f1fa8c58b354138b2d63dcb427ea865e71042379
Karthik <karthikeyan.kannan@education.gov.uk>
2022-08-30 09:34:58 +0100

    Updated data types to match extract ssis packages used by data science team

--- src/SFA.DAS.DATA.MANAGEMENT/Views/DataPub_DAS_Employer_Account_Transfers_V2.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/DataPub_DAS_Employer_Account_Transfers_V2.sql
@@ -1,15 +1,15 @@
 ﻿CREATE VIEW [Data_Pub].[DAS_Employer_Account_Transfers_V2]	
 AS 
-	SELECT
-      MAX( ISNULL(CAST(AT.Id AS bigint),-1) )                                                As TransferId
-    , ISNULL(CAST(AT.TransferSenderAccountId AS bigint),-1)                                  As SenderAccountId
-    , ISNULL(CAST(AT.AccountId AS bigint),-1)                                                As ReceiverAccountId
-    , MAX( ISNULL(at.eventid,'00000000-0000-0000-0000-000000000000') )                       As RequiredPaymentId
-    , ISNULL(CAST(ApprenticeshipId AS bigint),-1)                                            As CommitmentId
-    , SUM(CAST(at.Amount as decimal(18,5)))                                                  As Amount
-    , 'Levy'                                                                                 As Type
-    , ISNULL(CAST (at.PeriodEnd AS NVARCHAR(10)),'XXXX')                                     As CollectionPeriodName
-    , MAX(ISNULL(at.eventtime,'9999-12-31'))                                                 As UpdateDateTime
+SELECT
+      CAST(MAX( ISNULL(AT.Id ,-1) ) AS bigint)                                              As TransferId
+    , CAST(ISNULL(AT.TransferSenderAccountId ,-1)  AS bigint  )                             As SenderAccountId
+    , CAST(ISNULL(AT.AccountId ,-1) AS bigint)                                              As ReceiverAccountId
+    , CAST(MAX( ISNULL(at.eventid,'00000000-0000-0000-0000-000000000000') ) as uniqueidentifier)                      As RequiredPaymentId
+    , CAST(ISNULL(ApprenticeshipId ,-1) AS bigint)                                          As CommitmentId
+    , CAST(SUM(at.Amount ) as decimal(18,5))                                                As Amount
+    , CAST('Levy' as nvarchar(50))                                                          As Type
+    , CAST(ISNULL(at.PeriodEnd ,'XXXX') AS NVARCHAR(10))                                    As CollectionPeriodName
+    , CAST(MAX(ISNULL(at.eventtime,'9999-12-31'))  as datetime)                             As UpdateDateTime
     FROM (SELECT *,  Cast( sp.AcademicYear AS varchar) + '-R' + RIGHT( '0' + Cast (sp.CollectionPeriod AS varchar),2)  As PeriodEnd
                 FROM stgpmts.Payment sp
                 WHERE FundingSource=5) AT

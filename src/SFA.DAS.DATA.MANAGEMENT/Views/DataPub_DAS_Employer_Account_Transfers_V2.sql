dffd7b00ff9013ad72ab1317a523dbdecdd541a1
Karthik <karthikeyan.kannan@education.gov.uk>
2022-08-31 17:42:39 +0100

    Removed aggregations/group by

--- src/SFA.DAS.DATA.MANAGEMENT/Views/DataPub_DAS_Employer_Account_Transfers_V2.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/DataPub_DAS_Employer_Account_Transfers_V2.sql
@@ -1,20 +1,16 @@
 ﻿CREATE VIEW [Data_Pub].[DAS_Employer_Account_Transfers_V2]	
 AS 
 SELECT
-      CAST(MAX( ISNULL(AT.Id ,-1) ) AS bigint)                                              As TransferId
+      CAST(ISNULL(AT.Id ,-1)  AS bigint)                                              As TransferId
     , CAST(ISNULL(AT.TransferSenderAccountId ,-1)  AS bigint  )                             As SenderAccountId
     , CAST(ISNULL(AT.AccountId ,-1) AS bigint)                                              As ReceiverAccountId
-    , CAST(MAX( ISNULL(at.eventid,'00000000-0000-0000-0000-000000000000') ) as uniqueidentifier)                      As RequiredPaymentId
+    , CAST(ISNULL(at.eventid,'00000000-0000-0000-0000-000000000000')  as uniqueidentifier)                      As RequiredPaymentId
     , CAST(ISNULL(ApprenticeshipId ,-1) AS bigint)                                          As CommitmentId
-    , CAST(SUM(at.Amount ) as decimal(18,5))                                                As Amount
+    , CAST(at.Amount  as decimal(18,5))                                                As Amount
     , CAST('Levy' as nvarchar(50))                                                          As Type
     , CAST(ISNULL(at.PeriodEnd ,'XXXX') AS NVARCHAR(10))                                    As CollectionPeriodName
-    , CAST(MAX(ISNULL(at.eventtime,'9999-12-31'))  as datetime)                             As UpdateDateTime
+    , CAST(ISNULL(at.eventtime,'9999-12-31')  as datetime)                             As UpdateDateTime
     FROM (SELECT *,  Cast( sp.AcademicYear AS varchar) + '-R' + RIGHT( '0' + Cast (sp.CollectionPeriod AS varchar),2)  As PeriodEnd
                 FROM stgpmts.Payment sp
-                WHERE FundingSource=5) AT
-	GROUP BY AT.TransferSenderAccountId
-            ,AT.AccountId
-            ,AT.ApprenticeshipId
-            ,AT.PeriodEnd,AT.ApprenticeshipEmployerType
+                WHERE FundingSource=5) AT	
 GO

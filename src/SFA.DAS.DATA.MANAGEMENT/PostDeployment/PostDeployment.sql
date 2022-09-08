420463a7060b8210f44436bbba398c3e86654133
NarashimReddy <narashim.reddy@education.gov.uk>
2022-09-08 13:38:22 +0100

    did the required changes

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -1,2 +1,12 @@
 ﻿/* Execute Stored Procedure */
 EXEC [dbo].[Build_AS_DataMart]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupOversightReviewStatus]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupPageTitles]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupQuestionTitles]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupSectionTitles]
+
+DROP TABLE IF EXISTS [Mtd].[RP_LookupSequenceTitles]

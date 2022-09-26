ddbc9fa06a5694d094b97032e120365a8991e1ab
NarashimReddy <narashim.reddy@education.gov.uk>
2022-09-26 21:38:43 +0100

    added new line

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
@@ -240,14 +240,15 @@ VALUES
 		,'[Filename],[ContentType],[Size],[UserId],[UserName]',0,1
 		,'select [Id],[ApplicationId],[Filename],[ContentType],[Size],STUFF([UserId],2,len([UserId])-2,REPLICATE(''''*'''',len([UserId])-2)) As [UserId],STUFF([UserName],2,len([UserName])-2,REPLICATE(''''*'''',len([UserName])-2)) As [UserName],[CreatedOn]  from [dbo].[AppealFile]'
 		,'RP_AppealFile')
-,('Apply','OrganisationAddresses','dbo','[Id]',''
-		,'',0,1
-		,'select [Id] from [dbo].[OrganisationAddresses]'
-		,'RP_OrganisationAddresses')
 ,('Apply','FinancialReview','dbo','[Id],[ApplicationId],[Status],[SelectedGrade],[FinancialDueDate],[GradedOn],[ClarificationRequestedOn],[ClarificationResponse]',''
 		,'[Comments],[ExternalComments]',0,1
 		,'select [Id],[ApplicationId],[Status],[SelectedGrade],[FinancialDueDate],[GradedOn],[Comments],[ExternalComments],[ClarificationRequestedOn],[ClarificationResponse]  from [dbo].[FinancialReview]'
 		,'RP_FinancialReview')
+
+INSERT INTO Mtd.SourceConfigForImport
+(SourceDatabaseName,SourceTableName,SourceSchemaName,ColumnNamesToInclude,ColumnNamesToExclude,ColumnNamesToMask,PLTableName,[ModelDataToPL],[FullCopyToPL])
+VALUES
+('Apply','OrganisationAddresses','dbo','[Id],[OrganisationId],[AddressType],[AddressLine1],[AddressLine2],[AddressLine3],[City],[Postcode]','','','RP_OrganisationAddresses',0,1)
 
  /* Assessor Configuration for Certificates,OrganisationStandard,CertificateLogs and  Organisations */
 

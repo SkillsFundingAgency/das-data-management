ac4630773df0803685c52d8648a6704ad539231e
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2022-10-13 15:35:18 +0100

    Added Pipeline Controller

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Pipeline.sql
@@ -0,0 +1,7 @@
+﻿
+CREATE TABLE [Mgmt].[Pipeline](
+	[PipelineId] int NOT NULL,
+	[PipelineName] varchar(100) NOT NULL,
+	[LastUpdatedTimestamp] [datetime2](7) NULL,
+	CONSTRAINT [PK_Pipeline_PipelineId] PRIMARY KEY CLUSTERED (PipelineId ASC)
+	)
---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_PipelineController.sql
@@ -0,0 +1,9 @@
+﻿
+CREATE TABLE [Mgmt].[Config_PipelineController](
+	[PipelineControllerId] int IDENTITY(1,1)  NOT NULL,
+	[MasterPipelineId]  int NOT NULL,
+	[ChildPipelineId] int not null,
+	[IsEnabled] BIT not null, 
+	[ExecutionOrder] int not null,
+	CONSTRAINT [PK_PipelineController]_PipelineControllerId] PRIMARY KEY CLUSTERED (PipelineControllerId ASC)
+	)

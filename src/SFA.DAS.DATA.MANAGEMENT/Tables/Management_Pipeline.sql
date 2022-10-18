e94ac4dff25432ee87a980e04a457d8ff86671c4
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2022-10-18 16:15:34 +0100

    Updated default for last updated timestamp

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Pipeline.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Pipeline.sql
@@ -2,6 +2,6 @@
 CREATE TABLE [Mgmt].[Pipeline](
 	[PipelineId] int NOT NULL,
 	[PipelineName] varchar(100) NOT NULL,
-	[LastUpdatedTimestamp] [datetime2](7) NULL,
+	[LastUpdatedTimestamp] [datetime2](7) DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT [PK_Pipeline_PipelineId] PRIMARY KEY CLUSTERED (PipelineId ASC)
 	)

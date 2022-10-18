6fe3b97950b6c9b6d206c29999bbda51a51db51e
NarashimReddy <narashim.reddy@education.gov.uk>
2022-10-18 09:17:56 +0100

    updated the columns

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
@@ -384,7 +384,7 @@ VALUES
 INSERT INTO Mtd.SourceConfigForImport
 (SourceDatabaseName,SourceTableName,SourceSchemaName,ColumnNamesToInclude,ColumnNamesToExclude,ColumnNamesToMask,PLTableName,[ModelDataToPL],[FullCopyToPL])
 VALUES
-('Rofjaa','Agency','dbo','[LegalEntityId],[IsGrantFunded],[EffectiveFrom],[CreatedDate],[LastUpdatedDate]','','','Rofjaa_Agency',0,1) 
+('Rofjaa','Agency','dbo','[LegalEntityId],[IsGrantFunded]','','','Rofjaa_Agency',0,1) 
 
 COMMIT TRANSACTION
 

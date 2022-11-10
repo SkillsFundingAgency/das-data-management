a088dd1878c4baf81893ac174f08a2298d1e11c6
NarashimReddy <narashim.reddy@education.gov.uk>
2022-11-10 12:12:43 +0000

    adding county and  [Va_LocalAuthority]

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacanciesAndApprenticeshipCandidateRegDetails.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacanciesAndApprenticeshipCandidateRegDetails.sql
@@ -113,7 +113,41 @@ SELECT [ApprenticeshipId]
            ,[CandidateEmail]
   FROM Stg.Comt_ApprenticeshipCandidateRegDetails
 
-       
+/* Import LocalAuthority to PL */
+
+DELETE FROM [ASData_PL].[Va_LocalAuthority]
+ 
+INSERT INTO [ASData_PL].[Va_LocalAuthority]
+           ([LocalAuthorityId]
+		  ,[CodeName]
+		  ,[ShortName]
+		  ,[FullName]
+		  ,[CountyId]
+		   )
+SELECT    [LocalAuthorityId]
+		  ,[CodeName]
+		  ,[ShortName]
+		  ,[FullName]
+		  ,[CountyId]
+  FROM Stg.Avms_LocalAuthority
+
+/* Import County to PL */
+
+DELETE FROM [ASData_PL].[Va_County]
+
+INSERT INTO [ASData_PL].[Va_County]
+           ([CountyId]
+           ,[CodeName]
+           ,[ShortName]
+           ,[FullName]
+		   )
+SELECT [CountyId]
+           ,[CodeName]
+           ,[ShortName]
+           ,[FullName]
+  FROM Stg.Avms_County
+  
+
 COMMIT TRANSACTION
 
 UPDATE Mgmt.Log_Execution_Results

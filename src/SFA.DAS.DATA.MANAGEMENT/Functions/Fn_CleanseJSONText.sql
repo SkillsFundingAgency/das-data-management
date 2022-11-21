85472c58060d087751aa424710104931409a0846
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2022-11-21 15:20:12 +0000

    Updated text cleansing into functions

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Functions/Fn_CleanseJSONText.sql
@@ -0,0 +1,11 @@
+﻿CREATE FUNCTION [dbo].[Fn_CleanseJSONText] (@JSONText VARCHAR(MAX))
+RETURNS VARCHAR(MAX) AS
+BEGIN
+
+    SELECT @JSONText=LTRIM(RTRIM(Replace(Replace(Replace(Replace(Replace (@JSONText, '"', ''), '{', ''), '}',''),'[', ''), ']', '')))
+    
+    RETURN case when charindex(',',REVERSE(@JSONText)) =1 then left(@HTMLText,len(@JSONText)-1)				
+	         else @JSONText			
+			 end
+END
+GO

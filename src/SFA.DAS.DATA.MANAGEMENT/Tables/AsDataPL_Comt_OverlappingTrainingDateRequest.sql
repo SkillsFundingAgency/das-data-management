3f7fd83f5f3bb5a680cf2a49447b464286fbdc2a
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2022-12-13 17:12:29 +0000

    ANSITEL-2782

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_OverlappingTrainingDateRequest.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [AsData_PL].[Comt_OverlappingTrainingDateRequest]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY ,
+	[DraftApprenticeshipId] BIGINT NOT NULL,
+	[PreviousApprenticeshipId] BIGINT NOT NULL,
+	[ResolutionType] SMALLINT NULL,
+	[Status] SMALLINT NOT NULL DEFAULT 0, 
+	[CreatedOn] DATETIME2 NOT NULL ,
+	[ActionedOn] DATETIME2 NULL,
+	[NotifiedServiceDeskOn] DATETIME2 NULL,
+	[RowVersion] ROWVERSION NOT NULL,
+	[AsDm_UpdatedDateTime] datetime2 default getdate()
+)
+GO

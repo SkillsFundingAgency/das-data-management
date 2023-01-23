af0bc69af2e21c59b76f636b95c70245f699e9bb
manju-nagarajan <manju.nagarajan@education.gov.uk>
2023-01-23 14:18:00 +0000

    Amend educationhistory details

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FAA_CandidateEducationHistory.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_FAA_CandidateEducationHistory.sql
@@ -3,9 +3,7 @@
 	[SourceSK] BIGINT NOT NULL IDENTITY(1,1) PRIMARY KEY
    ,[CandidateId] Varchar(256)
    ,[LegacyCandidateId] BIGINT
-   ,[Institution] varchar(max)
-   ,[FromYear] INT
-   ,[ToYear] INT
+   ,[Institution] varchar(max)  
    ,[RunID] BIGINT
    ,AsDm_CreatedDate datetime2 default(getdate()) 
    ,AsDm_UpdatedDate datetime2 default(getdatE())

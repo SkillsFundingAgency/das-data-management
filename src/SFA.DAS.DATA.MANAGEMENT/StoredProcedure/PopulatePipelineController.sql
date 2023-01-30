2a93d659407c18e20ab9d8ce96e3bc5d0f3cfd5c
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2023-01-30 15:21:49 +0000

    Seperated Public Sector

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulatePipelineController.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulatePipelineController.sql
@@ -77,7 +77,8 @@ VALUES
 ,(1	,9	,1	,9)
 ,(1	,10	,1	,10)
 ,(1	,11	,1	,11)
-,(1	,12	,1	,12)
+--2023-01-30 ImportPublicSectorToDM seperated to be an individual pipeline
+--,(1	,12	,1	,12)
 ,(1	,13	,1	,13)
 ,(1	,14	,1	,14)
 ,(1	,15	,1	,15)

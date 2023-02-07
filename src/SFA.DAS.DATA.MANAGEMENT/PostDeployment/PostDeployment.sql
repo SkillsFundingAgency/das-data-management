289278a167b6d4fde99baccd9c641e6434c87b29
manju-nagarajan <manju.nagarajan@education.gov.uk>
2023-02-07 11:38:10 +0000

    remove unused stor proc

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -3,3 +3,5 @@ EXEC [dbo].[Build_AS_DataMart]
 
 DROP TABLE IF EXISTS  [ASData_PL].[Acc_TransferConnectionInvitationChange]
 DROP TABLE IF EXISTS  [ASData_PL].[Acc_TransferConnectionInvitation]
+DROP PROCEDURE IF EXISTS [dbo].[BuildVacanciesPresentationLayer]
+DROP PROCEDURE IF EXISTS [dbo].[ImportVacanciesAdditionalInfoToPL]

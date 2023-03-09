ab02611f5a873556e106a4feac3fd3ef8472ab17
manju-nagarajan <manju.nagarajan@education.gov.uk>
2023-03-09 09:30:26 +0000

    rename table

--- src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PostDeployment/PostDeployment.sql
@@ -1,7 +1,5 @@
 ﻿/* Execute Stored Procedure */
 EXEC [dbo].[Build_AS_DataMart]
 
-DROP TABLE IF EXISTS  [ASData_PL].[Acc_TransferConnectionInvitationChange]
-DROP TABLE IF EXISTS  [ASData_PL].[Acc_TransferConnectionInvitation]
-DROP PROCEDURE IF EXISTS [dbo].[BuildVacanciesPresentationLayer]
-DROP PROCEDURE IF EXISTS [dbo].[ImportVacanciesAdditionalInfoToPL]
+DROP TABLE IF EXISTS [ASData_PL].[Provider]
+

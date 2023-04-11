e5482612c56cdd2f551a444c39fad0c6e7eacc2a
manju-nagarajan <manju.nagarajan@education.gov.uk>
2023-04-11 10:52:07 +0100

    removed all outdated tables creation

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_FAT2_ProviderRegistrationFeedbackRating.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [ASData_PL].[FAT2_ProviderRegistrationFeedbackRating]
-(
-		[UkPrn]						int					NOT NULL,
-		[FeedbackName]				varchar(100)		NOT NULL,
-		[FeedbackCount]				int					NULL,
-		[AsDm_UpdatedDateTime]		[datetime2](7) DEFAULT (getdate())	
-)

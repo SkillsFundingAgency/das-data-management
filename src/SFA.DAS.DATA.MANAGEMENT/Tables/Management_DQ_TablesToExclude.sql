3a0e65e13be879292ffb52bb32e6f6d73decfd54
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2023-04-24 12:55:50 +0100

    Corrected Primary Key

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_DQ_TablesToExclude.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_DQ_TablesToExclude.sql
@@ -3,5 +3,5 @@
 	[SchemaName] varchar(100) NOT NULL,
 	[TableName] BIT NOT NULL,	
 	[LastUpdatedTimestamp] [datetime2](7) DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT [PK_DQTablesToExclude] PRIMARY KEY CLUSTERED (DQCheckId ASC)
+	CONSTRAINT [PK_DQTablesToExclude] PRIMARY KEY CLUSTERED (DQTablesToExcludeId ASC)
 	)

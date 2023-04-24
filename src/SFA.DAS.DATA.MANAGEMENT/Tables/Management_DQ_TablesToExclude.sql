b9197b84318018765f9d46f25d32e33d185839b1
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2023-04-24 13:33:35 +0100

    Fix

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_DQ_TablesToExclude.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_DQ_TablesToExclude.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Mgmt].[DQ_TablesToExclude](
 	[DQTablesToExcludeId] BIGINT        IDENTITY (1, 1) NOT NULL,	
 	[SchemaName] varchar(250) NOT NULL,
-	[TableName] varchar(500) NOT NULL,,	
+	[TableName] varchar(500) NOT NULL,
 	[LastUpdatedTimestamp] [datetime2](7) DEFAULT CURRENT_TIMESTAMP,
 	CONSTRAINT [PK_DQTablesToExclude] PRIMARY KEY CLUSTERED (DQTablesToExcludeId ASC)
 	)

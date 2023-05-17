3e0123b6ec353f6557042f063a04e1a110f35a26
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2023-05-17 17:59:50 +0100

    Updated table

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_Marketo_Dates.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_Marketo_Dates.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [Mgmt].[Log_Marketo_Dates] (
     [Id]                    BIGINT        IDENTITY (1, 1) NOT NULL,
     [LogId]                 BIGINT NOT NULL,
-    [StartDate]             DATE NULL,
-    [EndDate]               DATE NULL,
+    [StartDate]             DATETIME2 NULL,
+    [EndDate]               DATETIME2 NULL,
     [TransactionTime]       DATETIME2 DEFAULT CURRENT_TIMESTAMP ,
+    [IsCompleted]           BIT DEFAULT FALSE,
     CONSTRAINT [PK_LMD_ID] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_LMD_LogID] FOREIGN KEY ([LogId]) REFERENCES [Mgmt].[Log_Execution_Results] ([LogId])
 );

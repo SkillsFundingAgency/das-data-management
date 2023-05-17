6342ae4c3f875e6fd99033c4e664d899197ad8c8
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2023-05-17 18:34:20 +0100

    Added SP

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_Marketo_Dates.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_Log_Marketo_Dates.sql
@@ -4,7 +4,6 @@
     [StartDate]             DATETIME2 NULL,
     [EndDate]               DATETIME2 NULL,
     [TransactionTime]       DATETIME2 DEFAULT CURRENT_TIMESTAMP ,
-    [IsCompleted]           BIT DEFAULT 0,
     CONSTRAINT [PK_LMD_ID] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_LMD_LogID] FOREIGN KEY ([LogId]) REFERENCES [Mgmt].[Log_Execution_Results] ([LogId])
 );

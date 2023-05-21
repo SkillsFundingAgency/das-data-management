1497357f342765096e2ff19ed96439dfb61d9167
karthik-dfe <110661779+karthik-dfe@users.noreply.github.com>
2023-05-21 19:40:40 +0100

    Added View

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/LoadStatus copy.sql
@@ -0,0 +1,23 @@
+﻿CREATE VIEW [Mgmt].[MarketoLoadStatus]
+as
+WITH cte AS (
+    SELECT * FROM (
+        VALUES
+            ('ImportMarketoProgramsToDMStaging','ImportMarketoReferenceTablesToPL'),
+            ('ImportMarketoProgramsToDMStaging','ImportMarketoReferenceTablesToPL'),
+			('ImportMarketoLeadActivitiesToDMStaging','ImportMarketoBulkExtractTablesToPL')
+        ) AS a (StagingActivity, PLActivity)
+    )
+
+select b.RunId
+		,b.ADFTaskType
+		,a.StartDate 
+		,a.EndDate 
+		,b.Execution_status as StagingExecutionStatus
+		,d.Execution_Status as PLExecutionStatus 
+		from mgmt.Log_Marketo_Dates a
+inner join mgmt.Log_Execution_Results b on a.LogId = b.LogId
+inner join cte c on c.StagingActivity = b.ADFTaskType
+inner join mgmt.Log_Execution_Results d on d.RunId = b.RunId
+and d.StoredProcedureName = c.PLActivity
+GO

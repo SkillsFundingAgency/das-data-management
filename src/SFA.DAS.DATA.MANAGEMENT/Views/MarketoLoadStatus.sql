eee5742ae5deb7be381d46b918171806f911bf9d
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2023-05-31 21:24:10 +0100

    ANSITEL-3061 added view

--- src/SFA.DAS.DATA.MANAGEMENT/Views/MarketoLoadStatus.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/MarketoLoadStatus.sql
@@ -4,8 +4,9 @@ WITH cte AS (
     SELECT * FROM (
         VALUES
             ('ImportMarketoProgramsToDMStaging','ImportMarketoReferenceTablesToPL'),
-            ('ImportMarketoProgramsToDMStaging','ImportMarketoReferenceTablesToPL'),
-			('ImportMarketoLeadActivitiesToDMStaging','ImportMarketoBulkExtractTablesToPL')
+            ('ImportMarketoSmartCampaignsToDMStaging','ImportMarketoReferenceTablesToPL'),
+			('ImportMarketoLeadActivitiesToDMStaging','ImportMarketoBulkExtractTablesToPL'),
+			('ImportMarketoLeadsToDMStaging','ImportMarketoBulkExtractTablesToPL')
         ) AS a (StagingActivity, PLActivity)
     )
 
@@ -15,9 +16,11 @@ select b.RunId
 		,a.EndDate 
 		,b.Execution_status as StagingExecutionStatus
 		,d.Execution_Status as PLExecutionStatus 
+		,a.TransactionTime,b.*
 		from mgmt.Log_Marketo_Dates a
 inner join mgmt.Log_Execution_Results b on a.LogId = b.LogId
-inner join cte c on c.StagingActivity = b.ADFTaskType
-inner join mgmt.Log_Execution_Results d on d.RunId = b.RunId
+left join cte c on c.StagingActivity = b.ADFTaskType
+left join mgmt.Log_Execution_Results d on d.RunId = b.RunId
 and d.StoredProcedureName = c.PLActivity
+
 GO

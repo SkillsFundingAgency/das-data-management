e10f771f430979501c3bd4cc9a262356879f412d
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2023-06-01 13:44:31 +0100

    removed order by

--- src/SFA.DAS.DATA.MANAGEMENT/Views/MarketoLoadStatus.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/MarketoLoadStatus.sql
@@ -22,5 +22,5 @@ inner join mgmt.Log_Execution_Results b on a.LogId = b.LogId
 left join cte c on c.StagingActivity = b.ADFTaskType
 left join mgmt.Log_Execution_Results d on d.RunId = b.RunId
 and d.StoredProcedureName = c.PLActivity
-order by a.transactiontime 
+ 
 GO

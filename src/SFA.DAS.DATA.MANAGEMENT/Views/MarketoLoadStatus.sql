b81fedc0cb171bb273825b0a97b4bfb3e02b8180
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2023-06-01 13:40:34 +0100

    Added order by transaction time

--- src/SFA.DAS.DATA.MANAGEMENT/Views/MarketoLoadStatus.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/MarketoLoadStatus.sql
@@ -16,11 +16,11 @@ select b.RunId
 		,a.EndDate 
 		,b.Execution_status as StagingExecutionStatus
 		,d.Execution_Status as PLExecutionStatus 
-		
+		,a.transactiontime
 		from mgmt.Log_Marketo_Dates a
 inner join mgmt.Log_Execution_Results b on a.LogId = b.LogId
 left join cte c on c.StagingActivity = b.ADFTaskType
 left join mgmt.Log_Execution_Results d on d.RunId = b.RunId
 and d.StoredProcedureName = c.PLActivity
-
+order by a.transactiontime 
 GO

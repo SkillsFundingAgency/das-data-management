c135c9566fb75e61c9b26482667d0c96e9e381a5
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2023-07-10 19:25:45 +0100

    Added Audit_LogExecutionResults

---
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/Audit_LogExecutionResults.sql
@@ -0,0 +1,55 @@
+﻿CREATE PROCEDURE [Mgmt].[Audit_LogExecutionResults]
+@RunId BIGINT,
+@StepNo VARCHAR (100) = NULL,
+@StoredProcedureName VARCHAR (100),
+@ADFTaskType VARCHAR(256),
+@StartDateTime DATETIME2 (7) = getdate(),
+@Execution_Status BIT = 0
+AS
+BEGIN TRY
+
+DECLARE @LogID int
+
+INSERT INTO Mgmt.Log_Execution_Results
+	  (
+	    RunId
+	   ,StepNo
+	   ,StoredProcedureName
+	   ,ADFTaskType
+	   ,StartDateTime
+	   ,Execution_Status
+	  )
+  SELECT 
+       @RunId
+	   ,@StepNo 
+	   ,@StoredProcedureName
+	   ,@ADFTaskType
+	   ,getdate()
+	   ,0
+
+SELECT @LogID=MAX(LogId) FROM [Mgmt].[Log_Execution_Results] 
+
+RETURN (@LogID)
+
+END TRY
+
+BEGIN CATCH
+
+  SELECT 
+        SUSER_SNAME(),
+	    ERROR_NUMBER(),
+	    ERROR_STATE(),
+	    ERROR_SEVERITY(),
+	    ERROR_LINE(),
+	    'Audit_LogExecutionResults' AS ErrorProcedure,
+	    ERROR_MESSAGE(),
+	    GETDATE()
+		;
+
+
+/* Update Log Execution Results as Fail if there is an Error*/
+
+END CATCH
+GO
+
+

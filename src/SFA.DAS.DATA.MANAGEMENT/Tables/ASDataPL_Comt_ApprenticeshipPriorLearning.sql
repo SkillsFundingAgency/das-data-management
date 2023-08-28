0246c24549ac7b8aaa6aebd89dbeb9d5a20dc7d0
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2023-08-28 20:43:10 +0100

    ANSITEL-3229

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_ApprenticeshipPriorLearning.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_ApprenticeshipPriorLearning.sql
@@ -3,5 +3,10 @@
 	[DurationReducedBy] [int] NULL,
 	[PriceReducedBy] [int] NULL,
 	[IsAccelerated] bit DEFAULT (0),
+	[DurationReducedByHours] [int] NULL,
+	[WeightageReducedBy] [int] NULL,
+	[ReasonForRplReduction] [nvarchar](1000) NULL,
+	[QualificationsForRplReduction] [text] NULL,
+	[IsDurationReducedByRpl] [bit] NULL,
 	[AsDm_UpdatedDateTime] datetime2 default getdate()
 )

fdeee8caa744de950218ad2e7522215cd24a8185
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2024-01-18 16:32:53 +0000

    ASINTEL-3473

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_LTM_Audit.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [ASData_PL].[LTM_Audit]
+(	
+   ID bigint
+  ,EntityType nvarchar(256)
+  ,EntityId bigint
+  ,UserId nvarchar(256)
+  ,UserAction nvarchar(256)
+  ,AuditDate datetime2
+  ,CorrelationId UNIQUEIDENTIFIER
+ 
+  ,[AsDm_UpdatedDateTime] datetime2 default getdate()
+) ON [PRIMARY]

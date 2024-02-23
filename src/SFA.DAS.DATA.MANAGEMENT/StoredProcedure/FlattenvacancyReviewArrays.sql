e4cf366c917dcb81183f69c2afa399b26e675455
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-02-23 16:00:34 +0000

    3578

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/FlattenvacancyReviewArrays.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/FlattenvacancyReviewArrays.sql
@@ -2,13 +2,19 @@ CREATE PROCEDURE [dbo].[FlattenvacancyReviewArrays]
 AS
 BEGIN
     -- Create a temporary table to store the parsed JSON values
-    CREATE TABLE #TempTable (VacancyReference varchar(256), JSONValue varchar(max)); -- Adjust the data type based on your JSON structure
+    CREATE TABLE #TempTable (VacancyReference varchar(256)--,binary varchar(256)
+    ,ruleId varchar(256),score varchar(256),narrative varchar(256),data varchar(256),target varchar(256))
 
     -- Insert parsed JSON values into the temporary table
-    INSERT INTO #TempTable (VacancyReference, JSONValue)
+    INSERT INTO #TempTable 
     SELECT
         VacancyReference,
-        JSON_VALUE(Rule_Details, '$') AS JSONValue
+       -- JSON_VALUE(Rule_Details, '$.binary') AS binary,
+        JSON_VALUE(Rule_Details, '$.ruleId') AS ruleId,
+        JSON_VALUE(Rule_Details, '$.score') AS score,
+        JSON_VALUE(Rule_Details, '$.narrative') AS narrative,
+        JSON_VALUE(Rule_Details, '$.data') AS data,
+        JSON_VALUE(Rule_Details, '$.target') AS target
     FROM
         stg.RAA_VacancyReviews_AutoQAoutcome
         CROSS APPLY OPENJSON(Rule_Details);
@@ -16,20 +22,20 @@ BEGIN
     -- Update the target table with flattened values
     UPDATE AD
     SET
-        Details_BinaryID = ISNULL(t1.JSONValue, binary), -- Replace Column1 with the actual column name
-        Details_RuleID = ISNULL(t2.JSONValue, ruleId),
-        Details_score = ISNULL(t3.JSONValue, score),
-        Details_narrative = ISNULL(t4.JSONValue, narrative),
-        Details_data = ISNULL(t5.JSONValue, data),
-        Details_target = ISNULL(t6.JSONValue, target)
+       -- Details_BinaryIDDetails_BinaryID = ISNULL(t1.binary, Details_BinaryID), -- Replace Column1 with the actual column name
+        Details_RuleID = ISNULL(t2.ruleId, Details_RuleID),
+        Details_score = ISNULL(t3.score,Details_score),
+        Details_narrative = ISNULL(t4.narrative, Details_narrativee),
+        Details_data = ISNULL(t5.data, Details_data),
+        Details_target = ISNULL(t6.target, Details_target)
     FROM
         stg.RAA_VacancyReviews_AutoQAoutcomedetails AD
-        LEFT JOIN #TempTable t1 ON AD.VacancyReference = t1.VacancyReference AND t1.JSONValue IS NOT NULL
-        LEFT JOIN #TempTable t2 ON AD.VacancyReference = t2.VacancyReference AND t2.JSONValue IS NOT NULL
-        LEFT JOIN #TempTable t3 ON AD.VacancyReference = t3.VacancyReference AND t3.JSONValue IS NOT NULL
-        LEFT JOIN #TempTable t4 ON AD.VacancyReference = t4.VacancyReference AND t4.JSONValue IS NOT NULL
-        LEFT JOIN #TempTable t5 ON AD.VacancyReference = t5.VacancyReference AND t5.JSONValue IS NOT NULL
-        LEFT JOIN #TempTable t6 ON AD.VacancyReference = t6.VacancyReference AND t6.JSONValue IS NOT NULL
+        --LEFT JOIN #TempTable t1 ON AD.VacancyReference = t1.VacancyReference AND t1.binary IS NOT NULL
+        LEFT JOIN #TempTable t2 ON AD.VacancyReference = t2.VacancyReference AND t2.ruleId IS NOT NULL
+        LEFT JOIN #TempTable t3 ON AD.VacancyReference = t3.VacancyReference AND t3.score IS NOT NULL
+        LEFT JOIN #TempTable t4 ON AD.VacancyReference = t4.VacancyReference AND t4.narrative IS NOT NULL
+        LEFT JOIN #TempTable t5 ON AD.VacancyReference = t5.VacancyReference AND t5.data IS NOT NULL
+        LEFT JOIN #TempTable t6 ON AD.VacancyReference = t6.VacancyReference AND t6.target IS NOT NULL
       
 
     -- Drop the temporary table

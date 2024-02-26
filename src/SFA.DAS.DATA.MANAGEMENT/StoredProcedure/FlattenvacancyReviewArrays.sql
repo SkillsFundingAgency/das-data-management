5932c3c99cb79b8039354da1bb554f1d2e602525
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-02-26 12:23:04 +0000

    3578

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/FlattenvacancyReviewArrays.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/FlattenvacancyReviewArrays.sql
@@ -19,7 +19,7 @@ BEGIN
         ,[Details_target])
 
     Select ad.binaryid,ad.EmployerAccountId,ad.UserId,ad.vacancyreference,c.Detailsbinaryid,b.ruleId,b.score,b.narrative,b.data,b.target
-          From stg.RAA_VacancyReviews_AutoQAoutcome Ad
+          From stg.RAA_VacancyReviews_AutoQARuleoutcome Ad
     Cross Apply OpenJSON(Ad.Rule_Details)
         WITH (
             _id     nvarchar(max) '$._id' as json,
@@ -32,6 +32,47 @@ BEGIN
     cross apply  OpenJSON(b._id)
         WITH 
             ( [DetailsBinaryid] varchar(100) '$."$binary"') c
+    
+    DELETE FROM Stg.RAA_VacancyReviews_AutoQAoutcome
+    
+    INSERT into Stg.RAA_VacancyReviews_AutoQAoutcome
+    (BinaryId
+    ,EmployerAccountId
+    ,UserId
+    ,VacancyReference
+    ,RuleoutcomeID
+    ,Rule_RuleId
+    ,Rule_Score
+    ,Rule_Narrative
+    ,Rule_Target
+    ,Details_BinaryID
+    ,Details_RuleID
+    ,Details_score
+    ,Details_narrative
+    ,Details_data
+    ,Details_target)
+
+    SELECT  A.BinaryId
+           ,A.VacancyReference
+           ,A.EmployerAccountId
+           ,A.UserId 
+           ,A.RuleOutcomeID
+           ,A.Rule_Ruleid
+           ,A.Rule_Score
+           ,A.Rule_Narrative
+           ,A.Rule_Target
+           ,AD.Details_BinaryID
+           ,AD.Details_RuleId 
+           ,AD.Details_score 
+           ,AD.Details_narrative
+           ,AD.Details_data 
+           ,AD.Details_target
+     FROM stg.RAA_VacancyReviews_AutoQAoutcomedetails AD
+   INNER JOIN stg.RAA_VacancyReviews_AutoQAoutcome A
+       on AD.binaryid=A.binaryid
+       and AD.VacancyReference = A.VacancyReference 
+
+    
 
 
     -- Update the target table with flattened values
--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReview_AutoQARuleoutcome.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReview_AutoQARuleoutcome.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Stg.RAA_VacancyReviews_AutoQAoutcome
+CREATE TABLE Stg.RAA_VacancyReviews_AutoQARuleoutcome
 (
   SourseSK BIGINT IDENTITY(1,1) NOT NULL
  ,BinaryId VARCHAR(256)
@@ -10,10 +10,5 @@ CREATE TABLE Stg.RAA_VacancyReviews_AutoQAoutcome
  ,Rule_Score varchar(256)
  ,Rule_Narrative varchar(max)
  ,Rule_Target varchar(256)
- ,Details_BinaryID varchar(256)
- ,Details_RuleID varchar(256)
- ,Details_score varchar(256)
- ,Details_narrative varchar(max)
- ,Details_data varchar(max)
- ,Details_target varchar(256)
- )
+ ,Rule_Details varchar(max)
+)

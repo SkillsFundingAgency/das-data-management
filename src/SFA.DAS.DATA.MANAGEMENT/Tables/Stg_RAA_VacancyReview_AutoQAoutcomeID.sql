bbaef474f368f5897e6094fd0cdb38a21298a576
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-02-27 09:43:26 +0000

    3578

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReview_AutoQAoutcomeID.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_RAA_VacancyReview_AutoQAoutcomeID.sql
@@ -7,4 +7,6 @@ CREATE TABLE Stg.RAA_VacancyReviews_AutoQAoutcomeID
  ,VacancyReference VARCHAR(256)
  ,Ruleoutcome_BinaryID Varchar(256)
  ,AutoQAfieldisReferred varchar(256)
+ ,AsDm_CreatedDate datetime2 default(getdate()) 
+ ,AsDm_UpdatedDate datetime2 default(getdatE())
  )

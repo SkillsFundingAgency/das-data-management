dd09dec4ee535d9bcae5b362ffae9547a5a57059
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-03-14 12:48:29 +0000

    3593

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_FAT_ROATPV2_NationalAchievementRate.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_FAT_ROATPV2_NationalAchievementRate.sql
@@ -7,7 +7,7 @@
 	[ApprenticeshipLevel]			smallint				NULL,
 	[OverallCohort]					int						NULL,
 	[OverallAchievementRate]		decimal(10,4)			NULL,
-	[SectorSubjectAreaTier1]        int 					NULL,
+	[SectorSubjectAreaTier1]        int 					NOT NULL,
 	[Ukprn] 						int 					NULL,
 	[AsDm_UpdatedDateTime]			[datetime2](7)			DEFAULT (getdate())
 )

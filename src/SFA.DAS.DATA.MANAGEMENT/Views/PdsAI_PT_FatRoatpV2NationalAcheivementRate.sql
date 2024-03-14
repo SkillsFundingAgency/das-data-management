44821781dd5ee6fb7df13a3f5025a9ccb2e913ae
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-03-14 16:48:05 +0000

    3625

--- src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_FatRoatpV2NationalAcheivementRate.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/PdsAI_PT_FatRoatpV2NationalAcheivementRate.sql
@@ -3,7 +3,7 @@
 SELECT distinct FNAR.[Id]                                       AS F1
       ,convert(NVarchar(500),HASHBYTES('SHA2_512',LTRIM(RTRIM(CONCAT(FP.UkPrn , skl.SaltKey)))),2) AS F2
       ,FNAR.[Age]                                      AS F3
-      ,SSA1.SectorSubjectAreaTier1                     AS F4
+      ,SSA1.SectorSubjectAreaTier1Desc                     AS F4
       ,FNAR.[ApprenticeshipLevel]                      AS F5
       ,FNAR.[OverallCohort]                            AS F6
       ,FNAR.[OverallAchievementRate]                   AS F7
@@ -17,7 +17,7 @@ SELECT distinct FNAR.[Id]                                       AS F1
           from ASData_PL.FAT2_SectorSubjectAreaTier1
          union 
         select 99,'All Sector Subject Area  Tier 1') SSA1
-	ON SSA1.SectorSubjectAreaTier1Desc=FNAR.SectorSubjectAreaTier1
+	ON SSA1.SectorSubjectAreaTier1=FNAR.SectorSubjectAreaTier1
  CROSS
   JOIN (Select TOP 1 SaltKeyID,SaltKey From Mgmt.SaltKeyLog Where SourceType ='EmployerReference'  Order by SaltKeyID DESC ) Skl 
 

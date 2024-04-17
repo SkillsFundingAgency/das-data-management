1472eba26313f49883e3c4dfb6ad1a5d50696a55
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-04-17 09:24:50 +0100

    3711

--- src/SFA.DAS.DATA.MANAGEMENT/Views/ASData_PL_DimEmployer.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/ASData_PL_DimEmployer.sql
@@ -1,3 +1,21 @@
+BEGIN
+    -- Check if the view exists
+    IF EXISTS (
+        SELECT 1 
+        FROM INFORMATION_SCHEMA.VIEWS 
+        WHERE TABLE_SCHEMA = 'ASData_PL' 
+        AND TABLE_NAME = 'EmployerDim'
+    )
+    BEGIN
+        DECLARE @sql NVARCHAR(MAX);
+        SET @sql = 'DROP VIEW [ASData_PL].[EmployerDim];';
+        EXEC sp_executesql @sql;
+        PRINT 'View [ASData_PL].[EmployerDim] dropped successfully.';
+    END
+END
+Go
+
+
 CREATE VIEW [ASData_PL].[DimEmployer]
 As
  SELECT EmployerAccountId,EmployerAccountName,EmployerAccountHashedId,EmployerType,EmployerSector AS EmployerSectorEstimate

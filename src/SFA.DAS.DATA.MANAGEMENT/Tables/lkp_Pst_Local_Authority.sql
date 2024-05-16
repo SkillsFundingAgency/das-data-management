a53d91744bb9aa1f90f1ac0177299f9d411e94bf
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-05-16 16:01:30 +0100

    3788 fix

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/lkp_Pst_Local_Authority.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [lkp].[Pst_Local_Authority]
+(
+	[Pst_LOCAL_AUTHORITY]			[varchar](9) NULL,
+	[Pst_LOCAL_AUTHO_Desc]			[varchar](50) NULL,
+	[Pst_LOCAL_AUTHO_Desc2]			[varchar](30) NULL,
+) ON [Primary]
+GO

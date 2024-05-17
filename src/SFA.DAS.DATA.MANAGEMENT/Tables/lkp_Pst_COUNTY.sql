daf967c55590051d735dc854f0acd170db75776c
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-05-17 12:50:24 +0100

    3796

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/lkp_Pst_COUNTY.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [lkp].[Pst_COUNTY](
+       [Pst_COUNTY_County] [int] NOT NULL,
+       [Pst_COUNTY] [float] NULL,
+       [Pst_COUNTY_Coun_Desc] [varchar](35) NULL,
+       [Pst_COUNTY_Coun_Desc2] [varchar](15) NULL
+) ON [Primary]
+GO

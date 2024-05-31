d9545eba3cb47484be3cf30745f3e3b5a3eb3e34
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-05-31 11:50:28 +0100

    3815

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/stg_Pst_LatLong.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/stg_Pst_LatLong.sql
@@ -3,6 +3,7 @@ CREATE TABLE [stg].[Pst_latlong]
     Id BigINT Not NUll,
     Postcode Nvarchar(30),
     Latitude DECIMAL(18,6 ),
-    Longitude DECIMAL(18,6 )
+    Longitude DECIMAL(18,6 ),
+    AsDm_UpdatedDate datetime2 default(getdate())
 ) ON [Primary]
 GO

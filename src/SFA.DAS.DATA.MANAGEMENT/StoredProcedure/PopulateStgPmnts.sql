160041283c8bb703733558aa5f8369b403305266
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2024-06-27 20:36:53 +0100

    Updated metadata

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateStgPmnts.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateStgPmnts.sql
@@ -37,7 +37,7 @@ INSERT INTO [Mgmt].[Config_StgPmnts]
            (SourceDBName, SourceTable, DestSchema, DestTable, LoadType, WhereClause)
      VALUES	
 ('DASPayments','[Payments2].[Apprenticeship]','[StgPmts]','[Apprenticeship]','Full','')
-,('DASPayments','[Payments2].[ApprenticeshipPause]','[StgPmts]','[Apprenticeship]','Full','')
+,('DASPayments','[Payments2].[ApprenticeshipPause]','[StgPmts]','[ApprenticeshipPause]','Full','')
 /*,('DASPayments','[Payments2].[ApprenticeshipPriceEpisode]','[StgPmts].[Apprenticeship]','Full','')
 ,('DASPayments','[Payments2].[ApprenticeshipStatus]','[StgPmts].[Apprenticeship]','Full','')
 ,('DASPayments','[Payments2].[EmployerProviderPriority]','[StgPmts].[Apprenticeship]','Full','')

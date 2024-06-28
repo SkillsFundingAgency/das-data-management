275a14b0779b5553506420eaa6a470e6c8680e64
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2024-06-28 16:40:05 +0100

    Updated table

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_StgPmts.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Management_StgPmts.sql
@@ -1,8 +1,8 @@
 ﻿CREATE TABLE [Mgmt].[Config_StgPmnts](
-	SourceDBName NVARCHAR(50),
+	SourceDBName NVARCHAR(200),
 	SourceTable NVARCHAR(200),
 	DestSchema NVARCHAR(200),
 	DestTable NVARCHAR(200),
-    LoadType NVARCHAR(10),  -- 'Full' or 'Incremental'
+    LoadType NVARCHAR(20),  -- 'Full' or 'Incremental'
     WhereClause NVARCHAR(1000) 
 	)

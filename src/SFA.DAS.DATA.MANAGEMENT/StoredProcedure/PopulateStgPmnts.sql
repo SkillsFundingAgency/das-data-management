76704570e3bcaa46cc7a84a57db89100620a3a06
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2024-07-02 19:53:23 +0100

    Updated Metadata

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateStgPmnts.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateStgPmnts.sql
@@ -36,18 +36,18 @@ DELETE FROM [Mgmt].[Config_StgPmnts]
 INSERT INTO [Mgmt].[Config_StgPmnts]
            (SourceDBName, SourceTable, DestSchema, DestTable, LoadType, WhereClause, ColumnsToInclude)
      VALUES	
-('DASPayments','[Payments2].[Apprenticeship]','StgPmts','Apprenticeship','Full','','')
-,('DASPayments','[Payments2].[ApprenticeshipPause]','StgPmts','ApprenticeshipPause','Full','','')
-,('DASPayments','[Payments2].[ApprenticeshipPriceEpisode]','StgPmts','ApprenticeshipPriceEpisode','Full','','')
-,('DASPayments','[Payments2].[ApprenticeshipStatus]','StgPmts','ApprenticeshipStatus','Full','','')
-,('DASPayments','[Payments2].[EmployerProviderPriority]','StgPmts','EmployerProviderPriority','Full','','')
-,('DASPayments','[Payments2].[Job]','StgPmts','stg_Job','Incremental','where AcademicYear = ParamAcademicYear and collectionPeriod = ParamCollectionPeriod','')
-,('DASPayments','[Payments2].[JobStatus]','StgPmts','JobStatus','Full','','')
-,('DASPayments','[Payments2].[JobType]','StgPmts','JobType','Full','','')
-,('DASPayments','[Payments2].[LevyAccount]','StgPmts','LevyAccount','Full','','')
+('DASPayments','[Payments2].[Apprenticeship]','StgPmts','Apprenticeship','Full','','Id,AccountId,AgreementId,AgreedOnDate,Uln,Ukprn,EstimatedStartDate,EstimatedEndDate,Priority,StandardCode,ProgrammeType,FrameworkCode,PathwayCode,LegalEntityName,TransferSendingEmployerAccountId,StopDate,Status,IsLevyPayer,CreationDate,ApprenticeshipEmployerType')
+,('DASPayments','[Payments2].[ApprenticeshipPause]','StgPmts','ApprenticeshipPause','Full','','Id,ApprenticeshipId,PauseDate,ResumeDate')
+,('DASPayments','[Payments2].[ApprenticeshipPriceEpisode]','StgPmts','ApprenticeshipPriceEpisode','Full','','Id,ApprenticeshipId,StartDate,EndDate,Cost,Removed,CreationDate')
+,('DASPayments','[Payments2].[ApprenticeshipStatus]','StgPmts','ApprenticeshipStatus','Full','','Id,Description')
+,('DASPayments','[Payments2].[EmployerProviderPriority]','StgPmts','EmployerProviderPriority','Full','','Id,EmployerAccountId,Ukprn,Order')
+,('DASPayments','[Payments2].[Job]','StgPmts','stg_Job','Incremental','where AcademicYear = ParamAcademicYear and collectionPeriod = ParamCollectionPeriod','JobId,JobType,StartTime,EndTime,Status,CreationDate,DCJobId,Ukprn,IlrSubmissionTime,LearnerCount,AcademicYear,CollectionPeriod,DataLocksCompletionTime,DCJobSucceeded,DCJobEndTime')
+,('DASPayments','[Payments2].[JobStatus]','StgPmts','JobStatus','Full','','Id,Description')
+,('DASPayments','[Payments2].[JobType]','StgPmts','JobType','Full','','Id,Description')
+,('DASPayments','[Payments2].[LevyAccount]','StgPmts','LevyAccount','Full','','AccountId,AccountName,Balance,IsLevyPayer,TransferAllowance')
 ,('DASPayments','[Payments2].[Payment]','StgPmts','stg_Payment','Incremental','where AcademicYear = ParamAcademicYear and collectionPeriod = ParamCollectionPeriod','Id,EventId,EarningEventId,FundingSourceEventId,EventTime,JobId,DeliveryPeriod,CollectionPeriod,AcademicYear,Ukprn,LearnerReferenceNumber,LearningAimSequenceNumber,LearnerUln,PriceEpisodeIdentifier,Amount,LearningAimReference,LearningAimProgrammeType,LearningAimStandardCode,LearningAimFrameworkCode,LearningAimPathwayCode,LearningAimFundingLineType,ContractType,TransactionType,FundingSource,IlrSubmissionDateTime,SfaContributionPercentage,AgreementId,AccountId,TransferSenderAccountId,CreationDate,EarningsStartDate,EarningsPlannedEndDate,EarningsActualEndDate,EarningsCompletionStatus,EarningsCompletionAmount,EarningsInstalmentAmount,EarningsNumberOfInstalments,LearningStartDate,ApprenticeshipId,ApprenticeshipPriceEpisodeId,ApprenticeshipEmployerType,ReportingAimFundingLineType,NonPaymentReason,DuplicateNumber')
-,('DASPayments','[Payments2].[ProviderAdjustmentPayments]','StgPmts','stg_ProviderAdjustmentPayments','Incremental','where AcademicYear = ParamAcademicYear and collectionPeriod = ParamCollectionPeriod','')
-,('DASPayments','[Payments2].[SubmittedLearnerAim]','StgPmts','SubmittedLearnerAim','Full','','')
+,('DASPayments','[Payments2].[ProviderAdjustmentPayments]','StgPmts','stg_ProviderAdjustmentPayments','Incremental','CONCAT(''ParamAcademicYear'',''-R'',RIGHT(''00''+''ParamCollectionPeriod'',2)) ','Ukprn,SubmissionId,SubmissionCollectionPeriod,SubmissionAcademicYear,PaymentType,PaymentTypeName,Amount,CollectionPeriodName,CollectionPeriodMonth,CollectionPeriodYear')
+,('DASPayments','[Payments2].[SubmittedLearnerAim]','StgPmts','SubmittedLearnerAim','Full','','Id,Ukprn,LearnerReferenceNumber,LearningAimFrameworkCode,LearningAimPathwayCode,LearningAimProgrammeType,LearningAimStandardCode,LearningAimReference,CollectionPeriod,AcademicYear,IlrSubmissionDateTime,CreationDate,LearnerUln,JobId,ContractType')
 
 
 

84f84efc1956189bc2449dd024ae81aba1e2199d
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-07-10 09:24:39 +0100

    3897

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_CandidateRegDetails.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Va_CandidateRegDetails.sql
@@ -11,6 +11,5 @@
    ,[SourceDb] Varchar(256)
    ,[Migrated_EMailID] Varchar(255)
    ,[Migrated_CandidateID] BIGINT NOT NULL
-   ,[SourceDb] Varchar(256)
    ,[AsDm_UpdatedDateTime]  [datetime2](7)						DEFAULT (getdate())
 )

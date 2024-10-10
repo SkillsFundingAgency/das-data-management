861cb6ad11f4605c876763c13c16ac1db7b3f4e9
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-10-10 17:26:08 +0100

    4079

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_RAT_Region.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_RAT_Region.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [Asdata_pl].[RAT_Region]
 (
-	[Id] INT IDENTITY(1,1) PRIMARY KEY,
+	[Id] INT PRIMARY KEY,
 	[SubregionName] [varchar](250) NULL,
 	[RegionName] [varchar](25) NULL,
 	[Latitude] [float] NOT NULL,

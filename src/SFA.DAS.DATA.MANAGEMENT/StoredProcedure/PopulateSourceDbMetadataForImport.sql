706f30d2b9bed36fdbfcdef9fe02630c41afdf0e
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-10-14 16:24:42 +0100

    4079

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
@@ -104,12 +104,14 @@ VALUES
 
 /*RAT Metadata*/
 
+
+
 INSERT INTO Mtd.SourceConfigForImport (SourceDatabaseName,SourceTableName,SourceSchemaName,ColumnNamesToInclude,ColumnNamesToExclude,ColumnNamesToMask,FullCopyToPL,ModelDataToPL,PLTableName)
 VALUES
 ('RAT','EmployerRequest','dbo','[Id], [OriginalLocation], [RequestTypeId], [AccountId], [StandardReference], [NumberOfApprentices], [SameLocation], [SingleLocation], [AtApprenticesWorkplace], [DayRelease], [BlockRelease], [RequestedAt], [RequestedBy], [RequestStatusId], [ExpiredAt], [CancelledAt], [ModifiedBy], [ValidFrom], [ValidTo]','','',1,0,'RAT_EmployerRequest'),
 ('RAT','EmployerRequestRegion','dbo','[EmployerRequestId], [RegionId], [ModifiedBy], [ValidFrom], [ValidTo]','','',1,0,'RAT_EmployerRequestRegion'),
 ('RAT','ProviderResponseEmployerRequest','dbo','[EmployerRequestId],  [ProviderResponseId], [AcknowledgedAt], [AcknowledgedBy], [ValidFrom], [ValidTo]','','[Ukprn]',1,0,'RAT_ProviderResponseEmployerRequest'),
-('RAT','ProviderResponse','dbo','[Id], [RespondedAt], [RespondedBy], [ValidFrom], [ValidTo]','[PhoneNumber],[Website]','[ContactName],[Email]',1,1,'RAT_ProviderResponse'),
+('RAT','ProviderResponse','dbo','[Id], [RespondedAt], [RespondedBy], [ValidFrom], [ValidTo]','[PhoneNumber],[Website]','[ContactName],[Email]',1,0,'RAT_ProviderResponse'),
 ('RAT','Region','dbo','[Id], [SubregionName], [RegionName], [Latitude], [Longitude], [ValidFrom], [ValidTo]','','',1,0,'RAT_Region'),
 ('RAT','RequestStatus','dbo','[Id], [Description]','','',1,0,'RAT_RequestStatus'),
 ('RAT','RequestType','dbo','[Id], [Description]','','',1,0,'RAT_RequestType'),
@@ -117,10 +119,13 @@ VALUES
 ('RAT','Status','dbo','[Id], [Description]','','',1,0,'RAT_Status')
 
 
+/*
 INSERT INTO Mtd.SourceConfigForImport
 (SourceDatabaseName,SourceTableName,SourceSchemaName,ColumnNamesToInclude,ColumnNamesToExclude,ColumnNamesToMask,ModelDataToPL,IsQueryBasedImport,SourceQuery,PLTableName,StagingTableName)
 VALUES
  ('RAT','ProviderResponse','dbo','[Id], [RespondedAt], [RespondedBy], [ValidFrom], [ValidTo]','[PhoneNumber],[Website]','[ContactName],[Email]',1,1,'SELECT [Id], [RespondedAt], [RespondedBy], [ValidFrom], [ValidTo],[ContactName],[Email] FROM dbo.ProviderResponse','RAT_ProviderResponse','RAT_ProviderResponse')
+*/
+
 
 INSERT INTO Mtd.SourceConfigForImport
 (SourceDatabaseName,SourceTableName,SourceSchemaName,ColumnNamesToInclude,ColumnNamesToExclude,ColumnNamesToMask,ModelDataToPL,IsQueryBasedImport,SourceQuery,StagingTableName)

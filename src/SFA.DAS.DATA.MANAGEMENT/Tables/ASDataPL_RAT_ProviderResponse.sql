99417e3daa8f526a41d202519453b56ee35292b1
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-10-14 13:32:13 +0100

    4079

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_RAT_ProviderResponse.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_RAT_ProviderResponse.sql
@@ -1,12 +1,12 @@
 CREATE TABLE [Asdata_pl].[RAT_ProviderResponse](
 	[Id] [uniqueidentifier] PRIMARY KEY,
-	[ContactName] [nvarchar](256) NOT NULL,
-	[Email] [varchar](256) NOT NULL,
-	[PhoneNumber] [varchar](25) NULL,
-	[Website] [varchar](256) NULL,
 	[RespondedAt] [datetime2](7) NOT NULL,
 	[RespondedBy] [uniqueidentifier] NOT NULL,
 	[ValidFrom] [datetime2](0),
 	[ValidTo] [datetime2](0) ,
+	[ContactName] [nvarchar](256) NOT NULL,
+	[Email] [varchar](256) NOT NULL,
+	[Website] [varchar](256) NULL,
+	[PhoneNumber] [varchar](25) NULL,
 	AsDm_UpdatedDateTime datetime2 default getdate()	NULL
 )

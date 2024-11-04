62795782ff0d8e33eb744db73d9aad3989c832fd
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-11-04 15:52:09 +0000

    4174

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_PFBE_ProviderRatingSummary.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [Asdata_PL].[PfbE_ProviderRatingSummary](
-	[Ukprn] [bigint] NOT NULL,
-	[Rating] [nvarchar](20) NOT NULL,
-	[RatingCount] [int] NOT NULL,
-	[TimePeriod] [nvarchar](50) NOT NULL,
-	[UpdatedOn] [datetime2](7) NULL,
-	[AsDm_UpdatedDateTime] datetime2 default getdate()	NULL
-PRIMARY KEY ([Ukprn] ,[Rating] ,[TimePeriod] ))

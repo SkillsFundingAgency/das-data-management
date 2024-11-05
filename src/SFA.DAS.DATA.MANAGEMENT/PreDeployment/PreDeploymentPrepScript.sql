6be3a005b16bf2bde174d13bebf92c2e5227189c
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-11-05 10:26:16 +0000

    4177

--- src/SFA.DAS.DATA.MANAGEMENT/PreDeployment/PreDeploymentPrepScript.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PreDeployment/PreDeploymentPrepScript.sql
@@ -682,7 +682,8 @@ DROP TABLE IF EXISTS [StgPmts].[Postcode_LookupGeographicalAttributes]
 DROP TABLE IF EXISTS [StgPmts].[Pst_LookupGOR]
 DROP TABLE IF EXISTS [StgPmts].[Pst_LookupLocalAuthority] 
 DROP TABLE IF EXISTS [ASDATA_PL].[RAT_Status]
-
+DROP TABLE IF EXISTS [ASDATA_PL].[PFBE_ProviderStarsSummary]
+DROP TABLE IF EXISTS [ASDATA_PL].[PFBE_ProviderRatingSummary]
 
 --DROP TABLE IF EXISTS [lkp].[Pst_LocalAuthority]
 --DROP TABLE IF EXISTS [lkp].[Pst_GOR]

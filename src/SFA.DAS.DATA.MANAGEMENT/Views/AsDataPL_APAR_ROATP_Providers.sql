b8e8d1bd69095ab100d3a9b0bd9b9bf60ced1eb0
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-11-26 12:23:32 +0000

    4249

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataPL_APAR_ROATP_Providers.sql
@@ -0,0 +1,19 @@
+CREATE VIEW [AsData_PL].[APAR_ROATP_Providers]
+	AS 
+SELECT
+        o.[UKPRN]
+      , o.[LegalName] AS Name
+      , pt.ProviderType
+      , CAST(JSON_VALUE(OrganisationData, '$.StartDate') AS DATE) AS StartDate
+      , os.[Status]
+      , CAST(JSON_VALUE(OrganisationData, '$.ApplicationDeterminedDate') AS DATE) AS ApplicationDeterminedDate
+
+FROM APAR_ROATP_Organisations o
+    LEFT JOIN APAR_ROATP_ProviderTypes pt
+    ON o.ProviderTypeId = pt.Id
+    LEFT JOIN APAR_ROATP_OrganisationStatus os
+    ON o.StatusId = os.Id
+
+WHERE 
+ os.[Status] IN(
+    'Active','Active - but not taking on apprentices')

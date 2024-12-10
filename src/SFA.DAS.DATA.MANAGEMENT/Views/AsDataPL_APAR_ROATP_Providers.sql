b6960119d85ae07b8e567e7084f519114372bd85
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-12-10 09:57:14 +0000

    4249

--- src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataPL_APAR_ROATP_Providers.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Views/AsDataPL_APAR_ROATP_Providers.sql
@@ -1,17 +1,17 @@
 CREATE VIEW [AsData_PL].[APAR_ROATP_Providers]
 	AS 
 SELECT
-        o.[UKPRN]
+      o.[UKPRN]
       , o.[LegalName] AS Name
       , pt.ProviderType
       , CAST(JSON_VALUE(OrganisationData, '$.StartDate') AS DATE) AS StartDate
       , os.[Status]
       , CAST(JSON_VALUE(OrganisationData, '$.ApplicationDeterminedDate') AS DATE) AS ApplicationDeterminedDate
 
-FROM APAR_ROATP_Organisations o
-    LEFT JOIN APAR_ROATP_ProviderTypes pt
+FROM [ASData_PL].[APAR_ROATP_Organisations] o
+    LEFT JOIN [ASData_PL].[APAR_ROATP_ProviderTypes] pt
     ON o.ProviderTypeId = pt.Id
-    LEFT JOIN APAR_ROATP_OrganisationStatus os
+    LEFT JOIN [ASData_PL].[APAR_ROATP_OrganisationStatus] os
     ON o.StatusId = os.Id
 
 WHERE 

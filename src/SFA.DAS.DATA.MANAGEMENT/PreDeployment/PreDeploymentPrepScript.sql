ac4a866a7ae8fdb6cd625de828b7e9f621e259a8
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-12-10 12:11:45 +0000

    4245

--- src/SFA.DAS.DATA.MANAGEMENT/PreDeployment/PreDeploymentPrepScript.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/PreDeployment/PreDeploymentPrepScript.sql
@@ -682,6 +682,7 @@ DROP TABLE IF EXISTS [StgPmts].[Postcode_LookupGeographicalAttributes]
 DROP TABLE IF EXISTS [StgPmts].[Pst_LookupGOR]
 DROP TABLE IF EXISTS [StgPmts].[Pst_LookupLocalAuthority] 
 DROP TABLE IF EXISTS [ASDATA_PL].[RAT_Status]
+DROP TABLE IF EXISTS [ASDATA_PL].[FeedbackTargetVariant_Staging]
 
 
 
--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASData_PL_Appfb_FeedbackTargetVariant_Staging.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASData_PL_Appfb_FeedbackTargetVariant_Staging.sql
@@ -1,4 +1,4 @@
-CREATE TABLE [ASData_PL].[FeedbackTargetVariant_Staging](
+CREATE TABLE [ASData_PL].[Appfb_FeedbackTargetVariant_Staging](
 	 [ApprenticeshipId] [bigint] NOT NULL
 	,[Variant] VARCHAR(100) NOT NULL 
 	,[AsDm_UpdatedDateTime] datetime2 default getdate()

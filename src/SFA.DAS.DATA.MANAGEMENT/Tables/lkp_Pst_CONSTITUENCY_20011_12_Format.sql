fcc9a0a6b22cfae6d30c767be9829de64d42887e
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2024-12-11 12:06:22 +0000

    4250

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/lkp_Pst_CONSTITUENCY_20011_12_Format.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/lkp_Pst_CONSTITUENCY_20011_12_Format.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [lkp].[Pst_CONSTITUENCY_20011_12_Format](
 	[Pst_CONSTITUENCY] [varchar](9) NULL,
 	[Pst_CONSTITUENC_Desc] [varchar](60) NULL,
-	[Pst_CONSTITUENC_Desc2] [varchar](60) NULL
+	[Pst_CONSTITUENC_Desc2] [varchar](35) NULL
 )ON [Primary]
 GO

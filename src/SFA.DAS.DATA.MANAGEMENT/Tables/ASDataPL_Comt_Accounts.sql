834aa7010a961281ed19afa92058f438f6e5d1fd
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2025-01-13 17:37:45 +0000

    4316

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_Accounts.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASDataPL_Comt_Accounts.sql
@@ -2,7 +2,7 @@ CREATE TABLE [AsData_PL].[Comt_Accounts]
 (
 	[Id] [bigint] NOT NULL,
 	[HashedId] [nchar](6) NOT NULL,
-	[PublicHashedId] [nchar](6) NOT NULL,
+	[PublicHashedId] [nchar](6) NULL,
 	[Name] [nvarchar](100) NOT NULL,
 	[Created] [datetime2](7) NOT NULL,
 	[Updated] [datetime2](7) NULL,

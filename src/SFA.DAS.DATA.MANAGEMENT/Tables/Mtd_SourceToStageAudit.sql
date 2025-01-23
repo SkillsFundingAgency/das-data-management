70e4dfade82130b67453c1e294d33b3a35fad43b
ShafanaJ <shafana.roohijahubar@education.gov.uk>
2025-01-23 17:11:33 +0000

    4304

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/Mtd_SourceToStageAudit.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Mtd_SourceToStageAudit.sql
@@ -5,7 +5,7 @@ CREATE TABLE [Mtd].[SourceToStageAudit]
     SourceTableName NVARCHAR(100) NOT NULL,
     SourceQuery NVarchar(max) ,
     WatermarkColumnName NVARCHAR(100) NOT NULL,
-    WatermarkValue NVARCHAR(100) ,
+    WatermarkValue DATETIME ,
     StagingTableName NVARCHAR(100),
     LastUpdatedTimestamp DATETIME
 )

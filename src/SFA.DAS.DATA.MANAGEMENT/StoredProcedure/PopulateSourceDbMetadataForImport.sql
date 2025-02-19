39b836369543b2cafa7fc39738a73cb15b0b2b3d
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2025-02-19 19:55:45 +0000

    Fixed script

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
@@ -146,6 +146,7 @@ VALUES
 
 INSERT INTO Mtd.SourceConfigForImport
 (SourceDatabaseName,SourceTableName,SourceSchemaName,ColumnNamesToInclude,ColumnNamesToExclude,ColumnNamesToMask,[ModelDataToPL],[StagingTableName],[PLTableName])
+VALUES
 ('EmpInc','PendingPayment','archive','[Id],[AccountId],[ApprenticeshipIncentiveId],[DueDate],[Amount],[CalculatedDate],[PaymentMadeDate],[PeriodNumber],[PaymentYear],[AccountLegalEntityId],[EarningType],[ClawedBack]','','',0,'EI_Archive_PendingPayment','EI_Archive_PendingPayment')
 
 INSERT INTO Mtd.SourceConfigForImport

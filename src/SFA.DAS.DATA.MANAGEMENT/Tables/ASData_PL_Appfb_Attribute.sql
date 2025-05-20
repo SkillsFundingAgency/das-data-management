2c44631835ec4e201e83a55616adba31ab19e879
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2025-05-20 14:36:29 +0100

    Updated char length

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/ASData_PL_Appfb_Attribute.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/ASData_PL_Appfb_Attribute.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [ASData_PL].[Appfb_Attribute]
 (
 	[AttributeId] [int] NULL,
-	[AttributeName] [nvarchar](100) NULL,
+	[AttributeName] [nvarchar](150) NULL,
 	[AsDm_UpdatedDateTime] datetime2 default getdate(),
 	[Category] [nvarchar](100) NULL,
 	[AttributeType] [nvarchar](100) NULL,

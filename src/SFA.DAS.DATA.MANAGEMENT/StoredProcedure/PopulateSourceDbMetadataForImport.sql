80dffe36e6fc7a4255a7caca6a45d7b04614266d
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2025-12-16 19:40:32 +0000

    Fixed

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/PopulateSourceDbMetadataForImport.sql
@@ -470,7 +470,7 @@ VALUES
 ('Aprog','KSBProgress','dbo','[KSBProgressId], [ApprenticeshipId], [KSBProgressType], [KSBId], [KSBKey], [CurrentStatus], [Note]','','','aProg_KSBProgress',0,1),
 ('Aprog','Task','dbo','[TaskId], [ApprenticeshipId], [DueDate], [Title], [ApprenticeshipCategoryId], [Note], [CompletionDateTime], [CreatedDateTime], [Status]','','','aProg_Task',0,1),
 ('Aprog','TaskKSBs','dbo','[TaskId], [KSBProgressId]','','','aProg_TaskKSBs',0,1),
-('Aprog','KSBProgressStatusHistory','dbo','[KSBProgressId], [Status], [StatusTime]','','','aProg_KSBProgressStatusHistory',0,1),
+('Aprog','KSBProgressStatusHistory','dbo','[KSBProgressId], [Status], [StatusTime]','','','aProg_KSBProgressStatusHistory',0,1)
 
 
 /* Apprentice Feedback Config */

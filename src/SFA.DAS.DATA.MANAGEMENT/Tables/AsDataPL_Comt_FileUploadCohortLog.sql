9491ad627b135c7a51b804c9cb8b31dd7e4cfc96
Harish Naraharisetti <Harish.naraharisetti@education.gov.uk>
2026-02-12 09:08:39 +0000

    File Upload log changes

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Comt_FileUploadCohortLog.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [AsData_PL].[Comt_FileUploadCohortLog]
+(
+	[Id] BIGINT NOT NULL PRIMARY KEY ,
+	[FileUploadLogId] BIGINT NOT NULL,
+	[CommitmentId] BIGINT NOT NULL,
+	[RowCount] INT NULL,
+	[AsDm_UpdatedDateTime] datetime2 default getdate()
+)
+GO

708f1a0c97f309c111c742d5917a6c9033771d83
karthik-dfe <Karthikeyan.Kannan@education.gov.uk>
2026-03-04 17:01:36 +0000

    5308

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Cpg_UserData.sql
@@ -0,0 +1,15 @@
+CREATE TABLE [AsData_PL].[CPG_UserData]
+(
+	[Id] int NOT NULL,
+	[FirstName] [varchar](250) NOT NULL,
+	[LastName] [varchar](250) NOT NULL,
+	[Email] [varchar](250) NOT NULL,
+	[UkEmployerSize] [varchar](50) NOT NULL,
+	[PrimaryIndustry] [varchar](50) NOT NULL,
+	[PrimaryLocation] [varchar](50) NOT NULL,
+	[AppsgovSignUpDate] [datetime] NOT NULL,
+	[PersonOrigin] [varchar](50) NULL,
+	[IncludeInUR] [bit] NULL,
+	[Asdm_UpdatedDateTime] datetime2 default getdate(),
+    CONSTRAINT PK_aProg_TaskId PRIMARY KEY CLUSTERED ([Id])
+)

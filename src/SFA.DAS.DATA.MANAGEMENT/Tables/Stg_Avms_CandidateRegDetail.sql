43759f251ab3e6561fadd14c71cdbfc2c7ae0102
Harish Naraharisetti <Harish.naraharisetti@education.gov.uk>
2026-03-24 21:48:47 +0000

    added avms stage table

---
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/Stg_Avms_CandidateRegDetail.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [Stg].[Avms_CandidateRegDetails](
+	[CandidateId] [int] NULL,
+	[CandidateGuid] [uniqueidentifier] NULL,
+	[FirstName] [nvarchar](500) NULL,
+	[MiddleName] [nvarchar](500) NULL,
+	[SurName] [nvarchar](500) NULL,
+	[DateOfBirth] [nvarchar](500) NULL,
+	[FullName] [nvarchar](500) NULL,
+	[EmailAddress] [nvarchar](500) NULL
+) 

130a94d9229f64b59825fee36a38e515feee3ed5
Harish Naraharisetti <Harish.naraharisetti@education.gov.uk>
2026-05-27 11:56:07 +0100

    Increased length for campaign pipeline

--- src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Cpg_DisplayedEmails.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/Tables/AsDataPL_Cpg_DisplayedEmails.sql
@@ -12,7 +12,7 @@ CREATE TABLE [AsData_PL].[CPG_DisplayedEmails]
 	[ClientName] varchar(255) NULL,
 	[Os] varchar(255) NULL,
 	[OsFamily] varchar(255) NULL,
-	[IpAddress] varchar(15) NULL,
+	[IpAddress] varchar(60) NULL,
 	[ClientType] varchar(255) NULL,
 	[ClientFamily] varchar(255) NULL,
 	[Asdm_UpdatedDateTime] datetime2 DEFAULT getdate(),

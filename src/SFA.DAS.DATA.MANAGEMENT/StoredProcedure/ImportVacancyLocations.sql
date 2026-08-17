8e19b679f22135c088a33d33f68c00deebdb8db1
Leah Cooney <Leah.COONEY@education.gov.uk>
2026-08-17 17:06:34 +0100

    updated query to use employerlocations

--- src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacancyLocations.sql
+++ src/SFA.DAS.DATA.MANAGEMENT/StoredProcedure/ImportVacancyLocations.sql
@@ -70,95 +70,47 @@ select
 ,[SourceDb]
 from (
 
-SELECT  
-		  VV.VacancyId
+SELECT
+       VV.VacancyId
       ,E.EmployerId
-      ,EPA.Postcode as VacancyPostCode
-      ,EPA.AddressLine1 as VacancyAddressLine1
-      ,EPA.AddressLine2 as VacancyAddressLine2
-      ,EPA.AddressLine3 as VacancyAddressLine3
-      ,EPA.AddressLine4 as VacancyAddressLine4
-      ,COALESCE(EPA.AddressLine4,EPA.AddressLine3,EPA.AddressLine2) as VacancyTown
-      ,CAST(EPA.Id as varchar(256)) as SourceVacancyLocationsId
-      ,'RCRT'                                                    as SourceDb
-	 FROM (
-        SELECT DISTINCT
-               VacancyReference
-              ,AccountId
-              ,AccountLegalEntityId
-          FROM Stg.RCRT_Vacancy
-       ) V
-	  LEFT
-	  JOIN Stg.RCRT_EmployerProfileAddress EPA
-	    ON EPA.AccountLegalEntityId=V.AccountLegalEntityId
-	  LEFT
-	  JOIN ASData_PL.Va_Employer_Rcrt E
-	    ON E.DasAccountId_v2=V.AccountId
-	   AND E.SourceDb='RCRT'
-	  LEFT
-	  JOIN ASData_PL.Va_Vacancy VV
-	    ON VV.VacancyReferenceNumber=TRY_CAST(V.VacancyReference as bigint)
-	 WHERE COALESCE(EPA.Postcode
-	               ,EPA.AddressLine1
-	               ,EPA.AddressLine2
-	               ,EPA.AddressLine3
-	               ,EPA.AddressLine4
-	               ,'NA')<>'NA'
+      ,d.Postcode as VacancyPostCode
+      ,d.AddressLine1 as VacancyAddressLine1
+      ,d.AddressLine2 as VacancyAddressLine2
+      ,d.AddressLine3 as VacancyAddressLine3
+      ,d.AddressLine4 as VacancyAddressLine4
+      ,COALESCE(d.AddressLine4, d.AddressLine3, d.AddressLine2) as VacancyTown
+      ,V.SourceVacancyReference
+      ,'RCRT' as SourceDb
+FROM Stg.RCRT_Vacancy V
 
-Union
+CROSS APPLY OPENJSON(V.EmployerLocations)
+WITH
+(
+    AddressLine1 NVARCHAR(100) '$.addressLine1',
+    AddressLine2 NVARCHAR(100) '$.addressLine2',
+    AddressLine3 NVARCHAR(100) '$.addressLine3',
+    AddressLine4 NVARCHAR(100) '$.addressLine4',
+    Postcode     NVARCHAR(50)  '$.postcode'
+) d
 
+LEFT JOIN ASData_PL.Va_Employer E
+    ON E.DasAccountId_v2 = V.AccountId
+   AND E.SourceDb = 'RCRT'
 
-select 
+LEFT JOIN ASData_PL.Va_Vacancy VV
+    ON VV.VacancyReferenceNumber =
+       TRY_CAST(V.SourceVacancyReference AS bigint)
 
-VacancyId
-,EmployerId
-,VacancyPostCode
-,VacancyAddressLine1
-,VacancyAddressLine2
-,VacancyAddressLine3
-,VacancyAddressLine4
-,VacancyTown
-,SourceVacancyId as SourceVacancyLocationsId
-,'RCRT'                                                    as SourceDb
-
-from (
-        SELECT DISTINCT
-               V.VacancyId
-              ,E.EmployerId
-              ,EPA.Postcode as VacancyPostCode
-              ,EPA.AddressLine1 as VacancyAddressLine1
-              ,EPA.AddressLine2 as VacancyAddressLine2
-              ,EPA.AddressLine3 as VacancyAddressLine3
-              ,EPA.AddressLine4 as VacancyAddressLine4
-              ,COALESCE(EPA.AddressLine4,EPA.AddressLine3,EPA.AddressLine2) as VacancyTown
-              ,CAST(EPA.Id as varchar(256)) as SourceVacancyId
-              ,'RCRT' as SourceDb
-          FROM (
-                SELECT DISTINCT
-                       VacancyReference
-                      ,AccountId
-                      ,AccountLegalEntityId
-                  FROM Stg.RCRT_Vacancy
-               ) VR
-          LEFT
-          JOIN Stg.RCRT_EmployerProfileAddress EPA
-            ON EPA.AccountLegalEntityId=VR.AccountLegalEntityId
-          LEFT
-          JOIN ASData_PL.Va_Employer_Rcrt E
-            ON E.DasAccountId_v2=VR.AccountId
-           AND E.SourceDb='RCRT'
-          LEFT
-          JOIN ASData_PL.Va_Vacancy V
-            ON V.VacancyReferenceNumber=TRY_CAST(VR.VacancyReference as bigint)
-         WHERE COALESCE(EPA.Postcode
-                       ,EPA.AddressLine1
-                       ,EPA.AddressLine2
-                       ,EPA.AddressLine3
-                       ,EPA.AddressLine4
-                       ,'NA')<>'NA'
-       ) a
-) a
-
+WHERE ISJSON(V.EmployerLocations) = 1
+  AND COALESCE(
+        d.Postcode,
+        d.AddressLine1,
+        d.AddressLine2,
+        d.AddressLine3,
+        d.AddressLine4,
+        'NA'
+      ) <> 'NA'
+)a
 COMMIT TRANSACTION
 
 
